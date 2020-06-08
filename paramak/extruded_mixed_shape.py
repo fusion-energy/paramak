dcf4e4eb5e66a77fc1fd9c04ef8a5c1e948ac1ee
billingsley-john <johnbillingsley1996@yahoo.com>
2020-06-08 17:14:35 +0100

    added extrude classes

---
+++ paramak/extruded_mixed_shape.py
@@ -0,0 +1,180 @@
+"""
+This file is part of PARAMAK which is a design tool capable
+of creating 3D CAD models compatible with automated neutronics
+analysis.
+
+PARAMAK is released under GNU General Public License v3.0.
+Go to https://github.com/ukaea/paramak/blob/master/LICENSE
+for full license details.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Copyright (C) 2019  UKAEA
+
+THERE IS NO WARRANTY FOR THE PROGRAM, TO THE EXTENT PERMITTED BY
+APPLICABLE LAW.  EXCEPT WHEN OTHERWISE STATED IN WRITING THE COPYRIGHT
+HOLDERS AND/OR OTHER PARTIES PROVIDE THE PROGRAM "AS IS" WITHOUT WARRANTY
+OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING, BUT NOT LIMITED TO,
+THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+PURPOSE.  THE ENTIRE RISK AS TO THE QUALITY AND PERFORMANCE OF THE PROGRAM
+IS WITH YOU.  SHOULD THE PROGRAM PROVE DEFECTIVE, YOU ASSUME THE COST OF
+ALL NECESSARY SERVICING, REPAIR OR CORRECTION.
+"""
+
+from collections import Iterable
+
+import cadquery as cq
+
+from paramak import Shape
+
+
+class ExtrudeMixedShape(Shape):
+    """Extrude a 3d CadQuery solid from points connected with
+       a mixture of straight lines and splines
+
+        :param points: A list of XZ coordinates and connection types where the last
+         entry has the same XZ coordinates as the first entry. Also the connections
+         types are either 'spline' or 'straight'. For example [(2.,1.,'straight'),
+         (2.,2.,'straight'), (1.,2.,'spline'), (1.,1.,'spline'), (2.,1.,'spline')]
+        :type points: a list of tuples each containing X (float), Z (float), connection
+         type (string) values 
+        :param stp_filename: the filename used when saving stp files as part of a reactor
+        :type stp_filename: str
+        :param color: the color to use when exporting as html graphs or png images
+        :type color: Red, Green, Blue, [Alpha] values. RGB and RGBA are sequences of,
+        3 or 4 floats respectively each in the range 0-1
+        :param distance: The extrude distance to use (cm units if used for neutronics)
+        :type distance: float
+        :param azimuth_placement_angle: the angle or angles to use when rotating the 
+        shape on the azimuthal axis
+        :type azimuth_placement_angle: float or iterable of floats
+        :param cut: An optional cadquery object to perform a boolean cut with this object
+        :type cut: cadquery object
+        :param material_tag: The material name to use when exporting the neutronics description
+        :type material_tag: str
+        :param name: The legend name used when exporting a html graph of the shape
+        :type name: str
+       """
+
+    def __init__(
+        self,
+        points,
+        distance,
+        workplane="XZ",
+        stp_filename=None,
+        solid=None,
+        color=None,
+        azimuth_placement_angle=0,
+        cut=None,
+        material_tag=None,
+        name=None,
+    ):
+
+        super().__init__(
+            points,
+            name,
+            color,
+            material_tag,
+            stp_filename,
+            azimuth_placement_angle,
+            workplane,
+        )
+
+        self.distance = distance
+        self.solid = solid
+        self.cut = cut
+
+    @property
+    def cut(self):
+        return self._cut
+
+    @cut.setter
+    def cut(self, value):
+        self._cut = value
+
+    @property
+    def solid(self):
+        self.create_solid()
+        return self._solid
+
+    @solid.setter
+    def solid(self, value):
+        self._solid = value
+
+    @property
+    def distance(self):
+        return self._distance
+
+    @distance.setter
+    def distance(self, value):
+        self._distance = value
+
+    def create_solid(self):
+        """Creates a 3d solid using points with straight and spline
+        connections edges, azimuth_placement_angle and distance.
+
+        :return: a 3d solid volume
+        :rtype: a cadquery solid
+        """
+
+        # obtains the first two values of the points list
+        XZ_points = [(p[0], p[1]) for p in self.points]
+
+        # obtains the last values of the points list
+        connections = [p[2] for p in self.points[:-1]]
+
+        current_linetype = connections[0]
+        current_points_list = []
+        instructions = []
+        # groups together common connection types
+        for i, c in enumerate(connections):
+            if c == current_linetype:
+                current_points_list.append(XZ_points[i])
+            else:
+                current_points_list.append(XZ_points[i])
+                instructions.append({current_linetype: current_points_list})
+                current_linetype = c
+                current_points_list = [XZ_points[i]]
+        instructions.append({current_linetype: current_points_list})
+
+        if list(instructions[-1].values())[0][-1] != XZ_points[0]:
+            keyname = list(instructions[-1].keys())[0]
+            instructions[-1][keyname].append(XZ_points[0])
+
+        solid = cq.Workplane(self.workplane)
+        solid.moveTo(XZ_points[0][0], XZ_points[0][1])
+
+        for entry in instructions:
+            if list(entry.keys())[0] == "spline":
+                solid = solid.spline(listOfXYTuple=list(entry.values())[0])
+            if list(entry.keys())[0] == "straight":
+                solid = solid.polyline(list(entry.values())[0])
+
+        # performs extrude in both directions, hence distance / 2
+        solid = solid.close().extrude(distance=-self.distance / 2.0, both=True)
+
+        # Checks if the azimuth_placement_angle is a list of angles
+        if isinstance(self.azimuth_placement_angle, Iterable):
+            rotated_solids = []
+            # Perform seperate rotations for each angle
+            for angle in self.azimuth_placement_angle:
+                rotated_solids.append(solid.rotate((0, 0, -1), (0, 0, 1), angle))
+            solid = cq.Workplane(self.workplane)
+
+            # Joins the seperate solids together
+            for i in rotated_solids:
+                solid = solid.union(i)
+        else:
+            # Peform rotations for a single azimuth_placement_angle angle
+            solid = solid.rotate((0, 0, 1), (0, 0, -1), self.azimuth_placement_angle)
+
+        # If a cut solid is provided then perform a boolean cut
+        if self.cut is not None:
+            solid = solid.cut(self.cut.solid)
+
+        self.solid = solid
+
+        return solid
