f8100453cffc800f3ae1679570ebbe2e35cdcb20
shimwell <shimwell@yahoo.com>
2020-06-11 19:43:40 +0100

    added root dir files

---
+++ setup.py
@@ -0,0 +1,36 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="paramak",
+    version="0.0.1",
+    author="Jonathan Shimwell",
+    author_email="jonathan.shimwell@ukaea.uk",
+    description="Create 3D fusion reactor CAD models based on input parameters",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/shimwell/paramak",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    setup_requires=["pytest-runner"],
+    tests_require=["pytest-cov",],
+    install_requires=[
+        "pytest-cov",
+        "pyglet",
+        "plotly",
+        "scipy",
+        "numpy",
+        "tqdm",
+        "matplotlib",
+        "trimesh",
+        "pyrender",
+        "uncertainties",
+        "importlib_resources",
+    ],
+)
