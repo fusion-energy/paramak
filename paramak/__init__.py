fd53f8a0887c320b21bfaf489014d057f9007b27
shimwell <shimwell@yahoo.com>
2020-06-12 12:31:43 +0100

    added inits for package install

---
+++ paramak/__init__.py
@@ -0,0 +1,12 @@
+from .reactor import Reactor
+from .shape import Shape
+
+from .extruded_spline_shape import ExtrudeSplineShape
+from .extruded_straight_shape import ExtrudeStraightShape
+from .extruded_mixed_shape import ExtrudeMixedShape
+from .extruded_circle_shape import ExtrudeCircleShape
+
+from .rotate_spline_shape import RotateSplineShape
+from .rotate_straight_shape import RotateStraightShape
+from .rotate_mixed_shape import RotateMixedShape
+from .rotate_circle_shape import RotateCircleShape
