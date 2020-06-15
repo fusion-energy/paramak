656e927ad9bb63257b9d06e108ba86fead9c0eae
shimwell <shimwell@yahoo.com>
2020-06-15 18:23:53 +0100

    corrected volumes

--- tests/test_ExtrudeSplineShape.py
+++ tests/test_ExtrudeSplineShape.py
@@ -82,10 +82,10 @@ class test_object_properties(unittest.TestCase):
             distance=30,
         )
 
-        assert inner_shape.volume == pytest.approx(1165.2727)
-        assert outer_shape.volume == pytest.approx(3775.4836)
+        assert inner_shape.volume == pytest.approx(1068, abs=2)
+        assert outer_shape.volume == pytest.approx(3462, abs=2)
         assert outer_shape_with_cut.volume == pytest.approx(
-            3775.4836 - 1165.2727, abs=0.1
+            3462 - 1068, abs=2
         )
 
 
