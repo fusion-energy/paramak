0fd815d6e99acb88f59f6a0209afcc172c5d1ff0
billingsley-john <johnbillingsley1996@yahoo.com>
2020-06-15 15:28:07 +0100

    added multiple boolean cuts to user classes

--- paramak/rotate_straight_shape.py
+++ paramak/rotate_straight_shape.py
@@ -179,7 +179,12 @@ class RotateStraightShape(Shape):
 
         # If a cut solid is provided then perform a boolean cut
         if self.cut is not None:
-            solid = solid.cut(self.cut.solid)
+            # Allows for multiple cuts to be applied
+            if isinstance(self.cut, Iterable):
+                for cutting_solid in self.cut:
+                    solid = solid.cut(cutting_solid.solid)
+            else:
+                solid = solid.cut(self.cut.solid)
 
         self.solid = solid
 
