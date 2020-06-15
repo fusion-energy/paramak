f1bb23137c4a2120d044dae0be4f3a29af4aec80
billingsley-john <johnbillingsley1996@yahoo.com>
2020-06-15 10:48:28 +0100

    added dirty solid flag tests for some user classes

--- tests/test_ExtrudeCircleShape.py
+++ tests/test_ExtrudeCircleShape.py
@@ -85,3 +85,112 @@ class test_object_properties(unittest.TestCase):
         assert outer_shape_with_cut.volume == pytest.approx(
             (math.pi * 10 ** 2 * 20) - (math.pi * 5 ** 2 * 20)
         )
+
+    def test_initial_solid_construction(self):
+        """tests that a cadquery solid with a unique hash is constructed when .solid is called"""
+
+        test_shape = ExtrudeCircleShape(
+            points=[(0, 0), (0, 20), (20, 20), (20, 0), (0, 0)],
+            radius=10,
+            distance=20
+        )
+
+        assert test_shape.hash_value is None
+        assert test_shape.solid is not None
+        assert type(test_shape.solid).__name__ == "Workplane"
+        assert test_shape.hash_value is not None
+
+    def test_solid_return(self):
+        """tests that the same cadquery solid with the same unique hash is returned when shape.solid is called again when no changes have been made to the shape"""
+
+        test_shape = ExtrudeCircleShape(
+            points=[(0, 0), (0, 20), (20, 20), (20, 0), (0, 0)],
+            radius=10,
+            distance=20
+        )
+
+        assert test_shape.solid is not None
+        assert test_shape.hash_value is not None
+        initial_hash_value = test_shape.hash_value
+
+        assert test_shape.solid is not None
+        assert test_shape.hash_value is not None
+        assert initial_hash_value == test_shape.hash_value
+
+    def test_conditional_solid_reconstruction(self):
+        """tests that a new cadquery solid with a new unique hash is constructed when .solid is called again after changes have been made to the shape"""
+
+        test_shape = ExtrudeCircleShape(
+            points=[(0, 0), (0, 20), (20, 20), (0, 0)],
+            radius=10,
+            distance=20
+        )
+
+        assert test_shape.solid is not None
+        assert test_shape.hash_value is not None
+        initial_hash_value = test_shape.hash_value
+
+        test_shape.distance = 30
+
+        assert test_shape.solid is not None
+        assert test_shape.hash_value is not None
+        assert initial_hash_value != test_shape.hash_value
+
+    def test_hash_value_update(self):
+        """tests that the hash_value of the shape is not updated until a new solid has been created"""
+
+        test_shape = ExtrudeCircleShape(
+            points=[(0, 0), (0, 20), (20, 20), (0, 0)],
+            radius=10,
+            distance=20
+        )
+        test_shape.solid
+        assert test_shape.hash_value is not None
+        initial_hash_value = test_shape.hash_value
+
+        test_shape.distance = 30
+
+        assert test_shape.hash_value == initial_hash_value
+        test_shape.solid
+        assert test_shape.hash_value != initial_hash_value
+
+    def test_conditional_solid_reconstruction_parameters(self):
+        """tests that a new cadquery solid with a new unique hash is created when the shape properties of points, radius or distance are changed"""
+
+        # points
+        test_shape = ExtrudeCircleShape(
+            points=[(30, 0)], radius=5, distance=20
+        )
+        test_shape.solid
+        initial_hash_value = test_shape.hash_value
+        test_shape.points = [(40, 0)]
+        test_shape.solid
+        assert test_shape.solid is not None
+        assert test_shape.hash_value != initial_hash_value
+
+        # radius
+        test_shape = ExtrudeCircleShape(
+            points=[(30, 0)], radius=5, distance=20
+        )
+        test_shape.solid
+        initial_hash_value = test_shape.hash_value
+        test_shape.radius = 10
+        test_shape.solid
+        assert test_shape.solid is not None
+        assert test_shape.hash_value != initial_hash_value
+
+        # distance
+        test_shape = ExtrudeCircleShape(
+            points=[(30, 0)], radius=5, distance=20
+        )
+        test_shape.solid
+        initial_hash_value = test_shape.hash_value
+        test_shape.distance = 30
+        test_shape.solid
+        assert test_shape.solid is not None
+        assert test_shape.hash_value != initial_hash_value
+
+
+if __name__ == "__main__":
+    unittest.main()
+
