f4a7492ab9ab97776db3148c523796d4aa84a15f
billingsley-john <johnbillingsley1996@yahoo.com>
2020-06-16 18:12:58 +0100

    removed bash script for rebuilding rst files from source

--- documentation/make_docs_from_source.sh
+++
@@ -1,7 +0,0 @@
-sudo rm ./source/rst/modules.rst
-sudo rm ./source/rst/paramak.rst
-sudo rm ./source/rst/setup.rst
-sudo rm ./source/rst/tests.rst
-sudo rm ./source/rst/paramak.parametric_shapes.rst
-sphinx-apidoc -o ./source/rst/ ..
-sphinx-build -b html ./source/rst/ ./build/html/
