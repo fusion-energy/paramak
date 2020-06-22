63cd403bfe2756da0b1bc3c9e764afcd6e0a1640
shimwell <shimwell@yahoo.com>
2020-06-22 21:28:58 +0100

    updated volumes

--- tests/test_ExtrudeSplineShape.py
+++ tests/test_ExtrudeSplineShape.py
@@ -85,7 +85,7 @@ class test_object_properties(unittest.TestCase):
         assert inner_shape.volume == pytest.approx(1165, abs=2)
         assert outer_shape.volume == pytest.approx(3775, abs=2)
         assert outer_shape_with_cut.volume == pytest.approx(
-            3462 - 1068, abs=2
+            3775 - 1165, abs=2
         )
 
     def test_initial_solid_construction(self):
--- tests/test_RotateSplineShape.py
+++ tests/test_RotateSplineShape.py
@@ -67,7 +67,7 @@ class test_object_properties(unittest.TestCase):
         assert inner_shape.volume == pytest.approx(900.88, abs=0.1)
         assert outer_shape.volume == pytest.approx(2881.76, abs=0.1)
         assert outer_shape_with_cut.volume == pytest.approx(
-            2719.63 - 839.39, abs=0.1
+            2881.76 - 900.88, abs=0.2
         )
 
     def test_initial_solid_construction(self):
