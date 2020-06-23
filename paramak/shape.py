8dfc8877567968f8486e661e368d91f07b42713f
shimwell <shimwell@yahoo.com>
2020-06-23 11:28:38 +0100

    moved lic to github lic.md file

--- paramak/shape.py
+++ paramak/shape.py
@@ -1,30 +1,3 @@
-""""""
-
-"""
-This file is part of PARAMAK which is a design tool capable
-of creating 3D CAD models compatible with automated neutronics
-analysis.
-
-PARAMAK is released under GNU General Public License v3.0.
-Go to https://github.com/ukaea/paramak/blob/master/LICENSE
-for full license details.
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-Copyright (C) 2019  UKAEA
-
-THERE IS NO WARRANTY FOR THE PROGRAM, TO THE EXTENT PERMITTED BY
-APPLICABLE LAW.  EXCEPT WHEN OTHERWISE STATED IN WRITING THE COPYRIGHT
-HOLDERS AND/OR OTHER PARTIES PROVIDE THE PROGRAM "AS IS" WITHOUT WARRANTY
-OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING, BUT NOT LIMITED TO,
-THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
-PURPOSE.  THE ENTIRE RISK AS TO THE QUALITY AND PERFORMANCE OF THE PROGRAM
-IS WITH YOU.  SHOULD THE PROGRAM PROVE DEFECTIVE, YOU ASSUME THE COST OF
-ALL NECESSARY SERVICING, REPAIR OR CORRECTION.
-"""
 
 import math
 import numbers
