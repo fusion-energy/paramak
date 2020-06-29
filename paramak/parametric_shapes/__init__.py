0ad7936f662e27dd3ce2212f4d3bd9cdc317b2cf
=shimwell <mail@jshimwell.com>
2020-06-29 16:45:16 +0100

    added parametric shape to init

--- paramak/parametric_shapes/__init__.py
+++ paramak/parametric_shapes/__init__.py
@@ -11,3 +11,4 @@ from .center_column_plasma_dependant import CenterColumnShieldPlasmaHyperbola
 
 from .poloidal_field_coil import PoloidalFieldCoil
 from .poloidal_field_coil_case import PoloidalFieldCoilCase
+from .poloidal_field_coil_case import PoloidalFieldCoilCaseFromCoil
