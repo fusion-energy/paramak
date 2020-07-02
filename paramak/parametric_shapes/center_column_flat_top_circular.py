7d16d78b054aec3ae3ec7849f69a5898e308d3fa
billingsley-john <johnbillingsley1996@yahoo.com>
2020-07-02 13:40:03 +0100

    added circular shield parametric shapes

---
+++ paramak/parametric_shapes/center_column_flat_top_circular.py
@@ -0,0 +1,111 @@
+
+from paramak import RotateMixedShape
+
+class CenterColumnShieldFlatTopCircular(RotateMixedShape):
+
+    def __init__(
+        self,
+        height,
+        arc_height,
+        inner_radius,
+        mid_radius,
+        outer_radius,
+        workplane="XZ",
+        rotation_angle=360,
+        solid=None,
+        stp_filename="center_column.stp",
+        color=None,
+        points=None,
+        name="center_column",
+        material_tag="center_column_material",
+        azimuth_placement_angle=0,
+        cut=None,
+        hash_value=None,
+    ):
+
+        super().__init__(
+            points,
+            workplane,
+            name,
+            color,
+            material_tag,
+            stp_filename,
+            azimuth_placement_angle,
+            solid,
+            rotation_angle,
+            cut,
+            hash_value,
+        )
+
+        self.height = height
+        self.arc_height = arc_height
+        self.mid_radius = mid_radius
+        self.outer_radius = outer_radius
+        self.inner_radius = inner_radius
+
+    @property
+    def points(self):
+        self.find_points()
+        return self._points
+
+    @points.setter
+    def points(self, points):
+        self._points = points
+
+    @property
+    def height(self):
+        return self._height
+
+    @height.setter
+    def height(self, height):
+        self._height = height
+
+    @property
+    def arc_height(self):
+        return self._arc_height
+
+    @arc_height.setter
+    def arc_height(self, arc_height):
+        self._arc_height = arc_height
+
+    @property
+    def inner_radius(self):
+        return self._inner_radius
+
+    @inner_radius.setter
+    def inner_radius(self, inner_radius):
+        self._inner_radius = inner_radius
+
+    @property
+    def mid_radius(self):
+        return self._mid_radius
+
+    @mid_radius.setter
+    def mid_radius(self, mid_radius):
+        self._mid_radius = mid_radius
+
+    @property
+    def outer_radius(self):
+        return self._outer_radius
+
+    @outer_radius.setter
+    def outer_radius(self, outer_radius):
+        self._outer_radius = outer_radius
+
+    def find_points(self):
+        """Finds the XZ points and connection types (straight and circle) that
+        describe the 2D profile of the center column shield shape."""
+
+        points = [
+            (self.inner_radius, 0, "straight"),
+            (self.inner_radius, self.height / 2, "straight"),
+            (self.outer_radius, self.height / 2, "straight"),
+            (self.outer_radius, self.arc_height / 2, "circle"),
+            (self.mid_radius, 0, "circle"),
+            (self.outer_radius, -self.arc_height / 2, "straight"),
+            (self.outer_radius, -self.height / 2, "straight"),
+            (self.inner_radius, -self.height / 2, "straight"),
+            (self.inner_radius, 0, "straight"),
+        ]
+
+        self.points = points
