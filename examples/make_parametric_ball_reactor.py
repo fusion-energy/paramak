93c837ca661835afcfc700998ed477e01917ea79
=shimwell <mail@jshimwell.com>
2020-07-06 23:34:20 +0100

    added more components to ball reactor

--- examples/make_parametric_ball_reactor.py
+++ examples/make_parametric_ball_reactor.py
@@ -4,7 +4,11 @@ import paramak
 my_reactor = paramak.BallReactor(major_radius=300,
                                  minor_radius=100,
                                  offset_from_plasma=20,
-                                 blanket_thickness=100)
+                                 blanket_thickness=100,
+                                 center_column_shield_outer_radius=180,
+                                 center_column_shield_inner_radius=120,
+                                 number_of_tf_coils=16
+                                 )
 
 my_reactor.export_stp()
 
--- paramak/parametric_reactors/ball_reactor.py
+++ paramak/parametric_reactors/ball_reactor.py
@@ -17,7 +17,11 @@ class BallReactor(Reactor):
         major_radius,
         minor_radius,
         offset_from_plasma,
-        blanket_thickness
+        blanket_thickness,
+        center_column_shield_inner_radius,
+        center_column_shield_outer_radius,
+        number_of_tf_coils,
+        rotation_angle = 180
     ):
 
         super().__init__()
@@ -26,13 +30,19 @@ class BallReactor(Reactor):
         self.minor_radius = minor_radius
         self.offset_from_plasma = offset_from_plasma
         self.blanket_thickness = blanket_thickness
+        self.rotation_angle = rotation_angle
+        self.center_column_shield_inner_radius = center_column_shield_inner_radius
+        self.center_column_shield_outer_radius = center_column_shield_outer_radius
+        self.number_of_tf_coils = number_of_tf_coils
+
         self.create_components()
 
 
     def create_components(self):
 
         plasma = paramak.Plasma(major_radius=self.major_radius,
-                                minor_radius=self.minor_radius,)
+                                minor_radius=self.minor_radius,
+                                rotation_angle=self.rotation_angle)
         plasma.create_solid()
 
         self.add_shape_or_component(plasma)
@@ -46,7 +56,32 @@ class BallReactor(Reactor):
                                plasma.z_point+self.offset_from_plasma),
             inner_lower_point=(plasma.x_point,
                                -(plasma.z_point+self.offset_from_plasma)),
-            thickness=self.blanket_thickness
+            thickness=self.blanket_thickness,
+            rotation_angle=self.rotation_angle
         )
 
         self.add_shape_or_component(blanket)
+
+        # The height of this center column is calculated using CadQuery commands
+        center_column_shield = paramak.CenterColumnShieldCylinder(
+            height=2*(plasma.z_point + self.offset_from_plasma + self.blanket_thickness),
+            inner_radius=self.center_column_shield_inner_radius,
+            outer_radius=self.center_column_shield_outer_radius,
+            rotation_angle=self.rotation_angle,
+            # color=centre_column_color,
+            stp_filename="center_column_shield.stp",
+            material_tag="center_column_material",
+        )
+        self.add_shape_or_component(center_column_shield)
+
+        inboard_tf_coils = paramak.InnerTfCoilsCircular(
+            height=2*(plasma.z_point + self.offset_from_plasma + self.blanket_thickness),
+            outer_radius = self.center_column_shield_inner_radius,
+            inner_radius = 30,
+            number_of_coils = self.number_of_tf_coils,
+            gap_size=10,
+            stp_filename="inboard_tf_coils.stp",
+            material_tag="inboard_tf_coils_material",
+        )
+
+        self.add_shape_or_component(inboard_tf_coils)
