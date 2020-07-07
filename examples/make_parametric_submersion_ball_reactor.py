9db693587c43d969772fdb263dfc522f0b090029
=shimwell <mail@jshimwell.com>
2020-07-07 17:42:47 +0100

    added submersion blanket

---
+++ examples/make_parametric_submersion_ball_reactor.py
@@ -0,0 +1,17 @@
+
+import paramak
+
+my_reactor = paramak.SubmersionBallReactor(major_radius=300,
+                                 minor_radius=100,
+                                 offset_from_plasma=20,
+                                 blanket_thickness=150,
+                                 firstwall_thickness=10,
+                                 center_column_shield_outer_radius=160,
+                                 center_column_shield_inner_radius=100,
+                                 number_of_tf_coils=16,
+                                 casing_thickness=10
+                                 )
+
+my_reactor.export_stp()
+
+my_reactor.export_neutronics_description()
