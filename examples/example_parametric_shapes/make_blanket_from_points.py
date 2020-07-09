1ecbb6cbb0bc2d6cb795a05873e97654f6dc315b
billingsley-john <johnbillingsley1996@yahoo.com>
2020-07-09 11:34:33 +0100

    update examples to only execute if run as script

--- examples/example_parametric_shapes/make_blanket_from_points.py
+++ examples/example_parametric_shapes/make_blanket_from_points.py
@@ -1,17 +1,24 @@
-
-__doc__ = """This python script demonstrates the creation of a breeder blanket from points"""
+"""
+This python script demonstrates the creation of a breeder blanket from points
+"""
 
 from paramak import RotateMixedShape
 
-blanket = RotateMixedShape(
-    points=[
-        (538, 305, "straight"),
-        (538, -305, "straight"),
-        (322, -305, "spline"),
-        (470, 0, "spline"),
-        (322, 305, "straight")
-    ]
-)
+def main():
 
-blanket.rotation_angle = 180
-blanket.export_stp("blanket_from_points.stp")
+    blanket = RotateMixedShape(
+        points=[
+            (538, 305, "straight"),
+            (538, -305, "straight"),
+            (322, -305, "spline"),
+            (470, 0, "spline"),
+            (322, 305, "straight")
+        ]
+    )
+
+    blanket.rotation_angle = 180
+    blanket.export_stp("blanket_from_points.stp")
+
+
+if __name__ == "__main__":
+    main()
--- examples/example_parametric_shapes/make_can_reactor_from_parameters.py
+++ examples/example_parametric_shapes/make_can_reactor_from_parameters.py
@@ -1,124 +1,131 @@
-__doc__ = """This script creates a can shaped reactor with plasma,
-             center column, blanket, firstwall, divertor and core."""
+"""
+This script creates a can shaped reactor with plasma, center column, blanket, firstwall, divertor and core
+"""
 
 import paramak
 
-outer_most_x = 900
-blanket_height = 300
+def main():
 
-plasma = paramak.Plasma()
-plasma.major_radius = 250
-plasma.minor_radius = 100
-plasma.triangularity = 0.5
-plasma.elongation = 2.5
-plasma.rotation_angle = 180
+    outer_most_x = 900
+    blanket_height = 300
 
-centre_column = paramak.RotateMixedShape(
-    points=[
-        (74.6, 687.0, "straight"),
-        (171.0, 687.0, "straight"),
-        (171.0, 459.9232, "spline"),
-        (108.001, 249.9402, "spline"),
-        (92.8995, 0, "spline"),
-        (108.001, -249.9402, "spline"),
-        (171.0, -459.9232, "straight"),
-        (171.0, -687.0, "straight"),
-        (74.6, -687.0, "straight"),
-    ]
-)
-centre_column.stp_filename = "centre_column.stp"
-centre_column.rotation_angle = 180
+    plasma = paramak.Plasma()
+    plasma.major_radius = 250
+    plasma.minor_radius = 100
+    plasma.triangularity = 0.5
+    plasma.elongation = 2.5
+    plasma.rotation_angle = 180
 
-
-blanket = paramak.RotateMixedShape(
-    points=[
-        (325.4528, blanket_height, "straight"),
-        (outer_most_x, blanket_height, "straight"),
-        (outer_most_x, -blanket_height, "straight"),
-        (325.4528, -blanket_height, "spline"),
-        (389.9263, -138.1335, "spline"),
-        (404.5108, 0, "spline"),
-        (389.9263, 138.1335, "spline"),
-    ]
-)
-blanket.stp_filename = "blanket.stp"
-blanket.rotation_angle = 180
+    centre_column = paramak.RotateMixedShape(
+        points=[
+            (74.6, 687.0, "straight"),
+            (171.0, 687.0, "straight"),
+            (171.0, 459.9232, "spline"),
+            (108.001, 249.9402, "spline"),
+            (92.8995, 0, "spline"),
+            (108.001, -249.9402, "spline"),
+            (171.0, -459.9232, "straight"),
+            (171.0, -687.0, "straight"),
+            (74.6, -687.0, "straight"),
+        ]
+    )
+    centre_column.stp_filename = "centre_column.stp"
+    centre_column.rotation_angle = 180
 
 
-firstwall = paramak.RotateMixedShape(
-    points=[
-        (322.9528, blanket_height, "straight"),
-        (325.4528, blanket_height, "spline"),
-        (389.9263, 138.1335, "spline"),
-        (404.5108, 0, "spline"),
-        (389.9263, -138.1335, "spline"),
-        (325.4528, -blanket_height, "straight"),
-        (322.9528, -blanket_height, "spline"),
-        (387.4263, -138.1335, "spline"),
-        (402.0108, 0, "spline"),
-        (387.4263, 138.1335, "spline"),
-    ]
-)
-firstwall.stp_filename = "firstwall.stp"
-firstwall.rotation_angle = 180
+    blanket = paramak.RotateMixedShape(
+        points=[
+            (325.4528, blanket_height, "straight"),
+            (outer_most_x, blanket_height, "straight"),
+            (outer_most_x, -blanket_height, "straight"),
+            (325.4528, -blanket_height, "spline"),
+            (389.9263, -138.1335, "spline"),
+            (404.5108, 0, "spline"),
+            (389.9263, 138.1335, "spline"),
+        ]
+    )
+    blanket.stp_filename = "blanket.stp"
+    blanket.rotation_angle = 180
 
 
-divertor_bottom = paramak.RotateMixedShape(
-    points=[
-        (192.4782, -447.204, "spline"),
-        (272.4957, -370.5, "spline"),
-        (322.9528, -blanket_height, "straight"),
-        (outer_most_x, -blanket_height, "straight"),
-        (outer_most_x, -687.0, "straight"),
-        (171.0, -687.0, "straight"),
-        (171.0, -459.9232, "spline"),
-        (218.8746, -513.3484, "spline"),
-        (362.4986, -602.3905, "straight"),
-        (372.5012, -580.5742, "spline"),
-        (237.48395, -497.21782, "spline"),
-    ]
-)
-divertor_bottom.stp_filename = "divertor_bottom.stp"
-divertor_bottom.rotation_angle = 180
+    firstwall = paramak.RotateMixedShape(
+        points=[
+            (322.9528, blanket_height, "straight"),
+            (325.4528, blanket_height, "spline"),
+            (389.9263, 138.1335, "spline"),
+            (404.5108, 0, "spline"),
+            (389.9263, -138.1335, "spline"),
+            (325.4528, -blanket_height, "straight"),
+            (322.9528, -blanket_height, "spline"),
+            (387.4263, -138.1335, "spline"),
+            (402.0108, 0, "spline"),
+            (387.4263, 138.1335, "spline"),
+        ]
+    )
+    firstwall.stp_filename = "firstwall.stp"
+    firstwall.rotation_angle = 180
 
 
-divertor_top = paramak.RotateMixedShape(
-    points=[
-        (192.4782, 447.204, "spline"),
-        (272.4957, 370.5, "spline"),
-        (322.9528, blanket_height, "straight"),
-        (outer_most_x, blanket_height, "straight"),
-        (outer_most_x, 687.0, "straight"),
-        (171.0, 687.0, "straight"),
-        (171.0, 459.9232, "spline"),
-        (218.8746, 513.3484, "spline"),
-        (362.4986, 602.3905, "straight"),
-        (372.5012, 580.5742, "spline"),
-        (237.48395, 497.21782, "spline"),
-    ]
-)
-divertor_top.stp_filename = "divertor_top.stp"
-divertor_top.rotation_angle = 180
+    divertor_bottom = paramak.RotateMixedShape(
+        points=[
+            (192.4782, -447.204, "spline"),
+            (272.4957, -370.5, "spline"),
+            (322.9528, -blanket_height, "straight"),
+            (outer_most_x, -blanket_height, "straight"),
+            (outer_most_x, -687.0, "straight"),
+            (171.0, -687.0, "straight"),
+            (171.0, -459.9232, "spline"),
+            (218.8746, -513.3484, "spline"),
+            (362.4986, -602.3905, "straight"),
+            (372.5012, -580.5742, "spline"),
+            (237.48395, -497.21782, "spline"),
+        ]
+    )
+    divertor_bottom.stp_filename = "divertor_bottom.stp"
+    divertor_bottom.rotation_angle = 180
 
 
-core = paramak.RotateStraightShape(
-    points=[(0, 687.0), (74.6, 687.0), (74.6, -687.0), (0, -687.0)]
-)
-core.stp_filename = "core.stp"
-core.rotation_angle = 180
+    divertor_top = paramak.RotateMixedShape(
+        points=[
+            (192.4782, 447.204, "spline"),
+            (272.4957, 370.5, "spline"),
+            (322.9528, blanket_height, "straight"),
+            (outer_most_x, blanket_height, "straight"),
+            (outer_most_x, 687.0, "straight"),
+            (171.0, 687.0, "straight"),
+            (171.0, 459.9232, "spline"),
+            (218.8746, 513.3484, "spline"),
+            (362.4986, 602.3905, "straight"),
+            (372.5012, 580.5742, "spline"),
+            (237.48395, 497.21782, "spline"),
+        ]
+    )
+    divertor_top.stp_filename = "divertor_top.stp"
+    divertor_top.rotation_angle = 180
 
 
-# initiates a reactor object
-myreactor = paramak.Reactor()
+    core = paramak.RotateStraightShape(
+        points=[(0, 687.0), (74.6, 687.0), (74.6, -687.0), (0, -687.0)]
+    )
+    core.stp_filename = "core.stp"
+    core.rotation_angle = 180
 
-# adds components to the reactor
-myreactor.add_shape_or_component(plasma)
-myreactor.add_shape_or_component(blanket)
-myreactor.add_shape_or_component(core)
-myreactor.add_shape_or_component(divertor_top)
-myreactor.add_shape_or_component(divertor_bottom)
-myreactor.add_shape_or_component(firstwall)
-myreactor.add_shape_or_component(centre_column)
 
-myreactor.export_stp(output_folder="can_reactor_from_parameters")
-myreactor.export_html(filename="can_reactor_from_parameters/reactor.html")
+    # initiates a reactor object
+    myreactor = paramak.Reactor()
+
+    # adds components to the reactor
+    myreactor.add_shape_or_component(plasma)
+    myreactor.add_shape_or_component(blanket)
+    myreactor.add_shape_or_component(core)
+    myreactor.add_shape_or_component(divertor_top)
+    myreactor.add_shape_or_component(divertor_bottom)
+    myreactor.add_shape_or_component(firstwall)
+    myreactor.add_shape_or_component(centre_column)
+
+    myreactor.export_stp(output_folder="can_reactor_from_parameters")
+    myreactor.export_html(filename="can_reactor_from_parameters/reactor.html")
+
+
+if __name__ == "__main__":
+    main()
