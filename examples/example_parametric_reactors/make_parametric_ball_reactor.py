54080feccd69464ac803ce7225dc1b8f56e3b236
billingsley-john <johnbillingsley1996@yahoo.com>
2020-07-09 11:56:21 +0100

    added conditional script execution

--- examples/example_parametric_reactors/make_parametric_ball_reactor.py
+++ examples/example_parametric_reactors/make_parametric_ball_reactor.py
@@ -1,21 +1,29 @@
+"""
+This example creates a ball reactor using the BallReactor parametric shape
+"""
 
 import paramak
 
-my_reactor = paramak.BallReactor(major_radius=300,
-                                 minor_radius=100,
-                                 offset_from_plasma=20,
-                                 blanket_thickness=100,
-                                 center_column_shield_outer_radius=180,
-                                 center_column_shield_inner_radius=120,
-                                 number_of_tf_coils=16,
-                                 number_of_pf_coils=5,
-                                 pf_coil_height= 20
-                                 pf_coil_width= 20
-                                 pf_case_thickness=10
-                                 
-                                 )
+def main():
+    
+    my_reactor = paramak.BallReactor(major_radius=300,
+                                    minor_radius=100,
+                                    offset_from_plasma=20,
+                                    blanket_thickness=100,
+                                    center_column_shield_outer_radius=180,
+                                    center_column_shield_inner_radius=120,
+                                    number_of_tf_coils=16,
+                                    number_of_pf_coils=5,
+                                    pf_coil_height= 20
+                                    pf_coil_width= 20
+                                    pf_case_thickness=10
+                                    
+                                    )
 
-my_reactor.export_stp()
+    my_reactor.export_stp()
 
-my_reactor.export_neutronics_description()
+    my_reactor.export_neutronics_description()
 
+
+if __name__ == "__main__":
+    main()
