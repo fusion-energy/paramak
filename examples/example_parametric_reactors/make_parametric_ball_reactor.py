1dc63d6f648818efa496023ca1365c6b58a54db9
=shimwell <mail@jshimwell.com>
2020-07-11 10:45:45 +0100

    hot fix reduce major radius

--- examples/example_parametric_reactors/make_parametric_ball_reactor.py
+++ examples/example_parametric_reactors/make_parametric_ball_reactor.py
@@ -6,7 +6,7 @@ import paramak
 
 def main():
 
-    my_reactor = paramak.BallReactor(major_radius=700,
+    my_reactor = paramak.BallReactor(major_radius=300,
                                      minor_radius=100,
                                      offset_from_plasma=20,
                                      blanket_thickness=100,
