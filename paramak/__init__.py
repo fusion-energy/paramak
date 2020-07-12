a96cab34faf3900268322e734c787b617a41a33e
RemiTheWarrior <delaporte.mathurin@laposte.net>
2020-07-12 20:10:49 +0200

    fixed import

--- paramak/__init__.py
+++ paramak/__init__.py
@@ -23,7 +23,7 @@ from .parametric_components.blanket_constant_thickness_arc_v import (
 from .parametric_components.blanket_constant_thickness_fp import (
     BlanketConstantThicknessFP,
 )
-from .parametric_components.blanket_constant_thickness_fp import (
+from .parametric_components.blanket_constant_thickness_plasma import (
     BlanketConstantThicknessPlasma,
 )
 
