c0dde993b47b4536aa8bed40efeece7d37ca4bcc
RemiTheWarrior <delaporte.mathurin@laposte.net>
2020-07-12 20:05:55 +0200

    refactoring + plasma object can be given as argument

--- paramak/parametric_components/blanket_constant_thickness_plasma.py
+++ paramak/parametric_components/blanket_constant_thickness_plasma.py
@@ -46,6 +46,9 @@ class BlanketConstantThicknessPlasma(RotateMixedShape):
         major_radius,
         triangularity,
         elongation,
+        plasma=None,
+        plasma_offset=0,
+        num_points=50,
         workplane="XZ",
         points=None,
         stp_filename=None,
@@ -56,7 +59,7 @@ class BlanketConstantThicknessPlasma(RotateMixedShape):
         name=None,
         material_tag=None,
         cut=None,
-    ):
+            ):
 
         super().__init__(
             points,
@@ -74,10 +77,20 @@ class BlanketConstantThicknessPlasma(RotateMixedShape):
         self.thickness = thickness
         self.start_angle = start_angle
         self.stop_angle = stop_angle
-        self.minor_radius = minor_radius
-        self.major_radius = major_radius
-        self.triangularity = triangularity
-        self.elongation = elongation
+        self.plasma = plasma
+        if plasma is None:
+            self.minor_radius = minor_radius
+            self.major_radius = major_radius
+            self.triangularity = triangularity
+            self.elongation = elongation
+            self.plasma_offset = 0
+        else:  # if plasma object is given, use its parameters
+            self.minor_radius = plasma.minor_radius
+            self.major_radius = plasma.major_radius
+            self.triangularity = plasma.triangularity
+            self.elongation = plasma.elongation
+            self.plasma_offset = plasma_offset
+        self.num_points = num_points
         self.points = points
 
     @property
@@ -107,7 +120,6 @@ class BlanketConstantThicknessPlasma(RotateMixedShape):
 
     def find_points(self):
         conversion_factor = 2*np.pi/360
-        num = 50
 
         def R(theta, pkg=np):
             return self.major_radius + self.minor_radius*pkg.cos(
@@ -115,6 +127,14 @@ class BlanketConstantThicknessPlasma(RotateMixedShape):
 
         def Z(theta, pkg=np):
             return self.elongation*self.minor_radius*pkg.sin(theta)
+        # create array of angles theta
+        thetas = np.linspace(
+                    self.start_angle*conversion_factor,
+                    self.stop_angle*conversion_factor,
+                    num=self.num_points,
+                    endpoint=True)
+
+        # create sympy objects and derivatives
         theta_sp = sp.Symbol("theta")
 
         R_sp = R(theta_sp, pkg=sp)
@@ -122,29 +142,64 @@ class BlanketConstantThicknessPlasma(RotateMixedShape):
 
         R_derivative = sp.diff(R_sp, theta_sp)
         Z_derivative = sp.diff(Z_sp, theta_sp)
-
-        # create array of angles theta
-        thetas = np.linspace(
-                    self.start_angle*conversion_factor,
-                    self.stop_angle*conversion_factor,
-                    num=num,
-                    endpoint=True)
-
         # create inner points
-        inner_points_R = R(thetas)
-        inner_points_Z = Z(thetas)
+        if self.plasma is None:
+            # if no plasma object is given simply use the equation
+            inner_points_R = R(thetas)
+            inner_points_Z = Z(thetas)
 
-        points = [
-            [inner_points_R[i], inner_points_Z[i], 'spline']
-            for i in range(len(thetas))
-            ]
+            points = [
+                [inner_points_R[i], inner_points_Z[i], 'spline']
+                for i in range(len(thetas))
+                ]
+        else:
+            # if a plasma is given
+            points = self.create_offset_points(
+                thetas, R, Z,
+                R_derivative, Z_derivative,
+                self.plasma_offset)
         points[-1][2] = 'straight'
 
         # compute outer points
-        for theta in np.flip(thetas):
+        outer_points = self.create_offset_points(
+            thetas, R, Z,
+            R_derivative, Z_derivative,
+            self.thickness + self.plasma_offset, flip=True)
+        points = points + outer_points
+        points[-2][2] = 'straight'
+        self.points = points
+
+    def create_offset_points(
+            self, thetas, R_fun, Z_fun, R_derivative, Z_derivative, offset,
+            flip=False):
+        """generates a list of points following parametric equations with an
+            offset.
+
+        Args:
+            thetas (list): list of angles (radiants)
+            R_fun (callable): parametric function for R coordinate (cm)
+            Z_fun ([type]): parametric function for Z coordinate (cm)
+            R_derivative (sympy.Mul): derivative of R over theta (cm/rad)
+            Z_derivative (sympy.Mul): derivative of Z over theta (cm/rad)
+            offset (float): offset value (cm). offset=0 will follow the
+                parametric equations.
+            flip (bool, optional): If True thetas will be iterated from the
+                end. Defaults to False.
+
+        Returns:
+            list: list of points
+                [[R1, Z1, connection1], [R2, Z2, connection2], ...]
+        """
+
+        points = []
+        list_of_thetas = thetas
+        if flip:
+            list_of_thetas = np.flip(thetas)
+
+        for theta in list_of_thetas:
             # get local value of derivatives
-            val_R_derivative = float(R_derivative.subs(theta_sp, theta))
-            val_Z_derivative = float(Z_derivative.subs(theta_sp, theta))
+            val_R_derivative = float(R_derivative.subs('theta', theta))
+            val_Z_derivative = float(Z_derivative.subs('theta', theta))
 
             # get normal vector components
             nx = val_Z_derivative
@@ -156,9 +211,8 @@ class BlanketConstantThicknessPlasma(RotateMixedShape):
             ny /= normal_vector_norm
 
             # calculate outer points
-            val_R_outer = R(theta) + self.thickness*nx
-            val_Z_outer = Z(theta) + self.thickness*ny
+            val_R_outer = R_fun(theta) + offset*nx
+            val_Z_outer = Z_fun(theta) + offset*ny
 
             points.append([float(val_R_outer), float(val_Z_outer), 'spline'])
-        points[-2][2] = 'straight'
-        self.points = points
+        return points
