559992c0a4ddb932793377360af37f27185707c9
RemiTheWarrior <delaporte.mathurin@laposte.net>
2020-07-13 11:39:07 +0200

    added a new plasma object based on analytical formula

--- paramak/parametric_components/tokamak_plasma.py
+++ paramak/parametric_components/tokamak_plasma.py
@@ -39,6 +39,7 @@ class Plasma(RotateSplineShape):
         single_null=True,
         triangularity=0.55,
         vertical_displacement=0,
+        num_points=50,
         solid=None,
         stp_filename="plasma.stp",
         color=None,
@@ -70,28 +71,13 @@ class Plasma(RotateSplineShape):
         self.single_null = single_null
         self.triangularity = triangularity
         self.vertical_displacement = vertical_displacement
+        self.num_points = num_points
         # self.color = color   # Inherited by super
         # self.solid = solid   # Inherited by super
         # self.stp_filename = stp_filename   # Inherited by super
         self.points = points
         self.x_point = None
         self.z_point = None
-
-    @property
-    def x_point(self):
-        return self._x_point
-
-    @x_point.setter
-    def x_point(self, value):
-        self._x_point = value
-
-    @property
-    def z_point(self):
-        return self._z_point
-
-    @z_point.setter
-    def z_point(self, value):
-        self._z_point = value
 
     @property
     def points(self):
@@ -165,200 +151,26 @@ class Plasma(RotateSplineShape):
         else:
             self._elongation = elongation
 
-    def find_inside_and_outside_arc_positions(self):
-        """Finds the X axis value of the center point of the two arcs that make
-        up the plasma.
-
-        :return: the X postion of the ouside arc and the X position of the inside arc
-        :rtype: floats
-        """
-        # x position of outside arc
-        x_position_for_outside_arc = (
-            2 * self.major_radius * (1 + self.triangularity)
-            - self.minor_radius * (self.triangularity ** 2 + self.elongation ** 2 - 1)
-        ) / (2 * (1 + self.triangularity))
-
-        # x position of inside arc
-        x_position_for_inside_arc = (
-            2 * self.major_radius * (self.triangularity - 1)
-            - self.minor_radius * (self.triangularity ** 2 + self.elongation ** 2 - 1)
-        ) / (2 * (self.triangularity - 1))
-
-        self.x_position_for_outside_arc = x_position_for_outside_arc
-        self.x_position_for_inside_arc = x_position_for_inside_arc
-
-        return x_position_for_outside_arc, x_position_for_inside_arc
-
-    def find_inside_outside_radius(self):
-        """Finds the radius of the the two arcs used to create the plasma surfaces.
-
-        :return: the radius of the ouside arc and the radius of the inside arc
-        :rtype: floats
-        """
-
-        # Radius of outside arc
-        radius_of_outside_arc = 0.5 * scipy.sqrt(
-            (
-                self.minor_radius ** 2
-                * ((self.triangularity + 1) ** 2 + self.elongation ** 2) ** 2
-            )
-            / ((self.triangularity + 1) ** 2)
-        )
-        # Radius of inside arc
-        radius_of_inside_arc = 0.5 * scipy.sqrt(
-            (
-                self.minor_radius ** 2
-                * ((self.triangularity - 1) ** 2 + self.elongation ** 2) ** 2
-            )
-            / ((self.triangularity - 1) ** 2)
-        )
-
-        self.radius_of_outside_arc = radius_of_outside_arc
-        self.radius_of_inside_arc = radius_of_inside_arc
-
-        return radius_of_outside_arc, radius_of_inside_arc
-
-    def find_x_point(self):
-        """Finds the point on the X axis where the inner arc and outer arc meet
-
-        :return: the X value of the intersection
-        :rtype: float
-        """
-
-        # uses the cosin rule with the inner angle set to C
-
-        width_of_base = self.x_position_for_inside_arc - self.x_position_for_outside_arc
-
-        angle_of_C = math.acos(
-            (
-                (
-                    math.pow(self.radius_of_inside_arc, 2)
-                    - (
-                        math.pow(self.radius_of_outside_arc, 2)
-                        + math.pow(width_of_base, 2)
-                    )
-                )
-                / -(2 * self.radius_of_outside_arc * width_of_base)
-            )
-        )
-
-        partial_width_of_base = math.cos(angle_of_C) * self.radius_of_outside_arc
-
-        x_point = self.x_position_for_outside_arc + partial_width_of_base
-
-        self.x_point = x_point
-
-        return x_point
-
-    def find_z_point(self):
-        """Finds the point on the Z axis where the inner arc and outer
-        arc meet. This useful when finding the height of the plasma
-
-        :return: the Z value of the intersection
-        :rtype: float
-        """
-
-        length_of_triangle_base = self.x_point - self.x_position_for_inside_arc
-
-        # hypotenuse of triange is self.radius_of_inside_arc
-
-        # trigonometry
-        z_point = math.pow( math.pow(self.radius_of_inside_arc,2) - math.pow(length_of_triangle_base,2), 0.5 )
-
-        self.z_point = z_point
-
-        return z_point
-
-
-    def find_points_on_outer_arc(self):
-        """Finds the XZ points on the outer arc of the plasma
-
-        :return: the XZ points that make up the outer arc
-        :rtype: float
-        """
-
-        intercept_angle = math.acos(
-            (
-                (-self.x_point + self.x_position_for_outside_arc)
-                / self.radius_of_outside_arc
-            )
-        )
-
-        angles = np.linspace(
-            intercept_angle, 2 * math.pi - intercept_angle, 50, endpoint=False
-        )
-
-        xs = -(
-            self.radius_of_outside_arc * scipy.cos(angles)
-            - self.x_position_for_outside_arc
-        )
-        zs = self.radius_of_outside_arc * scipy.sin(angles) - self.vertical_displacement
-
-        self.xs_outer_arc = xs
-        self.zs_outer_arc = zs
-
-        return xs, zs
-
-    def find_points_on_inner_arc(self):
-        """Finds the XZ points on the inner arc of the plasma
-
-        :return: the XZ points that make up the inner arc
-        :rtype: float
-        """
-
-        intercept_angle = math.acos(
-            (
-                (-self.x_point + self.x_position_for_inside_arc)
-                / self.radius_of_inside_arc
-            )
-        )
-
-        lower_angles = np.linspace(
-            2 * math.pi - intercept_angle, math.radians(360), 25, endpoint=False
-        )
-        upper_angles = np.linspace(math.radians(0), intercept_angle, 25)
-        angles = np.concatenate((lower_angles, upper_angles), axis=0)
-
-        xs = -(
-            self.radius_of_inside_arc * scipy.cos(angles)
-            - self.x_position_for_inside_arc
-        )
-        zs = self.radius_of_inside_arc * scipy.sin(angles) - self.vertical_displacement
-
-        self.xs_inner_arc = xs
-        self.zs_inner_arc = zs
-
-        return xs, zs
-
     def find_points(self):
-        """Finds the XZ points that describe the 2D profile of the blanket shape.
-
-        :return: the XZ points that make up the outer arc
-        :rtype: list of tuples each with two floats
+        """Finds the XZ points that describe the 2D profile of the plasma.
         """
 
-        self.find_inside_and_outside_arc_positions()
+        # create array of angles theta
+        theta = np.linspace(0, 2*np.pi, num=self.num_points)
 
-        self.find_inside_outside_radius()
+        # parametric equations for plasma
+        def R(theta):
+            return self.major_radius + self.minor_radius*np.cos(
+                theta + self.triangularity*np.sin(theta))
 
-        self.find_x_point()
+        def Z(theta):
+            return self.elongation*self.minor_radius*np.sin(theta)
 
-        self.find_z_point() # optional but useful for finding height
+        # R and Z coordinates
+        R_points, Z_points = R(theta), Z(theta)
 
-        self.find_points_on_outer_arc()
+        # create a 2D array for points coordinates
+        points = np.stack((R_points, Z_points), axis=1)
 
-        self.find_points_on_inner_arc()
-
-        points = []
-
-        for x, z, in zip(self.xs_outer_arc, self.zs_outer_arc):
-            points.append([x, z])
-
-        for x, z, in zip(self.xs_inner_arc, self.zs_inner_arc):
-            points.append([x, z])
-
-        # points.append([self.xs_outer_arc[0], self.zs_outer_arc[0]])
-
-        self.points = points
-
-        return points
+        # set self.points
+        self.points = list(points)
