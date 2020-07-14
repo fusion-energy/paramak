cb4e38f4d224887c10fb68e6e7d8218756a674a6
Jonathan Shimwell <jonathan.shimwell@ukaea.uk>
2020-07-14 21:42:12 +0100

    adding svg to gitignore

---
+++ docs/source/paramak.parametric_reactors.rst
@@ -0,0 +1,11 @@
+Parametric Reactors
+===================
+
+These are reactors designs that can be created from parameters
+
+BallReactor()
+^^^^^^^^^^^^^
+
+.. automodule:: paramak.parametric_reactors.ball_reactor
+   :members:
+   :show-inheritance:
--- examples/example_parametric_reactors/make_parametric_ball_reactor.py
+++ examples/example_parametric_reactors/make_parametric_ball_reactor.py
@@ -7,18 +7,22 @@ import paramak
 def main():
 
     my_reactor = paramak.BallReactor(major_radius=300,
-                                     minor_radius=100,
-                                     offset_from_plasma=20,
-                                     blanket_thickness=100,
-                                     center_column_shield_outer_radius=180,
-                                     center_column_shield_inner_radius=120,
-                                     number_of_tf_coils=16,
-                                     divertor_width=100
+                                    minor_radius=100,
+                                    elongation=2,
+                                    triangularity=0.55,
+                                    offset_from_plasma=20,
+                                    blanket_thickness=100,
+                                    center_column_shield_outer_radius=180,
+                                    center_column_shield_inner_radius=120,
+                                    number_of_tf_coils=16,
+                                    divertor_width=100
+    )
+                                     
                                      # number_of_pf_coils=5,
                                      # pf_coil_height=20,
                                      # pf_coil_width=20,
                                      # pf_case_thickness=10,
-                                    )
+                                    # )
 
     my_reactor.export_stp()
 
