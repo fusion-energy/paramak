91f9ae7039cc7fc517dd908b8ab6e6192f7b6819
=shimwell <mail@jshimwell.com>
2020-07-25 20:46:51 +0100

    changed geometry parameters

--- examples/example_parametric_reactors/make_parametric_ball_reactor.py
+++ examples/example_parametric_reactors/make_parametric_ball_reactor.py
@@ -7,26 +7,27 @@ import paramak
 def main():
 
 
-    my_reactor = paramak.BallReactor(
-                                    inner_bore_radial_thickness=50,
-                                    inboard_tf_leg_radial_thickness = 200,
-                                    center_column_shield_radial_thickness= 50,
-                                    divertor_radial_thickness = 50,
-                                    inner_plasma_gap_radial_thickness = 150,
-                                    plasma_radial_thickness = 100,
-                                    outer_plasma_gap_radial_thickness = 50,
-                                    firstwall_radial_thickness=50,
-                                    blanket_radial_thickness=100,
-                                    blanket_rear_wall_radial_thickness=10,
-                                    elongation=2,
-                                    triangularity=0.55,
-                                    number_of_tf_coils=16,
-                                    rotation_angle=180
-    )
+    for blanket_radial_thickness in [10, 75, 150, 300]:
 
-    my_reactor.export_stp()
+        my_reactor = paramak.BallReactor(
+                                            inner_bore_radial_thickness=1,
+                                            inboard_tf_leg_radial_thickness = 30,
+                                            center_column_shield_radial_thickness= 60,
+                                            divertor_radial_thickness=50,
+                                            inner_plasma_gap_radial_thickness = 30,
+                                            plasma_radial_thickness = 300,
+                                            outer_plasma_gap_radial_thickness = 30,
+                                            firstwall_radial_thickness=3,
+                                            blanket_radial_thickness=blanket_radial_thickness,
+                                            blanket_rear_wall_radial_thickness=3,
+                                            elongation=2.75,
+                                            triangularity=0.5,
+                                            number_of_tf_coils=16,
+                                            rotation_angle=180)
 
-    my_reactor.export_neutronics_description()
+        my_reactor.export_stp(str(blanket_radial_thickness))
+
+        # my_reactor.export_neutronics_description()
 
 
 if __name__ == "__main__":
