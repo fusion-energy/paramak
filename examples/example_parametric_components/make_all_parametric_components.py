496bf53a3c7a85852d28a89b6f1f0f5283c33623
billingsley-john <johnbillingsley1996@yahoo.com>
2020-07-28 10:57:39 +0100

    added ITER divertor to examples

--- examples/example_parametric_components/make_all_parametric_components.py
+++ examples/example_parametric_components/make_all_parametric_components.py
@@ -214,6 +214,12 @@ def main():
                 number_of_coils=6)
     all_components.append(component)
 
+    component = paramak.ITERtypeDivertor(
+        rotation_angle=rot_angle,
+        stp_filename='ITER_type_divertor.stp'
+    )
+    all_components.append(component)
+
     return all_components
 
 
