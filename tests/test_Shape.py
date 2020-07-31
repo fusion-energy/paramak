03ea7ced58e1d8542f2081cd667dd87d1eb66043
RemiTheWarrior <delaporte.mathurin@laposte.net>
2020-07-31 11:13:04 +0200

    removed most of the moved tests

--- tests/test_Shape.py
+++ tests/test_Shape.py
@@ -4,7 +4,6 @@ import unittest
 from pathlib import Path
 
 import pytest
-
 from paramak import Shape
 
 # test command
@@ -77,52 +76,6 @@ class test_object_properties(unittest.TestCase):
         assert Path("filename.png").exists() is True
         os.system("rm filename.png")
 
-    def test_export_3d_image(self):
-        """checks that export_3d_image() exports png files with the \
-                correct suffix"""
-
-        test_shape = Shape()
-        test_shape.points = [(0, 0), (0, 20), (20, 20), (20, 0)]
-        os.system("rm filename.png")
-        test_shape.export_2d_image("filename")
-        assert Path("filename.png").exists() is True
-        os.system("rm filename.png")
-        test_shape.export_2d_image("filename.png")
-        assert Path("filename.png").exists() is True
-        os.system("rm filename.png")
-
-    def test_export_html_filename(self):
-        """checks that export_html() exports html files with the \
-                correct suffix"""
-
-        test_shape = Shape()
-        test_shape.points = [(0, 0), (0, 20), (20, 20), (20, 0)]
-        os.system("rm filename.html")
-        test_shape.export_html("filename")
-        assert Path("filename.html").exists() is True
-        os.system("rm filename.html")
-        test_shape.export_html("filename.html")
-        assert Path("filename.html").exists() is True
-        os.system("rm filename.html")
-
-    def test_export_stp(self):
-
-        test_shape = Shape()
-        test_shape.points = [(0, 0), (0, 20), (20, 20), (20, 0)]
-        test_shape.export_stp("tests/test.stp")
-        test_shape.export_stp()
-
-    def test_export_stl(self):
-
-        test_shape = Shape()
-        test_shape.points = [(0, 0), (0, 20), (20, 20), (20, 0)]
-        test_shape.export_stl("tests/test.stl")
-
-    def test_create_render_mesh(self):
-
-        test_shape = Shape()
-        test_shape.points = [(0, 0), (0, 20), (20, 20), (20, 0)]
-        test_shape._create_render_mesh()
 
 if __name__ == "__main__":
     unittest.main()
