e345c6353869ed5962490283269ef0730e95dc85
Jonathan Shimwell <jonathan.shimwell@ukaea>
2020-09-02 15:06:50 +0100

    trigger doc rebuild

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ sys.path.insert(0, os.path.abspath("../../examples"))
 # -- Project information -----------------------------------------------------
 
 project = "Paramak"
-copyright = "2020, John Billingsley"
+copyright = "2020, UKAEA"
 author = "John Billingsley"
 
 # The short X.Y version
