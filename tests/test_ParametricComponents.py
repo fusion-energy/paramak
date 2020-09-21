42d3fce1936c47d667eecb6c4104fa13cfc97d5f
Jonathan Shimwell <jonathan.shimwell@ukaea>
2020-09-21 20:07:05 +0100

    fixed test

--- tests/test_ParametricComponents.py
+++ tests/test_ParametricComponents.py
@@ -8,12 +8,19 @@ import paramak
 
 
 class test_PoloidalSegments(unittest.TestCase):
-    def test_PoloidalSegments_makes_correct_number_of_solids(self):
-        """creates a rotated ring and segments it into poloidal sections"""
+    def test_PoloidalSegments_solid_count(self):
+        """creates a rotated hollow ring and segments it into poloidal sections"""
 
-        test_shape_to_segment = paramak.PoloidalFieldCoil(height=100,
-                                                          width=100,
-                                                          center_point=(500, 500))
+        pf_coil = paramak.PoloidalFieldCoil(
+            height=100,
+            width=100,
+            center_point=(500, 500)
+        )
+
+        test_shape_to_segment = paramak.PoloidalFieldCoilCaseFC(
+            pf_coil=pf_coil,
+            casing_thickness=100
+        )
 
         test_shape = paramak.PoloidalSegments(
             shape_to_segment=test_shape_to_segment,
@@ -22,7 +29,25 @@ class test_PoloidalSegments(unittest.TestCase):
         )
 
         assert test_shape.solid is not None
-        assert len(test_shape.Solids()) == 22
+        assert len(test_shape.solid.Solids()) == 22
+
+    def test_PoloidalSegments_solid_count2(self):
+        """creates a rotated ring and segments it into poloidal sections"""
+
+        test_shape_to_segment = paramak.PoloidalFieldCoil(
+            height=100,
+            width=100,
+            center_point=(500, 500)
+        )
+
+        test_shape = paramak.PoloidalSegments(
+            shape_to_segment=test_shape_to_segment,
+            center_point=(500, 500),
+            number_of_segments=22,
+        )
+
+        assert test_shape.solid is not None
+        assert len(test_shape.solid.Solids()) == 22
 
 
 class test_BlanketConstantThicknessArcV(unittest.TestCase):
