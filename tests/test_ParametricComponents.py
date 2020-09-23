591664c5e30bc0780d9e371be8738f2cbb946303
RemiTheWarrior <delaporte.mathurin@laposte.net>
2020-09-23 11:08:18 +0200

    test the creation of the leg

--- tests/test_ParametricComponents.py
+++ tests/test_ParametricComponents.py
@@ -748,6 +748,17 @@ class test_ToroidalFieldCoilPrincetonD(unittest.TestCase):
         assert test_shape.solid is not None
         assert test_shape.volume > 1000
 
+    def test_ToroidalFieldCoilPrincetonD_with_leg(self):
+        """creates a ToroidalFieldCoilPrincetonD object and checks a leg can
+        be created"""
+
+        my_magnet = paramak.ToroidalFieldCoilPrincetonD(R1=0.29, R2=0.91, thickness=0.05, distance=0.05, number_of_coils=1)
+        my_magnet.export_stp('princeton.stp')
+
+        my_leg = paramak.ExtrudeStraightShape(points=my_magnet.inner_leg_connection_points, distance=0.05)
+
+        assert my_leg.solid is not None
+
 
 class test_CenterColumnShieldCircular(unittest.TestCase):
     def test_CenterColumnShieldCircular_creation(self):
