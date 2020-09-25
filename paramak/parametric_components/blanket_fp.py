e6094c2caf12a88353901b8694c4583b41216177
Jonathan Shimwell <jonathan.shimwell@ukaea>
2020-09-25 19:30:11 +0100

    updated blanketfp docstring

--- paramak/parametric_components/blanket_fp.py
+++ paramak/parametric_components/blanket_fp.py
@@ -42,10 +42,12 @@ class BlanketFP(RotateMixedShape):
         Others: see paramak.RotateMixedShape() arguments.
 
     Keyword Args:
-        thickness (float, (float, float), callable): the thickness of the
-            blanket (cm). If float, constant thickness. If tuple of floats,
-            thickness will vary linearly between the two values. If callable,
-            thickness will be a function of poloidal angle (in degrees).
+        thickness (float or [float] or callable or [(float), (float)]): 
+            the thickness of the blanket (cm). If float, constant thickness.
+            If tuple of floats, thickness will vary linearly between the two
+            values. If callable, thickness will be a function of poloidal
+            angle (in degrees). If a list of of two lists (thicknesses and angles)
+            then these will be used together with linear interpolation.
         start_angle (float): the angle in degrees to start the blanket,
             measured anti clockwise from 3 o'clock
         stop_angle (float): the angle in degrees to stop the blanket, measured
@@ -59,11 +61,12 @@ class BlanketFP(RotateMixedShape):
             Defaults to 2.0.
         vertical_displacement (float): the vertical_displacement of
             the plasma (cm).
-        offset_from_plasma (float, (float, float), callable): the distance
-            bettwen the plasma and the blanket (cm). If float, constant offset.
-            If tuple of floats, offset will vary linearly between the two
+        offset_from_plasma (float or [float] or callable or [(float), (float)]): 
+            the distance bettwen the plasma and the blanket (cm). If float,
+            constant offset. If list of floats, offset will vary linearly between the
             values. If callable, offset will be a function of poloidal angle
-            (in degrees) Defaults to 0.
+            (in degrees) Defaults to 0. If a list of of two lists (offsets and angles)
+            then these will be used together with linear interpolation.
         num_points (int): number of points that will describe the
             shape.
         Others: see paramak.RotateMixedShape() attributes.
