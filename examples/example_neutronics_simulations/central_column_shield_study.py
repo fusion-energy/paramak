6ed053dc697e936fb8cddc60666ed382610ae326
autopep8 <autopep8@users.noreply.github.com>
2020-09-26 09:56:27 +0000

    Automated autopep8 fixes

--- examples/example_neutronics_simulations/central_column_shield_study.py
+++ examples/example_neutronics_simulations/central_column_shield_study.py
@@ -25,14 +25,14 @@ def make_cad_model_with_paramak(params):
     divertor_radial_thickness = params[6]
     plasma_radial_thickness = params[7]
     outer_plasma_gap_radial_thickness = params[8]
-    plasma_gap_vertical_thickness= params[9]
-    center_column_arc_vertical_thickness= params[10]
+    plasma_gap_vertical_thickness = params[9]
+    center_column_arc_vertical_thickness = params[10]
 
     plasma_high_point_y = params[11]
-    
+
     # plasma_high_point_x_factor is param[12]
     plasma_high_point_x = (params[7] * params[12]) + sum(params[0:6])
-    plasma_high_point=(plasma_high_point_x, plasma_high_point_y) 
+    plasma_high_point = (plasma_high_point_x, plasma_high_point_y)
 
     print(params)
 
@@ -50,25 +50,25 @@ def make_cad_model_with_paramak(params):
         center_column_arc_vertical_thickness=center_column_arc_vertical_thickness,
         plasma_high_point=plasma_high_point,
         rotation_angle=180)
-    simulation_id =str(uuid.uuid4())
-    test_reactor.export_svg(simulation_id +'.png')
-    test_reactor.export_stp(output_folder= simulation_id)
+    simulation_id = str(uuid.uuid4())
+    test_reactor.export_svg(simulation_id + '.png')
+    test_reactor.export_stp(output_folder=simulation_id)
 
     input_and_output_data = {
-        "inner_bore_radial_thickness":inner_bore_radial_thickness,
-        "inboard_tf_leg_radial_thickness":inboard_tf_leg_radial_thickness,
-        "center_column_shield_radial_thickness_mid":center_column_shield_radial_thickness_mid,
-        "center_column_shield_radial_thickness_upper":center_column_shield_radial_thickness_upper,
-        "inboard_firstwall_radial_thickness":inboard_firstwall_radial_thickness,
-        "inner_plasma_gap_radial_thickness":inner_plasma_gap_radial_thickness,
-        "divertor_radial_thickness":divertor_radial_thickness,
-        "plasma_radial_thickness":plasma_radial_thickness,
-        "outer_plasma_gap_radial_thickness":outer_plasma_gap_radial_thickness,
-        "plasma_gap_vertical_thickness":plasma_gap_vertical_thickness,
-        "center_column_arc_vertical_thickness":center_column_arc_vertical_thickness,
-        "plasma_high_point_x":plasma_high_point[0],
-        "plasma_high_point_y":plasma_high_point[1],
-        }
+        "inner_bore_radial_thickness": inner_bore_radial_thickness,
+        "inboard_tf_leg_radial_thickness": inboard_tf_leg_radial_thickness,
+        "center_column_shield_radial_thickness_mid": center_column_shield_radial_thickness_mid,
+        "center_column_shield_radial_thickness_upper": center_column_shield_radial_thickness_upper,
+        "inboard_firstwall_radial_thickness": inboard_firstwall_radial_thickness,
+        "inner_plasma_gap_radial_thickness": inner_plasma_gap_radial_thickness,
+        "divertor_radial_thickness": divertor_radial_thickness,
+        "plasma_radial_thickness": plasma_radial_thickness,
+        "outer_plasma_gap_radial_thickness": outer_plasma_gap_radial_thickness,
+        "plasma_gap_vertical_thickness": plasma_gap_vertical_thickness,
+        "center_column_arc_vertical_thickness": center_column_arc_vertical_thickness,
+        "plasma_high_point_x": plasma_high_point[0],
+        "plasma_high_point_y": plasma_high_point[1],
+    }
 
     with open(simulation_id + ".json", 'w') as outfile:
         json.dump(input_and_output_data, outfile, indent=4)
@@ -76,23 +76,23 @@ def make_cad_model_with_paramak(params):
     return 1.
 
 
-
 if __name__ == "__main__":
     res = dummy_minimize(make_cad_model_with_paramak,
-                    [
-                        (10., 50.),  # inner_bore_radial_thickness 
-                        (50., 100.),  # inboard_tf_leg_radial_thickness 
-                        (10., 150.),  # center_column_shield_radial_thickness_mid 
-                        (1., 1.5),  # center_column_shield_radial_thickness_upper 
-                        (2., 10.),  # inboard_firstwall_radial_thickness 
-                        (2., 20.),  # inner_plasma_gap_radial_thickness 
-                        (20., 50.),  # divertor_radial_thickness 
-                        (100., 300.),  # plasma_radial_thickness 
-                        (2., 20.),  # outer_plasma_gap_radial_thickness 
-                        (2., 20.),  # plasma_gap_vertical_thickness
-                        (200., 500.),  # center_column_arc_vertical_thickness
-                        (200., 500.),  # plasma_high_point_y
-                        (0.,1.),  # plasma_high_point_x_factor
-                    ],
-                    n_calls = 20 # this can be increased to perform more samples
-                    )
+                         [
+                             (10., 50.),  # inner_bore_radial_thickness
+                             (50., 100.),  # inboard_tf_leg_radial_thickness
+                             # center_column_shield_radial_thickness_mid
+                             (10., 150.),
+                             (1., 1.5),  # center_column_shield_radial_thickness_upper
+                             (2., 10.),  # inboard_firstwall_radial_thickness
+                             (2., 20.),  # inner_plasma_gap_radial_thickness
+                             (20., 50.),  # divertor_radial_thickness
+                             (100., 300.),  # plasma_radial_thickness
+                             (2., 20.),  # outer_plasma_gap_radial_thickness
+                             (2., 20.),  # plasma_gap_vertical_thickness
+                             (200., 500.),  # center_column_arc_vertical_thickness
+                             (200., 500.),  # plasma_high_point_y
+                             (0., 1.),  # plasma_high_point_x_factor
+                         ],
+                         n_calls=20  # this can be increased to perform more samples
+                         )
