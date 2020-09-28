f044a02e18cb9b2ee78d3ce2326f31742aec6fb6
billingsley-john <johnbillingsley1996@yahoo.com>
2020-09-28 13:26:02 +0100

    added azimuth start angle test

--- tests/test_ParametricComponents.py
+++ tests/test_ParametricComponents.py
@@ -1098,6 +1098,22 @@ class test_InnerTfCoilsFlat(unittest.TestCase):
         assert test_shape.solid is not None
         assert test_shape.volume > 1000
 
+    def test_InnerTfCoilsFlat_azimuth_offset(self):
+        """creates an inner tf coil using the InnerTfCoilsFlat parametric component and checks
+        that the azimuthal start angle can be changed correctly"""
+
+        test_shape = paramak.InnerTfCoilsFlat(
+            height=500,
+            inner_radius=50,
+            outer_radius=150,
+            number_of_coils=6,
+            gap_size=5
+        )
+
+        assert test_shape.azimuth_placement_angle == [0, 60, 120, 180, 240, 300]
+        test_shape.azimuth_start_angle = 20
+        assert test_shape.azimuth_placement_angle == [20, 80, 140, 200, 260, 320]
+
 
 class test_InnerTfCoilsCircular(unittest.TestCase):
     def test_InnerTfCoilsCircular_creation(self):
@@ -1114,6 +1130,22 @@ class test_InnerTfCoilsCircular(unittest.TestCase):
 
         assert test_shape.solid is not None
         assert test_shape.volume > 1000
+
+    def test_InnerTfCoilsCircular_azimuth_offset(self):
+        """creates an inner tf coil using the InnerTfCoilsCircular parametric component and checks
+        that the azimuthal start angle can be changed correctly"""
+
+        test_shape = paramak.InnerTfCoilsCircular(
+            height=500,
+            inner_radius=50,
+            outer_radius=150,
+            number_of_coils=6,
+            gap_size=5
+        )
+
+        assert test_shape.azimuth_placement_angle == [0, 60, 120, 180, 240, 300]
+        test_shape.azimuth_start_angle = 20
+        assert test_shape.azimuth_placement_angle == [20, 80, 140, 200, 260, 320]
 
 
 class test_ParametricComponents(unittest.TestCase):
