a093c182357c26340e37cd02a0230e22dc615263
autopep8 <autopep8@users.noreply.github.com>
2020-09-30 22:19:10 +0000

    Automated autopep8 fixes

--- tests/test_Shape.py
+++ tests/test_Shape.py
@@ -4,6 +4,7 @@ from pathlib import Path
 
 import paramak
 
+
 class test_object_properties(unittest.TestCase):
     def test_shape_default_properties(self):
         """creates a Shape object and checks that the points attribute has
