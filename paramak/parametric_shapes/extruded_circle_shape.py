f72a4d4565dbeb7df5555904533c96de4da01c40
billingsley-john <johnbillingsley1996@yahoo.com>
2020-10-01 17:44:25 +0100

    changed azimuthal placement angle sense

--- paramak/parametric_shapes/extruded_circle_shape.py
+++ paramak/parametric_shapes/extruded_circle_shape.py
@@ -139,7 +139,7 @@ class ExtrudeCircleShape(Shape):
         else:
             # Peform rotations for a single azimuth_placement_angle angle
             solid = solid.rotate(
-                (0, 0, 1), (0, 0, -1), self.azimuth_placement_angle)
+                (0, 0, -1), (0, 0, 1), self.azimuth_placement_angle)
 
         self.perform_boolean_operations(solid)
 
--- paramak/parametric_shapes/extruded_mixed_shape.py
+++ paramak/parametric_shapes/extruded_mixed_shape.py
@@ -162,7 +162,7 @@ class ExtrudeMixedShape(Shape):
         else:
             # Peform rotations for a single azimuth_placement_angle angle
             solid = solid.rotate(
-                (0, 0, 1), (0, 0, -1), self.azimuth_placement_angle)
+                (0, 0, -1), (0, 0, 1), self.azimuth_placement_angle)
 
         self.perform_boolean_operations(solid)
 
