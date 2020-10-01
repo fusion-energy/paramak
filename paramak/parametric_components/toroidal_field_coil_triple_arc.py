bea2ce28f3ead7f0e9d982e770bd84cd26245f40
Jonathan Shimwell <jonathan.shimwel@ukaea.uk>
2020-10-01 14:11:02 +0100

    added create solid

--- paramak/parametric_components/toroidal_field_coil_triple_arc.py
+++ paramak/parametric_components/toroidal_field_coil_triple_arc.py
@@ -177,6 +177,20 @@ class ToroidalFieldCoilTripleArc(ExtrudeMixedShape):
         points[-1][2] = 'straight'
         self.points = points
 
+        # extract helping points for inner leg
+        inner_leg_connection_points = []
+
+        inner_leg_connection_points.append(
+            (R_inner[0], Z_inner[0]))
+        inner_leg_connection_points.append(
+            (R_inner[-1], Z_inner[-1]))
+        inner_leg_connection_points.append(
+            (R_outer[0], Z_outer[0]))
+        inner_leg_connection_points.append(
+            (R_outer[-1], Z_outer[-1]))
+
+        self.inner_leg_connection_points = inner_leg_connection_points
+
     def find_azimuth_placement_angle(self):
         """Calculates the azimuth placement angles based on the number of tf coils"""
 
@@ -188,3 +202,81 @@ class ToroidalFieldCoilTripleArc(ExtrudeMixedShape):
                 endpoint=False))
 
         self.azimuth_placement_angle = angles
+
+    def create_solid(self):
+        """Creates a 3d solid using points with straight and spline
+        connections edges, azimuth_placement_angle and distance.
+
+        :return: a 3d solid volume
+        :rtype: a cadquery solid
+        """
+
+        # obtains the first two values of the points list
+        XZ_points = [(p[0], p[1]) for p in self.points]
+
+        # obtains the last values of the points list
+        connections = [p[2] for p in self.points[:-1]]
+
+        current_linetype = connections[0]
+        current_points_list = []
+        instructions = []
+        # groups together common connection types
+        for i, c in enumerate(connections):
+            if c == current_linetype:
+                current_points_list.append(XZ_points[i])
+            else:
+                current_points_list.append(XZ_points[i])
+                instructions.append({current_linetype: current_points_list})
+                current_linetype = c
+                current_points_list = [XZ_points[i]]
+        instructions.append({current_linetype: current_points_list})
+
+        if list(instructions[-1].values())[0][-1] != XZ_points[0]:
+            keyname = list(instructions[-1].keys())[0]
+            instructions[-1][keyname].append(XZ_points[0])
+
+        solid = cq.Workplane(self.workplane)
+        solid.moveTo(XZ_points[0][0], XZ_points[0][1])
+
+        for entry in instructions:
+            if list(entry.keys())[0] == "spline":
+                solid = solid.spline(listOfXYTuple=list(entry.values())[0])
+            if list(entry.keys())[0] == "straight":
+                solid = solid.polyline(list(entry.values())[0])
+
+        # performs extrude in both directions, hence distance / 2
+        solid = solid.close().extrude(distance=-self.distance / 2.0, both=True)
+
+        if self.with_inner_leg is True:
+            inner_leg_solid = cq.Workplane(self.workplane)
+            inner_leg_solid.moveTo(XZ_points[0][0], XZ_points[0][1])
+            inner_leg_solid = inner_leg_solid.polyline(
+                self.inner_leg_connection_points)
+            inner_leg_solid = inner_leg_solid.close().extrude(
+                distance=-self.distance / 2.0, both=True)
+
+            solid = cq.Compound.makeCompound(
+                [a.val() for a in [inner_leg_solid, solid]]
+            )
+
+        # Checks if the azimuth_placement_angle is a list of angles
+        if isinstance(self.azimuth_placement_angle, Iterable):
+            rotated_solids = []
+            # Perform seperate rotations for each angle
+            for angle in self.azimuth_placement_angle:
+                rotated_solids.append(
+                    solid.rotate(
+                        (0, 0, -1), (0, 0, 1), angle))
+            solid = cq.Workplane(self.workplane)
+
+            # Joins the seperate solids together
+            for i in rotated_solids:
+                solid = solid.union(i)
+        else:
+            # Peform rotations for a single azimuth_placement_angle angle
+            solid = solid.rotate(
+                (0, 0, 1), (0, 0, -1), self.azimuth_placement_angle)
+
+        self.perform_boolean_operations(solid)
+
+        return solid
