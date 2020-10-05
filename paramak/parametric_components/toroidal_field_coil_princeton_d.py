721fe398da283c039f8fb96cc2f1b685e7da9090
RemiTheWarrior <delaporte.mathurin@laposte.net>
2020-10-05 15:44:47 +0200

    replaced the overwriting of create_solid

--- paramak/parametric_components/toroidal_field_coil_princeton_d.py
+++ paramak/parametric_components/toroidal_field_coil_princeton_d.py
@@ -177,33 +177,44 @@ class ToroidalFieldCoilPrincetonD(ExtrudeMixedShape):
         # compute inner and outer points
         R, Z, dz_dr = self.compute_inner_points(self.R1, self.R2)
         R_, Z_ = self.compute_outer_points(R, Z, self.thickness, dz_dr)
+        R_, Z_ = np.flip(R_), np.flip(Z_)
+        # extract helping points for inner leg
+        inner_leg_connection_points = []
 
+        inner_leg_connection_points.append(
+            (R[0], Z[0]))
+        inner_leg_connection_points.append(
+            (R[-1], Z[-1]))
+        inner_leg_connection_points.append(
+            (R_[0], Z_[0]))
+        inner_leg_connection_points.append(
+            (R_[-1], Z_[-1]))
+        self.inner_leg_connection_points = inner_leg_connection_points
+
+        # add the leg to the points
+        if self.with_inner_leg:
+            R = np.append(R, R[0])
+            Z = np.append(Z, Z[0])
+
+            R_ = np.append(R_, R_[0])
+            Z_ = np.append(Z_, Z_[0])
         # add connections
         inner_points = []
         for r, z in zip(R, Z):
             inner_points.append([r, z, 'spline'])
+        if self.with_inner_leg:
+            inner_points[-2][2] = 'straight'
         inner_points[-1][2] = 'straight'
         outer_points = []
-        for r, z in zip(np.flip(R_), np.flip(Z_)):
+        for r, z in zip(R_, Z_):
             outer_points.append([r, z, 'spline'])
+        if self.with_inner_leg:
+            outer_points[-2][2] = 'straight'
         outer_points[-1][2] = "straight"
 
         points = inner_points + outer_points
 
         self.points = points
-
-        # extract helping points for inner leg
-        inner_leg_connection_points = []
-
-        inner_leg_connection_points.append(
-            (inner_points[0][0], inner_points[0][1]))
-        inner_leg_connection_points.append(
-            (inner_points[-1][0], inner_points[-1][1]))
-        inner_leg_connection_points.append(
-            (outer_points[0][0], outer_points[0][1]))
-        inner_leg_connection_points.append(
-            (outer_points[-1][0], outer_points[-1][1]))
-        self.inner_leg_connection_points = inner_leg_connection_points
 
     def find_azimuth_placement_angle(self):
         """Calculates the azimuth placement angles based on the number of tf coils"""
@@ -216,81 +227,3 @@ class ToroidalFieldCoilPrincetonD(ExtrudeMixedShape):
                 endpoint=False))
 
         self.azimuth_placement_angle = angles
-
-    def create_solid(self):
-        """Creates a 3d solid using points with straight and spline
-        connections edges, azimuth_placement_angle and distance.
-
-        :return: a 3d solid volume
-        :rtype: a cadquery solid
-        """
-
-        # obtains the first two values of the points list
-        XZ_points = [(p[0], p[1]) for p in self.points]
-
-        # obtains the last values of the points list
-        connections = [p[2] for p in self.points[:-1]]
-
-        current_linetype = connections[0]
-        current_points_list = []
-        instructions = []
-        # groups together common connection types
-        for i, c in enumerate(connections):
-            if c == current_linetype:
-                current_points_list.append(XZ_points[i])
-            else:
-                current_points_list.append(XZ_points[i])
-                instructions.append({current_linetype: current_points_list})
-                current_linetype = c
-                current_points_list = [XZ_points[i]]
-        instructions.append({current_linetype: current_points_list})
-
-        if list(instructions[-1].values())[0][-1] != XZ_points[0]:
-            keyname = list(instructions[-1].keys())[0]
-            instructions[-1][keyname].append(XZ_points[0])
-
-        solid = cq.Workplane(self.workplane)
-        solid.moveTo(XZ_points[0][0], XZ_points[0][1])
-
-        for entry in instructions:
-            if list(entry.keys())[0] == "spline":
-                solid = solid.spline(listOfXYTuple=list(entry.values())[0])
-            if list(entry.keys())[0] == "straight":
-                solid = solid.polyline(list(entry.values())[0])
-
-        # performs extrude in both directions, hence distance / 2
-        solid = solid.close().extrude(distance=-self.distance / 2.0, both=True)
-
-        if self.with_inner_leg is True:
-            inner_leg_solid = cq.Workplane(self.workplane)
-            inner_leg_solid.moveTo(XZ_points[0][0], XZ_points[0][1])
-            inner_leg_solid = inner_leg_solid.polyline(
-                self.inner_leg_connection_points)
-            inner_leg_solid = inner_leg_solid.close().extrude(
-                distance=-self.distance / 2.0, both=True)
-
-            solid = cq.Compound.makeCompound(
-                [a.val() for a in [inner_leg_solid, solid]]
-            )
-
-        # Checks if the azimuth_placement_angle is a list of angles
-        if isinstance(self.azimuth_placement_angle, Iterable):
-            rotated_solids = []
-            # Perform seperate rotations for each angle
-            for angle in self.azimuth_placement_angle:
-                rotated_solids.append(
-                    solid.rotate(
-                        (0, 0, -1), (0, 0, 1), angle))
-            solid = cq.Workplane(self.workplane)
-
-            # Joins the seperate solids together
-            for i in rotated_solids:
-                solid = solid.union(i)
-        else:
-            # Peform rotations for a single azimuth_placement_angle angle
-            solid = solid.rotate(
-                (0, 0, 1), (0, 0, -1), self.azimuth_placement_angle)
-
-        self.perform_boolean_operations(solid)
-
-        return solid
--- paramak/parametric_components/toroidal_field_coil_triple_arc.py
+++ paramak/parametric_components/toroidal_field_coil_triple_arc.py
@@ -171,16 +171,6 @@ class ToroidalFieldCoilTripleArc(ExtrudeMixedShape):
         Z_outer += self.vertical_displacement
         Z_inner += self.vertical_displacement
 
-        # create points with connections
-        points = []
-        for i in range(len(R_inner)):
-            points.append([R_inner[i], Z_inner[i], 'spline'])
-        points[-1][2] = 'straight'
-        for i in range(len(R_outer)):
-            points.append([R_outer[i], Z_outer[i], 'spline'])
-        points[-1][2] = 'straight'
-        self.points = points
-
         # extract helping points for inner leg
         inner_leg_connection_points = []
 
@@ -194,9 +184,31 @@ class ToroidalFieldCoilTripleArc(ExtrudeMixedShape):
             (R_outer[-1], Z_outer[-1]))
 
         self.inner_leg_connection_points = inner_leg_connection_points
+        # add the leg to the points
+        if self.with_inner_leg:
+            R_inner = np.append(R_inner, R_inner[0])
+            Z_inner = np.append(Z_inner, Z_inner[0])
+
+            R_outer = np.append(R_outer, R_outer[0])
+            Z_outer = np.append(Z_outer, Z_outer[0])
+        # create points with connections
+        points = []
+        for i in range(len(R_inner)):
+            points.append([R_inner[i], Z_inner[i], 'spline'])
+        if self.with_inner_leg:
+            points[-2][2] = 'straight'
+        points[-1][2] = 'straight'
+        for i in range(len(R_outer)):
+            points.append([R_outer[i], Z_outer[i], 'spline'])
+        if self.with_inner_leg:
+            points[-2][2] = 'straight'
+        points[-1][2] = 'straight'
+
+        self.points = points
 
     def find_azimuth_placement_angle(self):
-        """Calculates the azimuth placement angles based on the number of tf coils"""
+        """Calculates the azimuth placement angles based on the number of tf
+        coils"""
 
         angles = list(
             np.linspace(
@@ -206,81 +218,3 @@ class ToroidalFieldCoilTripleArc(ExtrudeMixedShape):
                 endpoint=False))
 
         self.azimuth_placement_angle = angles
-
-    def create_solid(self):
-        """Creates a 3d solid using points with straight and spline
-        connections edges, azimuth_placement_angle and distance.
-
-        :return: a 3d solid volume
-        :rtype: a cadquery solid
-        """
-
-        # obtains the first two values of the points list
-        XZ_points = [(p[0], p[1]) for p in self.points]
-
-        # obtains the last values of the points list
-        connections = [p[2] for p in self.points[:-1]]
-
-        current_linetype = connections[0]
-        current_points_list = []
-        instructions = []
-        # groups together common connection types
-        for i, c in enumerate(connections):
-            if c == current_linetype:
-                current_points_list.append(XZ_points[i])
-            else:
-                current_points_list.append(XZ_points[i])
-                instructions.append({current_linetype: current_points_list})
-                current_linetype = c
-                current_points_list = [XZ_points[i]]
-        instructions.append({current_linetype: current_points_list})
-
-        if list(instructions[-1].values())[0][-1] != XZ_points[0]:
-            keyname = list(instructions[-1].keys())[0]
-            instructions[-1][keyname].append(XZ_points[0])
-
-        solid = cq.Workplane(self.workplane)
-        solid.moveTo(XZ_points[0][0], XZ_points[0][1])
-
-        for entry in instructions:
-            if list(entry.keys())[0] == "spline":
-                solid = solid.spline(listOfXYTuple=list(entry.values())[0])
-            if list(entry.keys())[0] == "straight":
-                solid = solid.polyline(list(entry.values())[0])
-
-        # performs extrude in both directions, hence distance / 2
-        solid = solid.close().extrude(distance=-self.distance / 2.0, both=True)
-
-        if self.with_inner_leg is True:
-            inner_leg_solid = cq.Workplane(self.workplane)
-            inner_leg_solid.moveTo(XZ_points[0][0], XZ_points[0][1])
-            inner_leg_solid = inner_leg_solid.polyline(
-                self.inner_leg_connection_points)
-            inner_leg_solid = inner_leg_solid.close().extrude(
-                distance=-self.distance / 2.0, both=True)
-
-            solid = cq.Compound.makeCompound(
-                [a.val() for a in [inner_leg_solid, solid]]
-            )
-
-        # Checks if the azimuth_placement_angle is a list of angles
-        if isinstance(self.azimuth_placement_angle, Iterable):
-            rotated_solids = []
-            # Perform seperate rotations for each angle
-            for angle in self.azimuth_placement_angle:
-                rotated_solids.append(
-                    solid.rotate(
-                        (0, 0, -1), (0, 0, 1), angle))
-            solid = cq.Workplane(self.workplane)
-
-            # Joins the seperate solids together
-            for i in rotated_solids:
-                solid = solid.union(i)
-        else:
-            # Peform rotations for a single azimuth_placement_angle angle
-            solid = solid.rotate(
-                (0, 0, 1), (0, 0, -1), self.azimuth_placement_angle)
-
-        self.perform_boolean_operations(solid)
-
-        return solid
