8e1c8269310e34ed93cba5c99bad29f5034fc612
autopep8 <autopep8@users.noreply.github.com>
2020-10-05 18:16:17 +0000

    Automated autopep8 fixes

--- paramak/parametric_components/port_cutters.py
+++ paramak/parametric_components/port_cutters.py
@@ -14,7 +14,7 @@ class PortCutterRotated(RotateStraightShape):
         poloidal_coverage_angle (float): the angluar extent of port in the
             poloidal direction (degrees).
         center_point (tuple of floats): the center point where the
-            ports are aimed towards, typically the center of the plasma.                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                              
+            ports are aimed towards, typically the center of the plasma.
         poloidal_placement_angle (float): The angle used when rotating the shape
             on the polar axis. 0 degrees is the outboard equatorial point.
         max_distance_from_center (float): the maximum distance from the center
@@ -131,22 +131,22 @@ class PortCutterRotated(RotateStraightShape):
         points = [self.center_point]
 
         outer_point = (self.center_point[0] + self.max_distance_from_center,
-            self.center_point[1])
+                       self.center_point[1])
 
         outer_point_rotated = rotate(
             self.center_point,
             outer_point,
             math.radians(self.poloidal_placement_angle))
-        
+
         outer_point_1 = rotate(
             self.center_point,
             outer_point_rotated,
-            math.radians(0.5*self.poloidal_coverage_angle))
+            math.radians(0.5 * self.poloidal_coverage_angle))
 
         outer_point_2 = rotate(
             self.center_point,
             outer_point_rotated,
-            math.radians(-0.5*self.poloidal_coverage_angle))
+            math.radians(-0.5 * self.poloidal_coverage_angle))
 
         if outer_point_1[0] < 0:
             m, c = coefficients_of_line_from_points(
