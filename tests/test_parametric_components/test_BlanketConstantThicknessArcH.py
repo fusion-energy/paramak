79f5344e417d452dc10df75c8b90848a8c543d94
Jonathan Shimwell <jonathan.shimwell@ukaea.uk>
2020-10-09 15:23:12 +0100

    test for seperate component

---
+++ tests/test_parametric_components/test_BlanketConstantThicknessArcH.py
@@ -0,0 +1,21 @@
+
+import paramak
+import unittest
+
+
+class test_BlanketConstantThicknessArcH(unittest.TestCase):
+    def test_BlanketConstantThickness_creation(self):
+        """creates a blanket using the BlanketConstantThicknessArcH parametric
+        component and checks that a cadquery solid is created"""
+
+        test_shape = paramak.BlanketConstantThicknessArcH(
+            inner_lower_point=(300, -200),
+            inner_mid_point=(500, 0),
+            inner_upper_point=(300, 200),
+            thickness=20,
+            rotation_angle=180,
+        )
+
+        assert test_shape.solid is not None
+        assert test_shape.volume > 1000
+
