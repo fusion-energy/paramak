2b3407176c993e3f25a859d2b57ef06cc8977738
Jonathan Shimwell <jonathan.shimwell@ukaea.uk>
2020-10-15 19:06:38 +0100

    replaced points with scaled_points

--- paramak/parametric_shapes/extruded_mixed_shape.py
+++ paramak/parametric_shapes/extruded_mixed_shape.py
@@ -50,7 +50,7 @@ class ExtrudeMixedShape(Shape):
         """
 
         # obtains the first two values of the points list
-        XZ_points = [(p[0], p[1]) for p in self.points]
+        XZ_points = [(p[0], p[1]) for p in self.scaled_points]
 
         # obtains the last values of the points list
         connections = [p[2] for p in self.points[:-1]]
