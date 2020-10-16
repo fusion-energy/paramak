665f28be4da429e5e90633eec60dfb135e92adac
Jonathan Shimwell <jonathan.shimwell@ukaea.uk>
2020-10-16 13:42:29 +0100

    fixed typo

--- paramak/parametric_components/poloidal_field_coil_case_set.py
+++ paramak/parametric_components/poloidal_field_coil_case_set.py
@@ -14,10 +14,10 @@ class PoloidalFieldCoilCaseSet(RotateStraightShape):
         casing_thicknesses (list of floats): the thickness of the casing (cm).
         center_points (tuple of floats): the center of the coil (x,z) values
             (cm).
-        stp_filename (str, optional): Defaults to "PoloidalFieldCoil.stp".
-        stl_filename (str, optional): Defaults to "PoloidalFieldCoil.stl".
-        name (str, optional): Defaults to "pf_coil".
-        material_tag (str, optional): Defaults to "pf_coil_mat".
+        stp_filename (str, optional): defaults to "PoloidalFieldCoil.stp".
+        stl_filename (str, optional): defaults to "PoloidalFieldCoil.stl".
+        name (str, optional): defaults to "pf_coil".
+        material_tag (str, optional): defaults to "pf_coil_mat".
     """
 
     def __init__(
