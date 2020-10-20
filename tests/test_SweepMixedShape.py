ebe04ff2a42efaaf99008a343310085ce9a21e0d
billingsley-john <johnbillingsley1996@yahoo.com>
2020-10-20 11:15:42 +0100

    added tests to increase codecov

--- tests/test_SweepMixedShape.py
+++ tests/test_SweepMixedShape.py
@@ -67,6 +67,64 @@ class test_object_properties(unittest.TestCase):
 
         assert test_shape_1.volume == pytest.approx(test_shape_2.volume * 0.25)
 
+    def test_iterable_azimuthal_placement(self):
+        """checks that swept solids can be placed at multiple azimuth placement angles"""
+
+        test_shape = SweepMixedShape(
+            points=[
+                (-10, 10, "straight"),
+                (10, 10, "spline"),
+                (10, -10, "spline"),
+                (-10, -10, "straight")
+            ],
+            path_points=[
+                (50, 0),
+                (30, 50),
+                (60, 100),
+                (50, 150)
+            ]
+        )
+        test_shape.create_solid()
+
+        test_volume = test_shape.volume
+        
+        test_shape.azimuth_placement_angle = [0, 90, 180, 270]
+
+        assert test_shape.volume == pytest.approx(test_volume * 4, rel=0.01)
+
+    def test_workplane_path_workplane_error_raises(self):
+        """checks that errors are raised when disallowed workplane and path_workplane
+        combinations are used"""
+
+        def workplane_and_path_workplane_equal():
+            test_shape = SweepMixedShape(
+                points=[
+                    (-20, 20, "straight"),
+                    (20, 20, "spline"),
+                    (20, -20, "spline"), 
+                    (-20, -20, "straight")
+                ],
+                path_points=[(50, 0), (30, 50), (60, 100), (50, 150)],
+                workplane="XZ",
+                path_workplane="XZ"
+            )
+
+        def invalid_relative_workplane_and_path_workplane():
+            test_shape = SweepMixedShape(
+                points=[
+                    (-20, 20, "straight"),
+                    (20, 20, "spline"),
+                    (20, -20, "spline"), 
+                    (-20, -20, "straight")
+                ],
+                path_points=[(50, 0), (30, 50), (60, 100), (50, 150)],
+                workplane="XZ",
+                path_workplane="YZ"
+            )
+
+        self.assertRaises(ValueError, workplane_and_path_workplane_equal)
+        self.assertRaises(ValueError, invalid_relative_workplane_and_path_workplane)
+
 
 if __name__ == "__main__":
     unittest.main()
