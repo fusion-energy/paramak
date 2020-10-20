3a3c6121cc486f22fd923fefb963eb817476e8d1
billingsley-john <johnbillingsley1996@yahoo.com>
2020-10-20 10:25:25 +0100

    fixed end point check

--- paramak/shape.py
+++ paramak/shape.py
@@ -299,7 +299,11 @@ class Shape:
                             3 but not a mixture of 2 and 3")
 
             if len(values) > 1:
-                if values[-1][:2] != values[0][:2]:
+                if values[-1][0] == values[0][0] and values[-1][1] == values[0][1]:
+                    raise ValueError(
+                        "The coordinates of the last and first points are the same."
+                    )
+                else:
                     values.append(values[0])
 
             self._points = values
