4021741e133ae05b9d0e718f3ae0bba84b44846c
remithewarrior <delaporte.mathurin@laposte.net>
2020-10-22 13:43:58 +0200

    refactored rotate_spline_shape

--- paramak/parametric_shapes/rotate_spline_shape.py
+++ paramak/parametric_shapes/rotate_spline_shape.py
@@ -1,15 +1,15 @@
 
-from collections import Iterable
-
 import cadquery as cq
 
-from paramak import Shape
+from paramak import RotateMixedShape
 
 
-class RotateSplineShape(Shape):
+class RotateSplineShape(RotateMixedShape):
     """Rotates a 3d CadQuery solid from points connected with splines.
 
     Args:
+        points (list): list of (float, float) containing each point
+            coordinates.
         rotation_angle (float, optional): The rotation_angle to use when
             revolving the solid (degrees). Defaults to 360.0.
         stp_filename (str, optional): Defaults to "RotateSplineShape.stp".
@@ -18,6 +18,7 @@ class RotateSplineShape(Shape):
 
     def __init__(
         self,
+        points,
         rotation_angle=360,
         stp_filename="RotateSplineShape.stp",
         stl_filename="RotateSplineShape.stl",
@@ -25,54 +26,9 @@ class RotateSplineShape(Shape):
     ):
 
         super().__init__(
+            points=[(*p, "spline") for p in points],
+            rotation_angle=rotation_angle,
             stp_filename=stp_filename,
             stl_filename=stl_filename,
             **kwargs
         )
-
-        self.rotation_angle = rotation_angle
-
-    @property
-    def rotation_angle(self):
-        return self._rotation_angle
-
-    @rotation_angle.setter
-    def rotation_angle(self, value):
-        self._rotation_angle = value
-
-    def create_solid(self):
-        """Creates a rotated 3d solid using points with spline edges.
-
-           Returns:
-              A CadQuery solid: A 3D solid volume
-        """
-
-        # Creates a cadquery solid from points and revolves
-        solid = (
-            cq.Workplane(self.workplane)
-            .spline(self.points)
-            .close()
-            .revolve(self.rotation_angle)
-        )
-
-        # Checks if the azimuth_placement_angle is a list of angles
-        if isinstance(self.azimuth_placement_angle, Iterable):
-            rotated_solids = []
-            # Perform seperate rotations for each angle
-            for angle in self.azimuth_placement_angle:
-                rotated_solids.append(
-                    solid.rotate(
-                        (0, 0, -1), (0, 0, 1), angle))
-            solid = cq.Workplane(self.workplane)
-
-            # Joins the seperate solids together
-            for i in rotated_solids:
-                solid = solid.union(i)
-        else:
-            # Peform rotations for a single azimuth_placement_angle angle
-            solid = solid.rotate(
-                (0, 0, -1), (0, 0, 1), self.azimuth_placement_angle)
-
-        self.perform_boolean_operations(solid)
-
-        return solid
