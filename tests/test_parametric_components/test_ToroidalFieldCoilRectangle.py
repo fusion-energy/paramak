116a606029224e2bb3ef167a87727b6e085353f3
billingsley-john <johnbillingsley1996@yahoo.com>
2020-10-23 13:13:30 +0100

    added tests for rotation_angle cutting

--- tests/test_parametric_components/test_ToroidalFieldCoilRectangle.py
+++ tests/test_parametric_components/test_ToroidalFieldCoilRectangle.py
@@ -1,5 +1,6 @@
 
 import paramak
+import pytest
 import unittest
 
 
@@ -32,3 +33,21 @@ class test_ToroidalFieldCoilRectangle(unittest.TestCase):
         )
         assert test_shape.solid is not None
         assert test_shape.volume > 1000
+
+    def test_ToroidalFieldCoilRectangle_rotation_angle(self):
+        """creates a tf coil with a rotation_angle < 360 and checks that the correct
+        cut is performed and the volume is correct"""
+
+        test_shape = paramak.ToroidalFieldCoilRectangle(
+            horizontal_start_point=(100, 700),
+            vertical_mid_point=(800, 0),
+            with_inner_leg=False,
+            thickness=50,
+            distance=50,
+            number_of_coils=8,
+        )
+        test_volume = test_shape.volume
+
+        test_shape.rotation_angle = 180
+
+        assert test_shape.volume == pytest.approx(test_volume * 0.5)
