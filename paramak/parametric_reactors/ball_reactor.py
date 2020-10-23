b3ff4d99f737ee8ca4fea4866fdda18417204817
autopep8 <autopep8@users.noreply.github.com>
2020-10-23 13:59:20 +0000

    Automated autopep8 fixes

--- paramak/parametric_reactors/ball_reactor.py
+++ paramak/parametric_reactors/ball_reactor.py
@@ -124,7 +124,6 @@ class BallReactor(paramak.Reactor):
 
         self.create_solids()
 
-
     @property
     def pf_coil_radial_thickness(self):
         return self._pf_coil_radial_thickness
@@ -169,7 +168,7 @@ class BallReactor(paramak.Reactor):
         self._make_component_cuts()
 
         return self.shapes_and_components
-    
+
     def _rotation_angle_check(self):
 
         if self.rotation_angle == 360:
