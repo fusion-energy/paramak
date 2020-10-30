4fd84fa0442582d5c760305537ee392163be150e
billingsley-john <johnbillingsley1996@yahoo.com>
2020-10-30 18:09:47 +0000

    updated volume tests

--- tests/test_parametric_components/test_ToroidalFieldCoilCoatHanger.py
+++ tests/test_parametric_components/test_ToroidalFieldCoilCoatHanger.py
@@ -5,9 +5,9 @@ import unittest
 
 
 class test_ToroidalFieldCoilCoatHanger(unittest.TestCase):
-    def test_ToroidalFieldCoilCoatHanger_creation(self):
-        """creates a tf coil using the ToroidalFieldCoilCoatHanger parametric component
-        and checks that a cadquery solid is created"""
+    def test_ToroidalFieldCoilCoatHanger_creation_with_inner_leg(self):
+        """creates a tf coil with inner leg using the ToroidalFieldCoilCoatHanger
+        parametric component and checks that a cadquery solid is created"""
 
         test_shape = paramak.ToroidalFieldCoilCoatHanger(
             horizontal_start_point=(200, 500),
@@ -16,11 +16,41 @@ class test_ToroidalFieldCoilCoatHanger(unittest.TestCase):
             vertical_length=500,
             thickness=50,
             distance=50,
-            number_of_coils=5,
+            number_of_coils=1,
+            with_inner_leg=True,
         )
-
         assert test_shape.solid is not None
         assert test_shape.volume > 1000
+        assert test_shape.inner_leg_connection_points is not None
+
+        test_inner_leg = paramak.ExtrudeStraightShape(
+            points=test_shape.inner_leg_connection_points, distance=50
+        )
+        assert test_inner_leg.solid is not None
+
+    def test_ToroidalFieldCoilCoatHanger_creation_no_inner_leg(self):
+        """creates a tf coil with no inner leg using the ToroidalFieldCoilRectangle
+        parametric component and checks that a cadquery solid is created"""
+
+        test_shape_1 = paramak.ToroidalFieldCoilCoatHanger(
+            horizontal_start_point=(200, 500), horizontal_length=400,
+            vertical_start_point=(700, 0), vertical_length=500, thickness=50,
+            distance=50, number_of_coils=1, with_inner_leg=True,
+        )
+        test_volume_1 = test_shape_1.volume
+
+        test_inner_leg = paramak.ExtrudeStraightShape(
+            points=test_shape_1.inner_leg_connection_points, distance=50
+        )
+        inner_leg_volume = test_inner_leg.volume
+
+        test_shape_2 = paramak.ToroidalFieldCoilCoatHanger(
+            horizontal_start_point=(200, 500), horizontal_length=400,
+            vertical_start_point=(700, 0), vertical_length=500, thickness=50,
+            distance=50, number_of_coils=1, with_inner_leg=False,
+        )
+        assert test_shape_2.solid is not None
+        assert test_shape_2.volume == pytest.approx(test_volume_1 - inner_leg_volume)
     
     def test_ToroidalFieldCoilCoatHanger_absolute_volume(self):
         """creates a tf coil using the ToroidalFieldCoilCoatHanger parametric
@@ -38,30 +68,8 @@ class test_ToroidalFieldCoilCoatHanger(unittest.TestCase):
 
         assert test_shape.volume == pytest.approx((400*50*30*2) + ((50*50*30/2)*2) + (50*500*30) + (((150*250*30) - (((100*250)/2)*30) - (((100*250)/2)*30))*2) + (50*1000*30))
 
-        # test_shape.with_inner_leg = False
-        # assert test_shape.volume == pytest.approx((400*50*30*2) + ((50*50*30/2)*2) + (50*500*30) + (((150*250) - ((100*250/2)*2))*30))
-
-
-    # def test_ToroidalFieldCoilCoatHanger_absolute_areas(self):
-    #     """creates tf coils using the ToroidalFieldCoilCoatHanger parametric
-    #     component and checks that the areas of the faces are correct"""
-
-    #     test_shape = paramak.ToroidalFieldCoilCoatHanger(
-    #         horizontal_start_point=(200, 500),
-    #         horizontal_length=400,
-    #         vertical_start_point=(700, 0),
-    #         vertical_length=500,
-    #         thickness=50,
-    #         distance=30,
-    #         number_of_coils=1,
-    #     )
-
-        # assert test_shape.area == pytest.approx((400*50*2*2) + ((50*50/2)*2*2) + (50*500*2) + (((150*250) - ((100*250/2)*2))*2) + (400*30*4) + (500*30*2) + (50*30*2) + ((29**0.5)*30*4) + (50*(2**0.5)*30))
-        # assert len(test_shape.areas) == 20
-        # assert test_shape.areas.count(pytest.approx(400*50*30)) == 2
-        # assert test_shape.areas.count(pytest.approx())
-
-
+        test_shape.with_inner_leg = False
+        assert test_shape.volume == pytest.approx((400*50*30*2) + ((50*50*30/2)*2) + (50*500*30) + (((150*250*30) - (((100*250)/2)*30) - (((100*250)/2)*30))*2))
 
     def test_ToroidalFieldCoilCoatHanger_rotation_angle(self):
         """creates a tf coil with a rotation_angle < 360 and checks that the correct
