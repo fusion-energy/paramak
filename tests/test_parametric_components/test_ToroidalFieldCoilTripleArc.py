1c2a33fe2c38f0b9128cbdc5d080659c8e1783d0
autopep8 <autopep8@users.noreply.github.com>
2020-10-30 16:01:23 +0000

    Automated autopep8 fixes

--- tests/test_parametric_components/test_ToroidalFieldCoilTripleArc.py
+++ tests/test_parametric_components/test_ToroidalFieldCoilTripleArc.py
@@ -21,40 +21,42 @@ class test_ToroidalFieldCoilTripleArc(unittest.TestCase):
         assert test_shape.solid is not None
 
     def test_ToroidalFieldCoilTripleArc_rotation_angle(self):
-<<<<<<< HEAD
-        """Creates a tf coil with a rotation_angle < 360 degrees and checks
-        that the correct cut is performed and the volume is correct."""
-=======
-        """creates tf coils with rotation_angles < 360 in different workplanes and
-        checks that the correct cuts are performed and their volumes are correct"""
->>>>>>> develop
 
-        test_shape = paramak.ToroidalFieldCoilTripleArc(
-            R1=150,
-            h=200,
-            radii=(50, 50),
-            coverages=(70, 70),
-            thickness=50,
-            distance=50,
-            number_of_coils=8,
-        )
 
-        test_shape.rotation_angle = 360
-        test_shape.workplane = "XZ"
-        test_volume = test_shape.volume
-        test_shape.rotation_angle = 180
-        assert test_shape.volume == pytest.approx(test_volume * 0.5, rel=0.01)
+<< << << < HEAD
+"""Creates a tf coil with a rotation_angle < 360 degrees and checks
+    that the correct cut is performed and the volume is correct."""
+== == == =
+"""creates tf coils with rotation_angles < 360 in different workplanes and
+    checks that the correct cuts are performed and their volumes are correct"""
+>>>>>> > develop
 
-        test_shape.rotation_angle = 360
-        test_shape.workplane = "YZ"
-        test_volume = test_shape.volume
-        test_shape.rotation_angle = 180
-        assert test_shape.volume == pytest.approx(test_volume * 0.5, rel=0.01)
+test_shape = paramak.ToroidalFieldCoilTripleArc(
+    R1=150,
+    h=200,
+    radii=(50, 50),
+    coverages=(70, 70),
+    thickness=50,
+    distance=50,
+    number_of_coils=8,
+)
 
-        # this test will remain commented until workplane issue #308 is resolved
-        # currently causes terminal to crash due to large number of unions
-        # test_shape.rotation_angle = 360
-        # test_shape.workplane = "XY"
-        # test_volume = test_shape.volume
-        # test_shape.rotation_angle = 180
-        # assert test_shape.volume == pytest.approx(test_volume * 0.5)
+test_shape.rotation_angle = 360
+test_shape.workplane = "XZ"
+test_volume = test_shape.volume
+test_shape.rotation_angle = 180
+assert test_shape.volume == pytest.approx(test_volume * 0.5, rel=0.01)
+
+test_shape.rotation_angle = 360
+test_shape.workplane = "YZ"
+test_volume = test_shape.volume
+test_shape.rotation_angle = 180
+assert test_shape.volume == pytest.approx(test_volume * 0.5, rel=0.01)
+
+# this test will remain commented until workplane issue #308 is resolved
+# currently causes terminal to crash due to large number of unions
+# test_shape.rotation_angle = 360
+# test_shape.workplane = "XY"
+# test_volume = test_shape.volume
+# test_shape.rotation_angle = 180
+# assert test_shape.volume == pytest.approx(test_volume * 0.5)
