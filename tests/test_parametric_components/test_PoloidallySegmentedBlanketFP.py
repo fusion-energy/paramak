43da5638c446de229a4887dc8a9cd2aa8fb5311e
RemDelaporteMathurin <delaporte.mathurin@laposte.net>
2020-11-06 16:04:01 +0100

    added changing segment_angles test

--- tests/test_parametric_components/test_PoloidallySegmentedBlanketFP.py
+++ tests/test_parametric_components/test_PoloidallySegmentedBlanketFP.py
@@ -1,5 +1,6 @@
 import os
 import paramak
+import numpy as np
 import unittest
 
 
@@ -17,3 +18,14 @@ class test_BlanketFP(unittest.TestCase):
         assert blanket.num_points == blanket.num_segments + 1
         blanket.num_segments = 60
         assert blanket.num_points == blanket.num_segments + 1
+
+    def test_segment_angles_affects_solid(self):
+        blanket1 = paramak.BlanketFPPoloidalSegments(
+            thickness=20, start_angle=0,
+            stop_angle=180, rotation_angle=180)
+        blanket2 = paramak.BlanketFPPoloidalSegments(
+            thickness=20, start_angle=0,
+            stop_angle=180, rotation_angle=180,
+            cut=blanket1)
+        blanket2.segments_angles = np.array([0, 25, 50, 90, 130, 150, 180])
+        assert blanket2.volume != 0
