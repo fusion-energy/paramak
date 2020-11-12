4b788270893457a8310f55d958813d8cde300d26
billingsley-john <johnbillingsley1996@yahoo.com>
2020-11-12 18:26:02 +0000

    added tests for new parametric components

---
+++ tests/test_parametric_components/test_CoolantChannelRingCurved.py
@@ -0,0 +1,51 @@
+
+import os
+import math
+import unittest
+from pathlib import Path
+
+import paramak
+import pytest
+
+
+class test_CoolantChannelRingCurved(unittest.TestCase):
+    def test_CoolantChannelRingCurved_creation(self):
+        """creates a coolant channel ring using the CoolantChannelRingCurved parametric shape
+        and checks that a cadquery solid is created"""
+
+        test_shape = paramak.CoolantChannelRingCurved(
+            height=200,
+            channel_radius=10,
+            ring_radius=70,
+            mid_offset=-20,
+            number_of_coolant_channels=8
+        )
+
+        assert test_shape.solid is not None
+        assert test_shape.volume > 1000
+
+    def test_CoolantChannelRingCurved_relative_volumes(self):
+        """creates coolant channel rings using the CoolantChannelRingCurved parametric shape
+        and checks the relative volumes are correct"""
+
+        # number_of_coolant_channels
+        test_shape_1 = paramak.CoolantChannelRingCurved(
+            height=200,
+            channel_radius=10,
+            ring_radius=70,
+            mid_offset=-20,
+            number_of_coolant_channels=4
+        )
+
+        test_shape_2 = paramak.CoolantChannelRingCurved(
+            height=200,
+            channel_radius=10,
+            ring_radius=70,
+            mid_offset=-20,
+            number_of_coolant_channels=8
+        )
+
+        assert test_shape_1.volume == pytest.approx(test_shape_2.volume * 0.5)
+
+        # needs similar test for mid_offset (volume should increase with
+        # mid_offset)
