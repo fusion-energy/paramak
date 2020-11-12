33449bf3e3bd1b8d8a4d8fe371fb9e01b50e6805
RemDelaporteMathurin <delaporte.mathurin@laposte.net>
2020-11-12 11:47:03 +0100

    CuttingWedgeFS inherits from CuttingWedge and workplane is computed from rotation_axis

--- paramak/parametric_components/cutting_wedge_fs.py
+++ paramak/parametric_components/cutting_wedge_fs.py
@@ -1,9 +1,10 @@
-from paramak import RotateStraightShape
+from paramak import CuttingWedge
 
 from collections import Iterable
+from operator import itemgetter
 
 
-class CuttingWedgeFS(RotateStraightShape):
+class CuttingWedgeFS(CuttingWedge):
     """Creates a wedge from a Shape that can be useful for cutting sector
     models.
 
@@ -12,7 +13,6 @@ class CuttingWedgeFS(RotateStraightShape):
             height and radius of the wedge
         stp_filename (str, optional): Defaults to "CuttingWedgeFS.stp".
         stl_filename (str, optional): Defaults to "CuttingWedgeFS.stl".
-        rotation_angle (float, optional): Defaults to 180.0.
         material_tag (str, optional): Defaults to "cutting_slice_mat".
     """
 
@@ -24,15 +24,15 @@ class CuttingWedgeFS(RotateStraightShape):
         material_tag="cutting_slice_mat",
         **kwargs
     ):
-
+        self.shape = shape
         super().__init__(
+            height=self.height,
+            radius=self.radius,
             material_tag=material_tag,
             stp_filename=stp_filename,
             stl_filename=stl_filename,
             **kwargs
         )
-
-        self.shape = shape
 
     @property
     def shape(self):
@@ -40,48 +40,97 @@ class CuttingWedgeFS(RotateStraightShape):
 
     @shape.setter
     def shape(self, value):
+        if value.rotation_angle == 360:
+            msg = 'cutting_wedge cannot be created,' + \
+                ' rotation_angle must be < 360'
+            raise ValueError(msg)
         self._shape = value
 
-    def find_points(self):
+    @property
+    def radius(self):
+        self.find_radius_height()
+        return self._radius
 
-        if self.shape.rotation_angle == 360:
-            raise ValueError(
-                'cutting_wedge cannot be created, rotation_angle must be < 360')
+    @radius.setter
+    def radius(self, value):
+        self._radius = value
+
+    @property
+    def height(self):
+        self.find_radius_height()
+        return self._height
+
+    @height.setter
+    def height(self, value):
+        self._height = value
+
+    @property
+    def rotation_angle(self):
+        self.rotation_angle = 360 - self.shape.rotation_angle
+        return self._rotation_angle
+
+    @rotation_angle.setter
+    def rotation_angle(self, value):
+        self._rotation_angle = value
+
+    @property
+    def workplane(self):
+        workplanes = ["XY", "XZ", "YZ"]
+        for wp in workplanes:
+            if self.shape.get_rotation_axis()[1] in wp:
+                self.workplane = wp
+                break
+        return self._workplane
+
+    @workplane.setter
+    def workplane(self, value):
+        self._workplane = value
+
+    @property
+    def rotation_axis(self):
+        self.rotation_axis = self.shape.rotation_axis
+        return self._rotation_axis
+
+    @rotation_axis.setter
+    def rotation_axis(self, value):
+        self._rotation_axis = value
+
+    @property
+    def azimuth_placement_angle(self):
+        if isinstance(self.shape.azimuth_placement_angle, Iterable):
+            self.azimuth_placement_angle = self.shape.rotation_angle
+        else:
+            self.azimuth_placement_angle = \
+                self.shape.azimuth_placement_angle + self.shape.rotation_angle
+        return self._azimuth_placement_angle
+
+    @azimuth_placement_angle.setter
+    def azimuth_placement_angle(self, value):
+        self._azimuth_placement_angle = value
+
+    def find_radius_height(self):
+        shape = self.shape
+        if shape.rotation_angle == 360:
+            msg = 'cutting_wedge cannot be created,' + \
+                ' rotation_angle must be < 360'
+            raise ValueError(msg)
+        shape_points = shape.points
+        if hasattr(shape, 'radius') and len(shape_points) == 1:
+            max_x = shape_points[0][0] + shape.radius
+            max_y = shape_points[0][1] + shape.radius
+
+        elif len(shape_points) > 1:
+            max_x = max(shape_points, key=itemgetter(0))[0]
+            if shape.get_rotation_axis()[1] not in shape.workplane and \
+                    hasattr(shape, "distance"):
+                max_y = shape.distance
+                if shape.extrude_both is False:
+                    max_y *= 0.5
+            else:
+                max_y = max(shape_points, key=itemgetter(1))[1]
 
         else:
-
-            max_x = 0
-            max_y = 0
-
-            if hasattr(self.shape, 'radius') and len(self.shape.points) == 1:
-                max_x = self.shape.points[0][0] + self.shape.radius
-                max_y = self.shape.points[0][1] + self.shape.radius
-
-            elif len(self.shape.points) > 1:
-                for point in self.shape.points:
-                    if point[0] > max_x:
-                        max_x = point[0]
-                    if point[1] > max_y:
-                        max_y = point[1]
-
-            else:
-                raise ValueError('cutting_wedge cannot be created')
-
-            points = [
-                (0, max_y * 2),
-                (max_x * 2, max_y * 2),
-                (max_x * 2, -max_y * 2),
-                (0, -max_y * 2)
-            ]
-
-            rotation_angle = 360 - self.shape.rotation_angle
-
-            if isinstance(self.shape.azimuth_placement_angle, Iterable):
-                azimuth_placement_angle = self.shape.rotation_angle
-            else:
-                azimuth_placement_angle = self.shape.azimuth_placement_angle + self.shape.rotation_angle
-
-        self.points = points
-        self.rotation_angle = rotation_angle
-        self.azimuth_placement_angle = azimuth_placement_angle
-        self.workplane = self.shape.workplane
+            raise ValueError('cutting_wedge cannot be created')
+        safety_factor = 2.1
+        self.radius = safety_factor*max_x
+        self.height = safety_factor*max_y
