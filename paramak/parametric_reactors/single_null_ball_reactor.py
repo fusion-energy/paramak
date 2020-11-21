8da4ab06358bd4e0bff21f10b7ce7d3f390f8c46
RemDelaporteMathurin <delaporte.mathurin@laposte.net>
2020-11-21 20:04:00 +0100

    modified make_divertor in singlenull ball reactor

--- paramak/parametric_reactors/single_null_ball_reactor.py
+++ paramak/parametric_reactors/single_null_ball_reactor.py
@@ -9,67 +9,18 @@ class SingleNullBallReactor(paramak.BallReactor):
     most spherical reactors.
 
     Arguments:
-        inner_bore_radial_thickness (float): the radial thickness of the
-            inner bore (cm)
-        inboard_tf_leg_radial_thickness (float): the radial thickness of the
-            inner leg of the toroidal field coils (cm)
-        center_column_shield_radial_thickness (float): the radial thickness of
-            the center column shield (cm)
-        divertor_radial_thickness (float): the radial thickness of the divertor
-            (cm), this fills the gap between the center column shield and
-            blanket
-        inner_plasma_gap_radial_thickness (float): the radial thickness of the
-            inboard gap between the plasma and the center column shield (cm)
-        plasma_radial_thickness (float): the radial thickness of the plasma
-        outer_plasma_gap_radial_thickness (float): the radial thickness of the
-            outboard gap between the plasma and firstwall (cm)
-        firstwall_radial_thickness (float): the radial thickness of the first
-            wall (cm)
-        blanket_radial_thickness (float): the radial thickness of the blanket
-            (cm)
-        blanket_rear_wall_radial_thickness (float): the radial thickness of the
-            rear wall of the blanket (cm)
-        elongation (float): the elongation of the plasma
-        triangularity (float): the triangularity of the plasma
-        number_of_tf_coils (int): the number of tf coils
+
     """
 
     def __init__(
         self,
-        inner_bore_radial_thickness,
-        inboard_tf_leg_radial_thickness,
-        center_column_shield_radial_thickness,
-        divertor_radial_thickness,
-        inner_plasma_gap_radial_thickness,
-        plasma_radial_thickness,
-        outer_plasma_gap_radial_thickness,
-        firstwall_radial_thickness,
-        blanket_radial_thickness,
-        blanket_rear_wall_radial_thickness,
-        elongation,
-        triangularity,
-        number_of_tf_coils,
         divertor_position="upper",
         **kwargs
     ):
 
         self.divertor_position = divertor_position
 
-        super().__init__(
-            inner_bore_radial_thickness=inner_bore_radial_thickness,
-            inboard_tf_leg_radial_thickness=inboard_tf_leg_radial_thickness,
-            center_column_shield_radial_thickness=center_column_shield_radial_thickness,
-            divertor_radial_thickness=divertor_radial_thickness,
-            inner_plasma_gap_radial_thickness=inner_plasma_gap_radial_thickness,
-            plasma_radial_thickness=plasma_radial_thickness,
-            outer_plasma_gap_radial_thickness=outer_plasma_gap_radial_thickness,
-            firstwall_radial_thickness=firstwall_radial_thickness,
-            blanket_radial_thickness=blanket_radial_thickness,
-            blanket_rear_wall_radial_thickness=blanket_rear_wall_radial_thickness,
-            elongation=elongation,
-            triangularity=triangularity,
-            number_of_tf_coils=number_of_tf_coils,
-            **kwargs)
+        super().__init__(**kwargs)
 
     @property
     def divertor_position(self):
@@ -84,8 +35,6 @@ class SingleNullBallReactor(paramak.BallReactor):
             raise ValueError("divertor position must be 'upper' or 'lower'")
 
     def _make_divertor(self):
-        list_of_components = []
-
         if self.divertor_position == "upper":
             divertor_height = self._blanket_rear_wall_end_height
         elif self.divertor_position == "lower":
@@ -123,14 +72,4 @@ class SingleNullBallReactor(paramak.BallReactor):
             rotation_angle=self.rotation_angle
         )
 
-        list_of_components.append(self._divertor)
-
-        self._firstwall.cut.append(self._divertor)
-        self._blanket.cut.append(self._divertor)
-        self._blanket_rear_wall.cut.append(self._divertor)
-        self._blanket_rear_wall.cut.append(self._center_column_cutter)
-
-        list_of_components.append(self._firstwall)
-        list_of_components.append(self._blanket)
-        list_of_components.append(self._blanket_rear_wall)
-        return list_of_components
+        return self._divertor
