536128fe48fdcb116464f20622e182e9e5ce669c
RemDelaporteMathurin <delaporte.mathurin@laposte.net>
2020-11-21 17:19:18 +0100

    changed the order a bit to simplify the cut argument + pep8

--- paramak/parametric_reactors/segmented_blanket_ball_reactor.py
+++ paramak/parametric_reactors/segmented_blanket_ball_reactor.py
@@ -21,12 +21,12 @@ class SegmentedBlanketBallReactor(paramak.BallReactor):
     """
 
     def __init__(
-        self,
-        gap_between_blankets,
-        number_of_blanket_segments,
-        blanket_fillet_radius=10.0,
-        **kwargs
-    ):
+            self,
+            gap_between_blankets,
+            number_of_blanket_segments,
+            blanket_fillet_radius=10.0,
+            **kwargs
+            ):
 
         self.gap_between_blankets = gap_between_blankets
         self.number_of_blanket_segments = number_of_blanket_segments
@@ -66,7 +66,8 @@ class SegmentedBlanketBallReactor(paramak.BallReactor):
     def _make_blankets_layers(self):
 
         self._center_column_cutter = paramak.CenterColumnShieldCylinder(
-            height=self._center_column_shield_height * 1.5,  # extra 0.5 to ensure overlap,
+            # extra 0.5 to ensure overlap,
+            height=self._center_column_shield_height * 1.5,
             inner_radius=0,
             outer_radius=self._center_column_shield_end_radius,
             rotation_angle=360
@@ -76,25 +77,6 @@ class SegmentedBlanketBallReactor(paramak.BallReactor):
             distance=self.gap_between_blankets,
             azimuth_placement_angle=np.linspace(
                 0, 360, self.number_of_blanket_segments, endpoint=False))
-
-        self._blanket_envelope = paramak.BlanketFP(
-            plasma=self._plasma,
-            thickness=self.firstwall_radial_thickness +
-            self.blanket_radial_thickness,
-            offset_from_plasma=[
-                self.inner_plasma_gap_radial_thickness,
-                self.plasma_gap_vertical_thickness,
-                self.outer_plasma_gap_radial_thickness,
-                self.plasma_gap_vertical_thickness,
-                self.inner_plasma_gap_radial_thickness],
-            start_angle=-180,
-            stop_angle=180,
-            rotation_angle=self.rotation_angle,
-            material_tag="firstwall_mat",
-            name="firstwall",
-            cut=[
-                self._center_column_cutter,
-                thin_cutter])
 
         # makes a thicker star shaped cutting tool
         thick_cutter = paramak.BlanketCutterStar(
@@ -129,7 +111,8 @@ class SegmentedBlanketBallReactor(paramak.BallReactor):
             cut=[self._center_column_cutter, thick_cutter])
 
         if self.blanket_fillet_radius != 0:
-            x = self.major_radius + 1  # tried firstwall start radius here already
+            # tried firstwall start radius here already
+            x = self.major_radius + 1
             front_face_b = self._blanket.solid.faces(
                 cq.NearestToPointSelector((0, x, 0)))
             front_edge_b = front_face_b.edges(
@@ -139,25 +122,45 @@ class SegmentedBlanketBallReactor(paramak.BallReactor):
                 paramak.EdgeLengthSelector(front_edge_length_b)).fillet(
                 self.blanket_fillet_radius)
 
+        self._firstwall = paramak.BlanketFP(
+            plasma=self._plasma,
+            thickness=self.firstwall_radial_thickness +
+            self.blanket_radial_thickness,
+            offset_from_plasma=[
+                self.inner_plasma_gap_radial_thickness,
+                self.plasma_gap_vertical_thickness,
+                self.outer_plasma_gap_radial_thickness,
+                self.plasma_gap_vertical_thickness,
+                self.inner_plasma_gap_radial_thickness],
+            start_angle=-180,
+            stop_angle=180,
+            rotation_angle=self.rotation_angle,
+            material_tag="firstwall_mat",
+            name="firstwall",
+            cut=[
+                self._center_column_cutter,
+                thin_cutter,
+                self._blanket])
+
         # TODO this segfaults at the moment but works as an opperation on the
         # reactor after construction in jupyter
         # tried different x values and (0, x, 0)
-        # noticed that it much quicker as a post process so perhaps some unwanted looping is happening
+        # noticed that it much quicker as a post process so perhaps some
+        # unwanted looping is happening
         # if self.blanket_fillet_radius != 0:
         #     x = self.major_radius # tried firstwall start radius here already
-        #     front_face = self._blanket_envelope.solid.faces(cq.NearestToPointSelector((x, 0, 0)))
+        #     front_face = \
+        #       self._firstwall.solid.faces(
+        #           cq.NearestToPointSelector((x, 0, 0)))
         #     print('found front face')
-        #     front_edge = front_face.edges(cq.NearestToPointSelector((x, 0, 0)))
+        #     front_edge = front_face.edges(
+        #           cq.NearestToPointSelector((x, 0, 0)))
         #     print('found front edge')
         #     front_edge_length = front_edge.val().Length()
         #     print('found front edge length', front_edge_length)
-        #     self._blanket_envelope.solid = self._blanket_envelope.solid.edges(
+        #     self._firstwall.solid = self._firstwall.solid.edges(
         #         paramak.EdgeLengthSelector(front_edge_length)).fillet(self.blanket_fillet_radius)
         # print('finished')
-
-        self._blanket_envelope.cut.append(self._blanket)
-
-        self._firstwall = self._blanket_envelope
 
         self._blanket_rear_wall = paramak.BlanketFP(
             plasma=self._plasma,
