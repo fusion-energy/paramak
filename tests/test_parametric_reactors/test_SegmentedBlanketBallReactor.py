08c72f4fa92407f912424b10f200995a77b74c8a
RemDelaporteMathurin <delaporte.mathurin@laposte.net>
2020-11-21 17:25:27 +0100

    added error tests to SegmentedBlanketBallReactor

--- tests/test_parametric_reactors/test_SegmentedBlanketBallReactor.py
+++ tests/test_parametric_reactors/test_SegmentedBlanketBallReactor.py
@@ -89,3 +89,40 @@ class test_SegmentedBlanketBallReactor(unittest.TestCase):
 
         assert blanket_many_segments < blanket_few_segments
         assert fw_many_segments > fw_few_segments
+
+    def test_inccorect_args(self):
+        reactor = paramak.SegmentedBlanketBallReactor(
+            inner_bore_radial_thickness=10,
+            inboard_tf_leg_radial_thickness=30,
+            center_column_shield_radial_thickness=60,
+            divertor_radial_thickness=150,
+            inner_plasma_gap_radial_thickness=30,
+            plasma_radial_thickness=300,
+            outer_plasma_gap_radial_thickness=30,
+            firstwall_radial_thickness=20,
+            blanket_radial_thickness=50,
+            blanket_rear_wall_radial_thickness=30,
+            elongation=2,
+            triangularity=0.55,
+            number_of_tf_coils=16,
+            rotation_angle=180,
+            pf_coil_radial_thicknesses=[50, 50, 50, 50],
+            pf_coil_vertical_thicknesses=[50, 50, 50, 50],
+            pf_coil_to_rear_blanket_radial_gap=50,
+            pf_coil_to_tf_coil_radial_gap=50,
+            outboard_tf_coil_radial_thickness=100,
+            outboard_tf_coil_poloidal_thickness=50,
+            gap_between_blankets=30,
+            blanket_fillet_radius=0,
+            number_of_blanket_segments=4,
+        )
+
+        def inccorect_number_of_blanket_segments():
+            reactor.number_of_blanket_segments = 1
+
+        self.assertRaises(ValueError, inccorect_number_of_blanket_segments)
+
+        def inccorect_gap_between_blankets():
+            reactor.gap_between_blankets = -1
+
+        self.assertRaises(ValueError, inccorect_gap_between_blankets)
