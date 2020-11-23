d5def77e0c3c560fd4a331f8c70e7b416a69bc8f
billingsley-john <johnbillingsley1996@yahoo.com>
2020-11-23 11:02:55 +0000

    added global test shape and default parameter test

--- tests/test_parametric_components/test_BlanketConstantThicknessArcH.py
+++ tests/test_parametric_components/test_BlanketConstantThicknessArcH.py
@@ -6,6 +6,23 @@ import pytest
 
 
 class test_BlanketConstantThicknessArcH(unittest.TestCase):
+
+    def setUp(self):
+        self.test_shape = paramak.BlanketConstantThicknessArcH(
+            inner_lower_point=(300, -200),
+            inner_mid_point=(500, 0),
+            inner_upper_point=(300, 200),
+            thickness=20,
+        )
+
+    def test_default_parameters(self):
+        """Checks that the default parameters of a BlanketConstantThicknessArcH are correct."""
+
+        assert self.test_shape.rotation_angle == 360
+        assert self.test_shape.stp_filename == "BlanketConstantThicknessArcH.stp"
+        assert self.test_shape.stl_filename == "BlanketConstantThicknessArcH.stl"
+        assert self.test_shape.material_tag = "blanket_mat"
+
     def test_BlanketConstantThickness_creation(self):
         """Creates a blanket using the BlanketConstantThicknessArcH parametric
         component and checks that a cadquery solid is created."""
