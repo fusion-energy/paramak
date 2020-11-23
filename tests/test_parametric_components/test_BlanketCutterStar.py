5711fba987d7495779e34b704140562369b7e0d7
billingsley-john <johnbillingsley1996@yahoo.com>
2020-11-23 12:39:47 +0000

    added points calculation test

--- tests/test_parametric_components/test_BlanketCutterStar.py
+++ tests/test_parametric_components/test_BlanketCutterStar.py
@@ -11,7 +11,7 @@ class test_BlanketCutterStar(unittest.TestCase):
         self.test_shape = paramak.BlanketCutterStar(distance=100)
 
     def test_default_parameters(self):
-        """Checks that the default parameters of a BlanketCutterStrar are correct."""
+        """Checks that the default parameters of a BlanketCutterStar are correct."""
 
         assert self.test_shape.azimuth_placement_angle == [0., 36., 72., 108., 144., 180.,
                                                            216., 252., 288., 324.]
@@ -21,6 +21,14 @@ class test_BlanketCutterStar(unittest.TestCase):
         assert self.test_shape.stl_filename == "BlanketCutterStar.stl"
         assert self.test_shape.name == "blanket_cutter_star"
         assert self.test_shape.material_tag == "blanket_cutter_star_mat"
+
+    def test_points_calculation(self):
+        """Checks that the points used to construct the BlanketCutterStar component
+        are calculated correctly from the parameters given."""
+
+        assert self.test_shape.points == [(0, -1000, "straight"), (2000, -1000, "straight"), 
+                                          (2000, 1000, "straight"), (0, 1000, "straight"),
+                                          (0, -1000, "straight")]
 
     def test_BlanketCutterStar_creation(self):
         """Creates a solid using the BlanketCutterStar parametric component
