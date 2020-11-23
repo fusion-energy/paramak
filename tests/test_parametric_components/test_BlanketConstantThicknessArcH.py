6e0803cc734215a2affb21fd8ace51e03fb509f7
billingsley-john <johnbillingsley1996@yahoo.com>
2020-11-23 11:18:21 +0000

    now using global component and added point calculation test

--- tests/test_parametric_components/test_BlanketConstantThicknessArcH.py
+++ tests/test_parametric_components/test_BlanketConstantThicknessArcH.py
@@ -23,52 +23,41 @@ class test_BlanketConstantThicknessArcH(unittest.TestCase):
         assert self.test_shape.stl_filename == "BlanketConstantThicknessArcH.stl"
         assert self.test_shape.material_tag = "blanket_mat"
 
-    def test_BlanketConstantThickness_creation(self):
+    def test_point_calculation(self):
+        """Checks that the points used to construct the BlanketConstantThicknessArcH component
+        are calculated correctly from the parameters given."""
+
+        assert self.test_shape.points = [
+            (300, 200, "circle"),
+            (500, 0, "circle"),
+            (300, -200, "straight"),
+            (320, -200, "circle"),
+            (520, 0, "circle"),
+            (320, 200, "straight")
+        ]
+
+    def test_component_creation(self):
         """Creates a blanket using the BlanketConstantThicknessArcH parametric
         component and checks that a cadquery solid is created."""
-
-        test_shape = paramak.BlanketConstantThicknessArcH(
-            inner_lower_point=(300, -200),
-            inner_mid_point=(500, 0),
-            inner_upper_point=(300, 200),
-            thickness=20,
-            rotation_angle=180,
-        )
 
         assert test_shape.solid is not None
         assert test_shape.volume > 1000
 
-    def test_BlanketConstantThickness_volume(self):
-        """creates two blankets using the BlanketConstantThicknessArcV parametric
-        component and checks that their relative volumes are correct"""
+    def test_relative_shape_volume(self):
+        """Creates two blankets using the BlanketConstantThicknessArcH parametric component
+        and checks that their relative volumes are correct."""
 
-        # rotation_angle
-        test_shape = paramak.BlanketConstantThicknessArcH(
-            inner_lower_point=(300, -200),
-            inner_mid_point=(500, 0),
-            inner_upper_point=(300, 200),
-            thickness=20,
-            rotation_angle=360,
-        )
-        test_shape_volume = test_shape.volume
+        test_volume = self.test_shape.volume
+        self.test_shape.rotation_angle = 180
+        assert test_volume == pytest.aprox(self.test_shape.volume * 2)
 
-        test_shape.rotation_angle = 180
-        assert test_shape.volume == pytest.approx(test_shape_volume * 0.5)
+    def test_shape_face_areas(self):
+        """Creates a blanket using the BlanketConstantThicknessArcH parametric component and
+        checks that the face areas are expected."""
 
-    def test_BlanketConstantThickness_faces(self):
-        """Creates a blanket using the BlanketConstantThicknessArcH parametric
-        component and checks that a solid with the correct number of faces is produced"""
+        assert len(self.test_shape.areas) == 4
+        assert len(set([round(i) for i in self.test_shape.areas])) == 3
 
-        test_shape = paramak.BlanketConstantThicknessArcH(
-            inner_lower_point=(300, -200),
-            inner_mid_point=(500, 0),
-            inner_upper_point=(300, 200),
-            thickness=20,
-            rotation_angle=360,
-        )
-        assert len(test_shape.areas) == 4
-        assert len(set(test_shape.areas)) == 3
-
-        test_shape.rotation_angle = 180
-        assert len(test_shape.areas) == 6
-        # assert len(set(test_shape.areas)) == 4
+        self.test_shape.rotation_angle = 180
+        assert len(self.test_shape.areas) == 6
+        assert len(set[round(i) for i in self.test_shape.areas]) == 4
