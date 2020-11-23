0406bbdf04f1c51641e6ae60b150749d535e7de1
Jonathan Shimwell <jonathan.shimwell@ukaea.uk>
2020-11-23 10:19:43 +0000

    version number increase

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="paramak",
-    version="0.0.19",
+    version="0.1.0",
     author="The Paramak Development Team",
     author_email="jonathan.shimwell@ukaea.uk",
     description="Create 3D fusion reactor CAD models based on input parameters",
