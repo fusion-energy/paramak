a2d469900a0213c2d5182773ef74c0b2890e4d61
billingsley-john <johnbillingsley1996@yahoo.com>
2020-11-23 11:26:30 +0000

    defined global component and added default parameter test

--- tests/test_parametric_components/test_BlanketConstantThicknessArcV.py
+++ tests/test_parametric_components/test_BlanketConstantThicknessArcV.py
@@ -6,6 +6,24 @@ import pytest
 
 
 class test_BlanketConstantThicknessArcV(unittest.TestCase):
+
+    def setUp(self):
+        self.test_shape = paramak.BlanketConstantThicknessArcH(
+            inner_lower_point=(300, -200),
+            inner_mid_point=(500, 0),
+            inner_upper_point=(300, 200),
+            thickness=20,
+            rotation_angle=180,
+        )
+    
+    def test_default_parameters(self):
+        """Checks that the default parameters of a BlanketConstantThicknessArcV are correct."""
+
+        assert self.test_shape.rotation_angle == 360
+        assert self.test_shape.stp_filename == "BlanketConstantThicknessArcV.stp"
+        assert self.test_shape.stl_filename == "BlanketConstantThicknessArcV.stl"
+        assert self.test_shape.material_tag == "blanket_mat"
+    
     def test_BlanketConstantThickness_creation(self):
         """Creates a blanket using the BlanketConstantThicknessArcV parametric
         component and checks that a cadquery solid is created."""
