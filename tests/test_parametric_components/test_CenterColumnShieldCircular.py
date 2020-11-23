57b3f45362ed86cb113306e838f79aa71e808b5b
billingsley-john <johnbillingsley1996@yahoo.com>
2020-11-23 13:31:00 +0000

    added global parametric component

--- tests/test_parametric_components/test_CenterColumnShieldCircular.py
+++ tests/test_parametric_components/test_CenterColumnShieldCircular.py
@@ -5,6 +5,12 @@ import paramak
 
 
 class test_CenterColumnShieldCircular(unittest.TestCase):
+
+    def setUp(self):
+        self.test_shape = paramak.CenterColumnShieldCircular(
+            height=600, inner_radius=100, mid_radius=150, outer_radius=200
+        )
+
     def test_CenterColumnShieldCircular_creation(self):
         """Creates a center column shield using the CenterColumnShieldCircular
         parametric component and checks that a cadquery solid is created."""
