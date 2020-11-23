fbd308908f565615ac41e9e841aff51095ece9c5
billingsley-john <johnbillingsley1996@yahoo.com>
2020-11-23 12:17:35 +0000

    added global test component and updated assertions

--- tests/test_parametric_components/test_BlanketCutterStar.py
+++ tests/test_parametric_components/test_BlanketCutterStar.py
@@ -6,25 +6,23 @@ import pytest
 
 
 class test_BlanketCutterStar(unittest.TestCase):
+
+    def setUp(self):
+        self.test_shape = paramak.BlanketCutterStar(distance=100)
+
     def test_BlanketCutterStar_creation(self):
         """Creates a solid using the BlanketCutterStar parametric component
         and checks that a cadquery solid is created."""
 
-        test_shape = paramak.BlanketCutterStar(
-            distance=100)
-
-        assert test_shape.solid is not None
-        assert test_shape.volume > 1000
+        assert self.test_shape.solid is not None
+        assert self.test_shape.volume > 1000
 
     def test_BlanketCutterStar_distance_volume_impact(self):
         """Creates solid using the BlanketCutterStar parametric component
         with different distances and checks that the volume changes accordingly
         ."""
 
-        small_shape = paramak.BlanketCutterStar(distance=50)
-
-        large_shape = paramak.BlanketCutterStar(distance=100)
-
-        # not quite two times as big as there is overlap in the center
-        assert 2 * \
-            small_shape.volume == pytest.approx(large_shape.volume, rel=0.1)
+        test_volume = self.test_shape.volume
+        self.test_shape.distance=50
+        # not quite two times as large as there is overlap in the center
+        assert test_volume == pytest.approx(self.test_shape.volume * 2, rel=0.1)
