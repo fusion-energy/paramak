4e9903605e84bcd82136d0f4fdfeb68c13d0f8bc
billingsley-john <johnbillingsley1996@yahoo.com>
2020-11-23 14:15:04 +0000

    updated tests to use global component

--- tests/test_parametric_components/test_CenterColumnShieldCylinder.py
+++ tests/test_parametric_components/test_CenterColumnShieldCylinder.py
@@ -11,7 +11,7 @@ import pytest
 class test_CenterColumnShieldCylinder(unittest.TestCase):
 
     def setUp(self):
-        self.test_shape = CenterColumnShieldCylinder(
+        self.test_shape = paramak.CenterColumnShieldCylinder(
             height=600, inner_radius=100, outer_radius=200
         )
 
@@ -28,85 +28,44 @@ class test_CenterColumnShieldCylinder(unittest.TestCase):
         """Creates a center column shield using the CenterColumnShieldCylinder
         parametric component and checks that a cadquery solid is created."""
 
-        test_shape = paramak.CenterColumnShieldCylinder(
-            height=600, inner_radius=100, outer_radius=200
-        )
-
-        assert test_shape.solid is not None
-        assert test_shape.volume > 1000
+        assert self.test_shape.solid is not None
+        assert self.test_shape.volume > 1000
 
     def test_CenterColumnShieldCylinder_relative_volume(self):
         """Creates CenterColumnShieldCylinder shapes and checks that their
         relative volumes are correct"""
 
-        test_shape = paramak.CenterColumnShieldCylinder(
-            rotation_angle=360,
-            height=15,
-            inner_radius=5,
-            outer_radius=10,
-            azimuth_placement_angle=0,
-        )
-
-        assert test_shape.volume == pytest.approx(3534.29)
-
-        test_shape = paramak.CenterColumnShieldCylinder(
-            rotation_angle=180,
-            height=15,
-            inner_radius=5,
-            outer_radius=10,
-            azimuth_placement_angle=95,
-        )
-
-        assert test_shape.volume == pytest.approx(3534.29 / 2.0)
+        test_volume = self.test_shape.volume
+        self.test_shape.rotation_angle = 180
+        assert test_volume == pytest.approx(self.test_shape.volume * 2)
 
     def test_CenterColumnShieldCylinder_absolute_volume(self):
         """Creates a CenterColumnShieldCylinder shape and checks that its
         relative volume is correct"""
 
-        test_shape = paramak.CenterColumnShieldCylinder(
-            inner_radius=20,
-            outer_radius=40,
-            height=200
+        assert self.test_shape.volume == pytest.approx(
+            ((math.pi * (200**2)) - (math.pi * (100**2))) * 600
         )
-
-        assert test_shape.volume == pytest.approx(
-            ((math.pi * (40**2)) - (math.pi * (20**2))) * 200)
 
     def test_CenterColumnShieldCylinder_absolute_area(self):
         """Creates a CenterColumnShieldCylinder shape and checks that the
         areas of the faces of the solid created are correct"""
 
-        test_shape = paramak.CenterColumnShieldCylinder(
-            inner_radius=20,
-            outer_radius=40,
-            height=200
-        )
-
-        assert len(test_shape.areas) == 4
-        assert test_shape.area == pytest.approx((((math.pi * (40**2)) - (math.pi * (
-            20**2))) * 2) + (math.pi * (2 * 40) * 200) + (math.pi * (2 * 20) * 200))
-        assert test_shape.areas.count(pytest.approx(
-            (math.pi * (40**2)) - (math.pi * (20**2)))) == 2
-        assert test_shape.areas.count(pytest.approx(math.pi * (2 * 40) * 200))
+        assert len(self.test_shape.areas) == 4
+        assert self.test_shape.area == pytest.approx((((math.pi * (200**2)) - (math.pi * (
+            100**2))) * 2) + (math.pi * (2 * 200) * 600) + (math.pi * (2 * 100) * 600))
+        assert self.test_shape.areas.count(pytest.approx(
+            (math.pi * (200**2)) - (math.pi * (100**2)))) == 2
+        assert self.test_shape.areas.count(pytest.approx(math.pi * (2 * 200) * 600)) == 1
+        assert self.test_shape.areas.count(pytest.approx(math.pi * (2 * 100) * 600)) == 1
 
     def test_export_stp_CenterColumnShieldCylinder(self):
         """Creates a CenterColumnShieldCylinder shape and checks that a stp
         file of the shape can be exported using the export_stp method."""
 
-        test_shape = paramak.CenterColumnShieldCylinder(
-            rotation_angle=360,
-            height=15,
-            inner_radius=5,
-            outer_radius=10,
-            azimuth_placement_angle=0,
-        )
-
         os.system("rm center_column_shield.stp")
-
-        test_shape.export_stp("center_column_shield.stp")
-
-        assert Path("center_column_shield.stp").exists()
-
+        self.test_shape.export_stp("center_column_shield.stp")
+        assert Path("center_column_shield.stp").exists() == True
         os.system("rm center_column_shield.stp")
 
     def test_parametric_component_hash_value(self):
@@ -120,38 +79,29 @@ class test_CenterColumnShieldCylinder(unittest.TestCase):
         hash_value of a parametric component is not updated until a new
         cadquery solid has been created."""
 
-        test_shape = paramak.CenterColumnShieldCylinder(
-            height=100,
-            inner_radius=20,
-            outer_radius=40
-        )
+        assert self.test_shape.hash_value is None
+        assert self.test_shape.solid is not None
+        assert self.test_shape.hash_value is not None
+        initial_hash_value = self.test_shape.hash_value
+        assert self.test_shape.solid is not None
+        assert initial_hash_value == self.test_shape.hash_value
+        self.test_shape.height = 120
+        assert initial_hash_value == self.test_shape.hash_value
+        assert self.test_shape.solid is not None
+        assert initial_hash_value != self.test_shape.hash_value
 
-        assert test_shape.hash_value is None
-        assert test_shape.solid is not None
-        assert test_shape.hash_value is not None
-        initial_hash_value = test_shape.hash_value
-        assert test_shape.solid is not None
-        assert initial_hash_value == test_shape.hash_value
-        test_shape.height = 120
-        assert initial_hash_value == test_shape.hash_value
-        assert test_shape.solid is not None
-        assert initial_hash_value != test_shape.hash_value
+    def test_center_column_shield_cylinder_invalid_parameters_errors(self):
+        """Checks that the correct errors are raised when invalid arguments are entered
+        as shape parameters."""
 
-    def test_center_column_shield_cylinder_error(self):
         def incorrect_radii():
-            test_shape = paramak.CenterColumnShieldCylinder(
-                height=100,
-                inner_radius=40,
-                outer_radius=20
-            )
-            test_shape.solid
+            self.test_shape.inner_radius = 40
+            self.test_shape.outer_radius = 20
+            self.test_shape.solid
 
         def incorrect_height():
-            test_shape = paramak.CenterColumnShieldCylinder(
-                height=None,
-                inner_radius=20,
-                outer_radius=40
-            )
-            test_shape.solid
+            self.test_shape.height = None 
+            self.test_shape.solid
+
         self.assertRaises(ValueError, incorrect_radii)
         self.assertRaises(ValueError, incorrect_height)
