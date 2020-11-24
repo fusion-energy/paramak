63e5f1754272ee4fb617c1b5ce97c91754251c59
billingsley-john <johnbillingsley1996@yahoo.com>
2020-11-24 09:56:15 +0000

    added default parameters test

--- tests/test_parametric_components/test_PoloidalFieldCoilCase.py
+++ tests/test_parametric_components/test_PoloidalFieldCoilCase.py
@@ -15,6 +15,14 @@ class test_PoloidalFieldCoilCase(unittest.TestCase):
             coil_width=50,
             center_point=(1000, 500)
         )
+
+    def test_default_parameters(self):
+        """Checks that the default parameters of a PoloidalFieldCoilCase are correct."""
+
+        assert self.test_shape.rotation_angle == 360
+        assert self.test_shape.stp_filename == "PoloidalFieldCoilCase.stp"
+        assert self.test_shape.stl_filename == "PoloidalFieldCoilCase.stl"
+        assert self.test_shape.material_tag == "pf_coil_case_mat"
     
     def test_PoloidalFieldCoilCase_creation(self):
         """Creates a pf coil case using the PoloidalFieldCoilCase parametric
