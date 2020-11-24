7ccd5114aa5f8e67764d2bbfef43de03455f3cd8
billingsley-john <johnbillingsley1996@yahoo.com>
2020-11-24 16:00:47 +0000

    added global component and updated tests

--- tests/test_parametric_components/test_PoloidalFieldCoilSet.py
+++ tests/test_parametric_components/test_PoloidalFieldCoilSet.py
@@ -8,60 +8,50 @@ import pytest
 
 class test_PoloidalFieldCoilSet(unittest.TestCase):
 
+    def setUp(self):
+        self.test_shape = paramak.PoloidalFieldCoilSet(
+            heights=[10, 15, 5],
+            widths=[20, 25, 30],
+            center_points=[(100, 100), (200, 200), (300, 300)]
+        )
+
     def test_PoloidalFieldCoilSet_creation(self):
         """Creates a solid using the PoloidalFieldCoilSet parametric component
         and checks that a cadquery solid is created."""
 
-        test_shape = paramak.PoloidalFieldCoilSet(heights=[10, 10, 10],
-                                                  widths=[20, 20, 20],
-                                                  center_points=[(100, 100),
-                                                                 (200, 200),
-                                                                 (300, 300)])
-        assert test_shape.solid is not None
-        assert len(test_shape.solid.Solids()) == 3
+        assert self.test_shape.solid is not None
+        assert len(self.test_shape.solid.Solids()) == 3
 
     def test_PoloidalFieldCoilSet_absolute_volume(self):
         """Creates a set of pf coils using the PoloidalFieldCoilSet parametric
-        component and checks that the volume is correct"""
+        component and checks that the volume is correct."""
 
-        test_shape = paramak.PoloidalFieldCoilSet(
-            heights=[10, 15, 5],
-            widths=[20, 25, 30],
-            center_points=[(100, 100), (200, 200), (300, 300)]
-        )
-
-        assert test_shape.volume == (pytest.approx((10 * 20 * math.pi * (2 * 100)) + (
+        assert self.test_shape.volume == (pytest.approx((10 * 20 * math.pi * (2 * 100)) + (
             15 * 25 * math.pi * (2 * 200)) + (5 * 30 * math.pi * (2 * 300))))
 
     def test_PoloidalFieldCoilSet_absolute_areas(self):
         """Creates a set of pf coils using the PoloidalFieldCoilSet parametric
-        component and checks that the areas of its faces are correct"""
+        component and checks that the areas of its faces are correct."""
 
-        test_shape = paramak.PoloidalFieldCoilSet(
-            heights=[10, 15, 5],
-            widths=[20, 25, 30],
-            center_points=[(100, 100), (200, 200), (300, 300)]
-        )
-
-        assert len(test_shape.areas) == 12
-        # assert len(set(test_shape.areas)) == 9
-        assert test_shape.areas.count(
+        assert len(self.test_shape.areas) == 12
+        assert len(set(round(i) for i in self.test_shape.areas)) == 9
+        assert self.test_shape.areas.count(
             pytest.approx(20 * math.pi * (2 * 100))) == 2
-        assert test_shape.areas.count(
+        assert self.test_shape.areas.count(
             pytest.approx(25 * math.pi * (2 * 200))) == 2
-        assert test_shape.areas.count(
+        assert self.test_shape.areas.count(
             pytest.approx(30 * math.pi * (2 * 300))) == 2
-        assert test_shape.areas.count(
+        assert self.test_shape.areas.count(
             pytest.approx(10 * math.pi * (2 * 90))) == 1
-        assert test_shape.areas.count(
+        assert self.test_shape.areas.count(
             pytest.approx(10 * math.pi * (2 * 110))) == 1
-        assert test_shape.areas.count(
+        assert self.test_shape.areas.count(
             pytest.approx(15 * math.pi * (2 * 187.5))) == 1
-        assert test_shape.areas.count(
+        assert self.test_shape.areas.count(
             pytest.approx(15 * math.pi * (2 * 212.5))) == 1
-        assert test_shape.areas.count(
+        assert self.test_shape.areas.count(
             pytest.approx(5 * math.pi * (2 * 285))) == 1
-        assert test_shape.areas.count(
+        assert self.test_shape.areas.count(
             pytest.approx(5 * math.pi * (2 * 315))) == 1
 
     def test_PoloidalFieldCoilSet_incorrect_args(self):
@@ -72,9 +62,9 @@ class test_PoloidalFieldCoilSet(unittest.TestCase):
             """Checks PoloidalFieldCoilSet with height as the wrong type."""
 
             paramak.PoloidalFieldCoilSet(
-                heights=10, widths=[
-                    20, 20, 20], center_points=[
-                    (100, 100), (200, 200), (300, 300)])
+                heights=10, 
+                widths=[20, 20, 20], 
+                center_points=[(100, 100), (200, 200), (300, 300)])
 
         self.assertRaises(
             ValueError,
@@ -84,9 +74,9 @@ class test_PoloidalFieldCoilSet(unittest.TestCase):
             """Checks PoloidalFieldCoilSet with width as the wrong type."""
 
             paramak.PoloidalFieldCoilSet(
-                heights=[
-                    10, 10, 10], widths=20, center_points=[
-                    (100, 100), (200, 200), (300, 300)])
+                heights=[10, 10, 10], 
+                widths=20, 
+                center_points=[(100, 100), (200, 200), (300, 300)])
 
         self.assertRaises(
             ValueError,
@@ -96,9 +86,10 @@ class test_PoloidalFieldCoilSet(unittest.TestCase):
             """Checks PoloidalFieldCoilSet with center_points as the wrong
             type."""
 
-            paramak.PoloidalFieldCoilSet(heights=[10, 10, 10],
-                                         widths=[20, 20, 20],
-                                         center_points=100)
+            paramak.PoloidalFieldCoilSet(
+                heights=[10, 10, 10],
+                widths=[20, 20, 20],
+                center_points=100)
 
         self.assertRaises(
             ValueError,
@@ -107,10 +98,9 @@ class test_PoloidalFieldCoilSet(unittest.TestCase):
         def test_PoloidalFieldCoilSet_incorrect_width_length():
             """Checks PoloidalFieldCoilSet with not enough entries in width."""
             paramak.PoloidalFieldCoilSet(
-                heights=[
-                    10, 10, 10], widths=[
-                    20, 20], center_points=[
-                    (100, 100), (200, 200), (300, 300)])
+                heights=[10, 10, 10], 
+                widths=[20, 20], 
+                center_points=[(100, 100), (200, 200), (300, 300)])
 
         self.assertRaises(
             ValueError,
