05b59b5809dd747175197b6f4f3ed6cd8810ef62
billingsley-john <johnbillingsley1996@yahoo.com>
2020-11-24 16:11:41 +0000

    added default parameters test

--- tests/test_parametric_components/test_PoloidalFieldCoilSet.py
+++ tests/test_parametric_components/test_PoloidalFieldCoilSet.py
@@ -15,6 +15,15 @@ class test_PoloidalFieldCoilSet(unittest.TestCase):
             center_points=[(100, 100), (200, 200), (300, 300)]
         )
 
+    def test_default_parameters(self):
+        """Checks that the default parameters of a PoloidalFieldCoilSet are correct."""
+
+        assert self.test_shape.rotation_angle == 360
+        assert self.test_shape.stp_filename == "PoloidalFieldCoilSet.stp"
+        assert self.test_shape.stl_filename == "PoloidalFieldCoilSet.stl"
+        # assert self.test_shape.name == "pf_coil"
+        assert self.test_shape.material_tag == "pf_coil_mat"
+
     def test_PoloidalFieldCoilSet_creation(self):
         """Creates a solid using the PoloidalFieldCoilSet parametric component
         and checks that a cadquery solid is created."""
