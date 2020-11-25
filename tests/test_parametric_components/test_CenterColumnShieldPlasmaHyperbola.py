365de57f0499f5b905bfc026d9e787d8b0a8e3a9
billingsley-john <johnbillingsley1996@yahoo.com>
2020-11-25 09:53:25 +0000

    added points calculation test

--- tests/test_parametric_components/test_CenterColumnShieldPlasmaHyperbola.py
+++ tests/test_parametric_components/test_CenterColumnShieldPlasmaHyperbola.py
@@ -33,6 +33,16 @@ class test_CenterColumnShieldPlasmaHyperbola(unittest.TestCase):
         assert self.test_shape.solid is not None
         assert self.test_shape.volume > 1000
 
+    def test_points_calculation(self):
+        """Checks that the points used to construct the CenterColumnShieldPlasmaHyperbola
+        component are calculated correctly fro the parameters given."""
+
+        assert self.test_shape.points == [
+            (100, 0, 'straight'), (100, 400.0, 'straight'), (337.5, 400.0, 'straight'), 
+            (337.5, 300.0, 'spline'), (260.0, 0.0, 'spline'), (337.5, -300.0, 'straight'), 
+            (337.5, -400.0, 'straight'), (100, -400.0, 'straight'), (100, 0, 'straight')
+        ]
+
     def test_CenterColumnShieldPlasmaHyperbola_invalid_parameters_errors(self):
         """Checks that the correct errors are raised when invalid arguments are input as
         shape parameters."""
