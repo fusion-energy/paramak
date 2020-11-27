d531619365f03a025f095abb122a04fd91962c59
Jonathan Shimwell <jonathan.shimwell@ukaea.uk>
2020-11-27 16:31:30 +0000

    added missing --cov

--- run_tests.sh
+++ run_tests.sh
@@ -1,5 +1,5 @@
-pytest tests/test_neutronics_utils.py -v --cov=paramak --cov-report term --cov-report xml
-pytest tests/test_utils.py -v --cov=paramak --cov-report term --cov-report xml
+pytest tests/test_neutronics_utils.py -v --cov=paramak --cov-append --cov-report term --cov-report xml
+pytest tests/test_utils.py -v --cov=paramak --cov-append --cov-report term --cov-report xml
 pytest tests/test_Shape.py -v --cov=paramak --cov-append --cov-report term --cov-report xml
 pytest tests/test_Reactor.py -v --cov=paramak --cov-append --cov-report term --cov-report xml
 pytest tests/test_parametric_shapes/ -v --cov=paramak --cov-append --cov-report term --cov-report xml
