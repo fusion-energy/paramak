4ef2de271c585f376d16bd369fc0d048c7626c22
Jonathan Shimwell <jonathan.shimwell@ukaea.uk>
2020-11-29 22:03:31 +0000

    refactored tests using setUp to duplicate shape

--- tests/test_parametric_neutronics/test_Shape_neutronics.py
+++ tests/test_parametric_neutronics/test_Shape_neutronics.py
@@ -8,11 +8,8 @@ import paramak
 
 class test_object_properties(unittest.TestCase):
 
-    def test_export_h5m_creates_file(self):
-        """Tests the Shape.export_h5m method results in an outputfile."""
-
-        os.system('rm test_shape.h5m')
-        test_shape = paramak.ExtrudeMixedShape(
+    def setUp(self):
+        self.test_shape = paramak.ExtrudeMixedShape(
             points=[
                 (50, 0, "straight"),
                 (50, 50, "spline"),
@@ -22,74 +19,38 @@ class test_object_properties(unittest.TestCase):
                 (70, 0, "straight")],
             distance=50
         )
-        test_shape.export_h5m(filename='test_shape.h5m')
+
+    def test_export_h5m_creates_file(self):
+        """Tests the Shape.export_h5m method results in an outputfile."""
+        os.system('rm test_shape.h5m')
+        self.test_shape.export_h5m(filename='test_shape.h5m')
         assert Path("test_shape.h5m").exists() is True
 
     def test_offset_from_graveyard_sets_attribute(self):
         os.system('rm test_shape.h5m')
-        test_shape = paramak.ExtrudeMixedShape(
-            points=[
-                (50, 0, "straight"),
-                (50, 50, "spline"),
-                (60, 70, "spline"),
-                (70, 50, "circle"),
-                (60, 25, "circle"),
-                (70, 0, "straight")],
-            distance=50
-        )
-        test_shape.export_h5m(filename='test_shape.h5m', graveyard_offset=101)
-        assert test_shape.graveyard_offset == 101
+        self.test_shape.export_h5m(filename='test_shape.h5m', graveyard_offset=101)
+        assert self.test_shape.graveyard_offset == 101
 
     def test_tolerance_increases_filesize(self):
         os.system('rm test_shape.h5m')
-        test_shape = paramak.ExtrudeMixedShape(
-            points=[
-                (50, 0, "straight"),
-                (50, 50, "spline"),
-                (60, 70, "spline"),
-                (70, 50, "circle"),
-                (60, 25, "circle"),
-                (70, 0, "straight")],
-            distance=50
-        )
-        test_shape.export_h5m(filename='test_shape_0001.h5m', tolerance=0.001)
-        test_shape.export_h5m(filename='test_shape_001.h5m', tolerance=0.01)
+        self.test_shape.export_h5m(filename='test_shape_0001.h5m', tolerance=0.001)
+        self.test_shape.export_h5m(filename='test_shape_001.h5m', tolerance=0.01)
         assert Path('test_shape_0001.h5m').stat().st_size > Path(
             'test_shape_001.h5m').stat().st_size
 
     def test_skipping_graveyard_decreases_filesize(self):
         os.system('rm test_shape.h5m')
-        test_shape = paramak.ExtrudeMixedShape(
-            points=[
-                (50, 0, "straight"),
-                (50, 50, "spline"),
-                (60, 70, "spline"),
-                (70, 50, "circle"),
-                (60, 25, "circle"),
-                (70, 0, "straight")],
-            distance=50
-        )
-        test_shape.export_h5m(filename='skiped.h5m', skip_graveyard=True)
-        test_shape.export_h5m(filename='not_skipped.h5m', skip_graveyard=False)
+        self.test_shape.export_h5m(filename='skiped.h5m', skip_graveyard=True)
+        self.test_shape.export_h5m(filename='not_skipped.h5m', skip_graveyard=False)
         assert Path('not_skipped.h5m').stat().st_size > Path(
             'skiped.h5m').stat().st_size
 
     def test_graveyard_offset_increases_voulme(self):
         os.system('rm test_shape.h5m')
-        test_shape = paramak.ExtrudeMixedShape(
-            points=[
-                (50, 0, "straight"),
-                (50, 50, "spline"),
-                (60, 70, "spline"),
-                (70, 50, "circle"),
-                (60, 25, "circle"),
-                (70, 0, "straight")],
-            distance=50
-        )
-        test_shape.make_graveyard(graveyard_offset=100)
-        small_offset = test_shape.graveyard.volume
-        test_shape.make_graveyard(graveyard_offset=1000)
-        large_offset = test_shape.graveyard.volume
+        self.test_shape.make_graveyard(graveyard_offset=100)
+        small_offset = self.test_shape.graveyard.volume
+        self.test_shape.make_graveyard(graveyard_offset=1000)
+        large_offset = self.test_shape.graveyard.volume
         assert small_offset < large_offset
 
 
