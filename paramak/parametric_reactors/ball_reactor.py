cd50744bcccf117eb176f13ca922ec0f228c2e14
autopep8 <autopep8@users.noreply.github.com>
2020-11-29 23:50:29 +0000

    Automated autopep8 fixes

--- paramak/parametric_reactors/ball_reactor.py
+++ paramak/parametric_reactors/ball_reactor.py
@@ -151,7 +151,7 @@ class BallReactor(paramak.Reactor):
             self.outer_plasma_gap_radial_thickness,
             self.plasma_gap_vertical_thickness,
             self.major_radius - self.minor_radius]
-        
+
         # calcualted internally by the class
         self._plasma = None
 
