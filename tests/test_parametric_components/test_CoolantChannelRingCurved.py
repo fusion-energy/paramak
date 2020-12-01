d9305ee288226728ca3d99b3d00c39f2ece2c03f
billingsley-john <johnbillingsley1996@yahoo.com>
2020-12-01 16:26:48 +0000

    added start angle test

--- tests/test_parametric_components/test_CoolantChannelRingCurved.py
+++ tests/test_parametric_components/test_CoolantChannelRingCurved.py
@@ -13,13 +13,14 @@ class TestCoolantChannelRingCurved(unittest.TestCase):
             channel_radius=10,
             ring_radius=70,
             mid_offset=-20,
-            number_of_coolant_channels=8
+            number_of_coolant_channels=6
         )
 
     def test_default_parameters(self):
         """Checks that the default parameters of a CoolantChannelRingCurved are correct."""
 
         # assert self.test_shape.rotation_angle == 360
+        assert self.test_shape.start_angle == 0
         assert self.test_shape.stp_filename == "CoolantChannelRingCurved.stp"
         assert self.test_shape.stl_filename == "CoolantChannelRingCurved.stl"
         assert self.test_shape.material_tag == "coolant_channel_mat"
@@ -36,7 +37,7 @@ class TestCoolantChannelRingCurved(unittest.TestCase):
         and checks the relative volumes are correct."""
 
         test_volume = self.test_shape.volume
-        self.test_shape.number_of_coolant_channels = 4
+        self.test_shape.number_of_coolant_channels = 3
         assert test_volume == pytest.approx(self.test_shape.volume * 2)
 
         test_volume = self.test_shape.volume
@@ -45,5 +46,14 @@ class TestCoolantChannelRingCurved(unittest.TestCase):
         self.test_shape.force_cross_section = True
         assert test_volume < self.test_shape.volume
 
-    # need to add check to warn/raise error when there is coolant channel
-    # overlap and a test
+    def test_start_angle(self):
+        """Checks that the coolant channels are placed at the correct azimuthal placement
+        angles for a given start angle."""
+
+        assert self.test_shape.azimuth_placement_angle == [
+            0, 60, 120, 180, 240, 300
+        ]
+        self.test_shape.start_angle = 10
+        assert self.test_shape.azimuth_placement_angle == [
+            10, 70, 130, 190, 250, 310
+        ]
