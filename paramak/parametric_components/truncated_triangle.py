9cc78ebd606b8a5ce10dd411d931cc5a719e11ca
Jonathan Shimwell <jonathan.shimwell@ukaea.uk>
2020-12-11 23:32:24 +0000

    add volume based tests for truncated triangle

--- paramak/parametric_components/truncated_triangle.py
+++ paramak/parametric_components/truncated_triangle.py
@@ -1,4 +1,6 @@
 
+import math
+
 from paramak import RotateStraightShape
 from paramak.utils import rotate
 
@@ -12,8 +14,8 @@ class TruncatedTriangle(RotateStraightShape):
         l3 (float): the center of the coil (x,z) values
         pivot_point (tuple of floats): the coordinates of the center of
             rotation (x,z) (cm).
-        pivot_angle (float, optional): the angle to pivot (rotate) the shape by
-            on the XY plane. Defaults to 0.
+        pivot_angle (float, optional): the angle (in degrees) to pivot (rotate) 
+            the shape by on the XY plane. Defaults to 0.
         stp_filename (str, optional): defaults to "TruncatedTriangle.stp".
         stl_filename (str, optional): defaults to "TruncatedTriangle.stl".
         name (str, optional): defaults to "truncated_tri".
@@ -26,6 +28,7 @@ class TruncatedTriangle(RotateStraightShape):
         l2,
         l3,
         pivot_point,
+        pivot_angle=0.,
         stp_filename="TruncatedTriangle.stp",
         stl_filename="TruncatedTriangle.stl",
         name="truncated_tri",
@@ -45,6 +48,7 @@ class TruncatedTriangle(RotateStraightShape):
         self.l2 = l2
         self.l3 = l3
         self.pivot_point = pivot_point
+        self.pivot_angle = pivot_angle
 
     @property
     def center_point(self):
@@ -74,7 +78,7 @@ class TruncatedTriangle(RotateStraightShape):
         """Finds the XZ points joined by straight connections that describe
         the 2D profile of the poloidal field coil shape."""
 
-        points = [
+        non_rotated_points = [
             (
                 self.pivot_point[0] + self.l1 / 2.0,
                 self.pivot_point[1]
@@ -92,8 +96,15 @@ class TruncatedTriangle(RotateStraightShape):
                 self.pivot_point[1] - self.l3
             ),
         ]
+
+        points = []
        
-        for point in points:
-            point = rotate(origin=pivot_point, point=point, angle=self.pivot_angle)
+        for point in non_rotated_points:
+            x, y = rotate(
+                origin=self.pivot_point,
+                point=point,
+                angle=math.radians(self.pivot_angle)
+            )
+            points.append((x, y))
 
         self.points = points
---
+++ tests/test_parametric_components/test_TruncatedTriangle.py
@@ -0,0 +1,76 @@
+
+
+import unittest
+
+import paramak
+import pytest
+
+
+class TruncatedTriangle(unittest.TestCase):
+
+    def setUp(self):
+        self.test_shape = paramak.TruncatedTriangle(
+            l1=10,
+            l2=20,
+            l3=30,
+            pivot_angle=0,
+            pivot_point=(100,50),
+            rotation_angle=45,
+        )
+
+
+    def test_component_creation(self):
+        """Creates a TruncatedTriangle object and checks that the .solid is not None."""
+
+        assert self.test_shape.solid is not None
+
+
+    def test_check_number_of_surfaces(self):
+        """Counts the surfaces in a fully rotated and partly rotated TruncatedTriangle."""
+
+        assert len(self.test_shape.areas) == 6
+
+        self.test_shape.rotation_angle = 360
+
+        assert len(self.test_shape.areas) == 4
+
+ 
+    def test_args_do_not_impact_volume(self):
+        """Changes args that should not impact the volume and checks that they don't"""
+
+        test_shape_vol = self.test_shape.volume
+
+        self.test_shape.pivot_angle = 180
+
+        assert pytest.approx(self.test_shape.volume, rel=0.01) == test_shape_vol
+
+        self.test_shape.pivot_point = (100, 300)
+
+        assert pytest.approx(self.test_shape.volume, rel=0.01) == test_shape_vol
+
+
+    def test_args_impact_volume(self):
+        """Changes args that should impact the volume and checks that they do"""
+
+        test_shape_vol = self.test_shape.volume
+        
+        self.test_shape.pivot_angle=45
+        
+        assert self.test_shape.volume > test_shape_vol
+        
+        self.test_shape.pivot_angle=0
+
+        self.test_shape.l1=42
+
+        assert self.test_shape.volume > test_shape_vol
+
+        self.test_shape.l1=10
+        self.test_shape.l2=42
+
+        assert self.test_shape.volume > test_shape_vol
+
+        self.test_shape.l1=10
+        self.test_shape.l2=20
+        self.test_shape.l3=42
+
+        assert self.test_shape.volume > test_shape_vol
