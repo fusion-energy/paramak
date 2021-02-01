96a673f69fad40acbccea1ba6f37e9303e4e3d79
Jonathan Shimwell <jonathan.shimwell@ukaea.uk>
2021-02-01 19:22:24 +0000

    added test for wrong view plane

--- tests/test_example_shapes.py
+++ tests/test_example_shapes.py
@@ -7,7 +7,10 @@ from pathlib import Path
 from examples.example_parametric_shapes import (
     make_blanket_from_parameters, make_blanket_from_points,
     make_CAD_from_points, make_can_reactor_from_parameters,
-    make_can_reactor_from_points, make_html_diagram_from_stp_file)
+    make_can_reactor_from_points, make_html_diagram_from_stp_file,
+    )
+
+import paramak
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'examples'))
 
@@ -98,6 +101,36 @@ class TestExampleShapes(unittest.TestCase):
         for output_filename in output_filenames:
             assert Path(output_filename).exists() is True
             os.system("rm " + output_filename)
+
+    def test_incorrect_view_plane(self):
+        """Checks than an error is raised when incorrect values of the
+        view_plane is set
+        """
+
+        def set_value():
+            example_shape = paramak.ExtrudeMixedShape(
+                distance=1,
+                points=[
+                    (100, 0, "straight"),
+                    (200, 0, "circle"),
+                    (250, 50, "circle"),
+                    (200, 100, "straight"),
+                    (150, 100, "spline"),
+                    (140, 75, "spline"),
+                    (110, 45, "spline"),
+                ]
+            )
+
+            paramak.utils.export_wire_to_html(
+                wires=example_shape.wire,
+                tolerance=0.1,
+                view_plane="coucou",
+                facet_splines=True,
+                facet_circles=True,
+                filename="example_shape_from_stp.html",
+            )
+
+        self.assertRaises(ValueError, set_value)
 
 
 if __name__ == "__main__":
