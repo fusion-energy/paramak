04bb692a71fb92c0b051d3ced702bba969e54ef9
bruceey42 <48457426+bruceey42@users.noreply.github.com>
2021-02-05 19:34:38 +0100

    Update test_HexagonPin.py
    Made changes to the tests with helpful instruction from the Jo(h)ns.

--- tests/test_parametric_components/test_HexagonPin.py
+++ tests/test_parametric_components/test_HexagonPin.py
@@ -6,7 +6,7 @@ import paramak
 import pytest
 
 
-class TestPoloidalFieldCoil(unittest.TestCase):
+class TestHexagonPin(unittest.TestCase):
 
     def setUp(self):
         self.test_shape = paramak.HexagonPin(
@@ -18,10 +18,10 @@ class TestPoloidalFieldCoil(unittest.TestCase):
         assert self.test_shape.length_of_side == 5
         assert self.test_shape.distance == 42.
         assert self.test_shape.center_point == (0, 0)
-        assert self.test_shape.stp_filename == "put_correct_value_here"
-        # assert self.test_shape.stl_filename == "put_correct_value_here"
-        # assert self.test_shape.name == "put_correct_value_here"
-        # assert self.test_shape.material_tag == "put_correct_value_here"
+        assert self.test_shape.stp_filename == "HexagonPin.stp"
+        assert self.test_shape.stl_filename == "HexagonPin.stl"
+        assert self.test_shape.name == "hexagon_pin"
+        assert self.test_shape.material_tag == "hexagon_pin_mat"
 
     def test_volume(self):
         """Checks the volume against the actual value"""
@@ -30,35 +30,31 @@ class TestPoloidalFieldCoil(unittest.TestCase):
         distance = self.test_shape.distance
 
         hexagon_face_area = (3 * math.sqrt(3) / 2) * math.pow(length, 2)
-
         # this needs a pytest.approx() as the volumes are not exact
-        # assert pytest.approx(self.test_shape.volume, rel=0.1) == hexagon_face_area * distance
-        assert self.test_shape.volume == hexagon_face_area * distance
+        assert pytest.approx(self.test_shape.volume,
+        rel=0.1) == hexagon_face_area * distance
 
     def test_distance_impacts_volume(self):
-        """Checks that changing the distance argument results in a the
+        """Checks that changing the distance argument results in the
         expected volume change"""
 
         test_shape_volume = self.test_shape.volume
 
         self.test_shape.distance = self.test_shape.distance * 2
-
-        # this needs a multiplication factor to make them equal
-        assert test_shape_volume == self.test_shape.volume
+        
+        assert pytest.approx(test_shape_volume * 2, 
+        rel=0.1)  == self.test_shape.volume
 
     def test_length_of_sides_impacts_volume(self):
         """Checks that changing the length_of_sides argument results in a the
         expected volume change"""
 
-        # in a similar manner to the previous test the length_of_side should increase the volume
-        # design a test for this check
+        test_shape_volume = self.test_shape.volume
 
-        # test_shape_volume = self.test_shape.volume
+        self.test_shape.length_of_side = self.test_shape.length_of_side * 2
 
-        # self.test_shape.length_of_side = self.test_shape.length_of_side * 2
-
-        # this could be improved as the < is the wrong symbol
-        assert test_shape_volume < self.test_shape.volume
+        assert pytest.approx(test_shape_volume * 4,
+        rel=0.1) == self.test_shape.volume
 
     def test_areas_are_correct(self):
         """Tests the areas of the faces are the correct sizes"""
@@ -70,11 +66,11 @@ class TestPoloidalFieldCoil(unittest.TestCase):
 
         hexagon_face_area = (3 * math.sqrt(3) / 2) * math.pow(length, 2)
 
-        self.test_shape.length_of_side = self.test_shape.length_of_side * 2
+        assert len(test_shape_areas) == 8
+        assert pytest.approx(hexagon_face_area, rel=0.1) in test_shape_areas
+        assert pytest.approx(length * distance, rel=0.1) in test_shape_areas
 
-        assert len(test_shape_areas) == 7  # correct this value
-        assert hexagon_face_area in test_shape_areas
-        assert length * distance in test_shape_areas
-
-        # add a check that makes sure only areas with these two values are
-        # present
+        # Make sure only areas with these two values are present
+        for area in test_shape_areas:
+            assert (pytest.approx(hexagon_face_area, rel=0.1) == area or
+                    pytest.approx(length * distance, rel=0.1) == area)
