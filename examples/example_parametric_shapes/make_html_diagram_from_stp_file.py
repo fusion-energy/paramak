a1a6e8b501a9f4ebde60d786a43416dfaecece8d
Jonathan Shimwell <jonathan.shimwell@ukaea.uk>
2021-02-09 11:35:37 +0000

    updated example to include 3d plots

--- examples/example_parametric_shapes/make_html_diagram_from_stp_file.py
+++ examples/example_parametric_shapes/make_html_diagram_from_stp_file.py
@@ -5,7 +5,9 @@ within the stp file."""
 import paramak
 
 
-def main():
+def make_stp_file():
+    """Creates an example stp file for plotting html point graphs"""
+
     # this creates a Shape object
     example_shape = paramak.ExtrudeMixedShape(
         distance=1,
@@ -19,34 +21,55 @@ def main():
             (110, 45, "spline"),
         ]
     )
+
+    # this exports the shape as a html image with a few different view planes
+    example_shape.export_html("example_shape_RZ.html")
+    example_shape.export_html("example_shape_XYZ.html", view_plane='XYZ')
+    example_shape.export_html("example_shape_XZ.html", view_plane='XZ')
+    
     # This exports the Shape object as an stp file that will be imported later
     example_shape.export_stp("example_shape.stp")
 
-    # this exports the shape as a html image
-    fig = example_shape.export_html("example_shape_from_solid.html")
 
-    # shows the html image resulting from the solid shape directly
-    fig.show()
+def load_stp_file_and_plot():
+    """Loads an stp file and plots html point graphs"""
 
-    # loads the stp file and obtains the solid 3d shape and list of wires
-    # (edges)
+    # loads the stp file and obtains the solid shape and list of wires / edges
     solid, wires = paramak.utils.load_stp_file(
         filename="example_shape.stp",
     )
 
     # produces a plot on the R (radius) Z axis and saves the html file
-    fig = paramak.utils.export_wire_to_html(
+    paramak.utils.export_wire_to_html(
+        wires=wires,
+        tolerance=0.1,
+        view_plane="RZ",
+        facet_splines=True,
+        facet_circles=True,
+        filename="example_shape_from_stp_RZ.html",
+    )
+
+    # produces a plot on the XZ axis and saves the html file
+    paramak.utils.export_wire_to_html(
         wires=wires,
         tolerance=0.1,
         view_plane="XZ",
         facet_splines=True,
         facet_circles=True,
-        filename="example_shape_from_stp.html",
+        filename="example_shape_from_stp_XZ.html",
     )
 
-    # shows the html image resulting from the stp file load
-    fig.show()
+    # produces a 3D plot with XYZ axis and saves the html file
+    paramak.utils.export_wire_to_html(
+        wires=wires,
+        tolerance=0.1,
+        view_plane="XYZ",
+        facet_splines=True,
+        facet_circles=True,
+        filename="example_shape_from_stp_XYZ.html",
+    )
 
 
 if __name__ == "__main__":
-    main()
+    make_stp_file()
+    load_stp_file_and_plot()
