209e7704efd6bcdf5ecb72c459e696a001fa1f11
Jonathan Shimwell <jonathan.shimwell@ukaea.uk>
2021-02-11 22:20:33 +0000

    added particle source extraction functions

--- paramak/neutronics_utils.py
+++ paramak/neutronics_utils.py
@@ -1,10 +1,19 @@
 
 import math
+import os
+import xml.etree.ElementTree as ET
 from collections import defaultdict
 
+import h5py
 import matplotlib.pyplot as plt
 import numpy as np
 
+try:
+    import openmc
+    from openmc.data import REACTION_MT, REACTION_NAME
+except ImportError:
+    warnings.warn('OpenMC not found, create_inital_particles \
+            method not available', UserWarning)
 
 def define_moab_core_and_tags():
     """Creates a MOAB Core instance which can be built up by adding sets of
@@ -361,3 +370,88 @@ def write_3d_mesh_tally_to_vtk(
     print('Writing %s' % outfile)
 
     writer.Write()
+
+
+def create_inital_particles(source, number_of_source_particles=2000):
+    """Accepts an openmc source and creates an inital_source.h5 that can be
+    used to find intial xyz, direction and energy of the partice source 
+    """
+
+    # MATERIALS
+
+    # no real materials are needed for finding the source
+    mats = openmc.Materials([])
+
+    # GEOMETRY
+
+    # just a minimal geometry
+    outer_surface = openmc.Sphere(r=100000, boundary_type='vacuum')
+    cell = openmc.Cell(region=-outer_surface)
+    universe = openmc.Universe(cells=[cell])
+    geom = openmc.Geometry(universe)
+
+
+    # SIMULATION SETTINGS
+    
+    # Instantiate a Settings object
+    sett = openmc.Settings()
+    sett.run_mode = "eigenvalue" # this will fail but it will write the inital_source.h5 file first
+    sett.particles = number_of_source_particles
+    sett.batches = 1
+    sett.inactive = 0
+    sett.write_initial_source = True
+
+    sett.source = source
+
+    model = openmc.model.Model(geom, mats, sett)
+
+    os.system('rm *.xml')
+    model.export_to_xml()
+
+    # this just adds write_initial_source == True to the settings.xml
+    tree = ET.parse("settings.xml")
+    root = tree.getroot()
+    elem = ET.SubElement(root, "write_initial_source")
+    elem.text = "true"
+    tree.write("settings.xml")
+
+    # This will crash hence the try except loop, but it writes the inital_source.h5
+    try:
+        openmc.run(output=False)
+    except:
+        pass
+
+    return "initial_source.h5"
+
+def extract_points_from_initial_source(
+    input_filename='initial_source.h5',
+    view_plane='RZ'):
+    """makes a plot of the inital creation locations of the particle source"""
+
+    f = h5py.File(input_filename,'r')
+    dset = f['source_bank']
+
+    points = []
+
+    for particle in dset:
+        if view_plane == 'XZ':
+            points.append((particle[0][0], particle[0][2]))
+        elif view_plane == 'XY':
+            points.append((particle[0][0], particle[0][1]))
+        elif view_plane == 'YZ':
+            points.append((particle[0][1], particle[0][2]))
+        elif view_plane == 'YX':
+            points.append((particle[0][1], particle[0][0]))
+        elif view_plane == 'ZY':
+            points.append((particle[0][2], particle[0][1]))
+        elif view_plane == 'ZX':
+            points.append((particle[0][2], particle[0][0]))
+        elif view_plane == 'RZ':
+            xy_coord = math.pow(particle[0][0], 2) + math.pow(particle[0][1], 2)
+            points.append((math.sqrt(xy_coord), particle[0][2]))
+        elif view_plane == 'XYZ':
+            points.append((particle[0][0], particle[0][1], particle[0][2]))
+        else:
+            raise ValueError('view_plane value of ', view_plane,
+                                ' is not supported')
+    return points
