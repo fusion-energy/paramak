77493825701228f60371e267b8e906c6ff023970
Jonathan Shimwell <jonathan.shimwell@ukaea.uk>
2021-02-15 20:03:53 +0000

    added magnets

--- examples/example_neutronics_simulations/ball_reactor_source_plot.py
+++ examples/example_neutronics_simulations/ball_reactor_source_plot.py
@@ -14,20 +14,26 @@ def make_model_and_simulate():
 
     # makes the 3d geometry
     my_reactor = paramak.BallReactor(
-        inner_bore_radial_thickness=1,
+        inner_bore_radial_thickness=10,
         inboard_tf_leg_radial_thickness=30,
         center_column_shield_radial_thickness=60,
-        divertor_radial_thickness=50,
+        divertor_radial_thickness=150,
         inner_plasma_gap_radial_thickness=30,
         plasma_radial_thickness=300,
         outer_plasma_gap_radial_thickness=30,
-        firstwall_radial_thickness=3,
-        blanket_radial_thickness=100,
-        blanket_rear_wall_radial_thickness=3,
-        elongation=2.75,
-        triangularity=0.5,
+        firstwall_radial_thickness=30,
+        blanket_radial_thickness=50,
+        blanket_rear_wall_radial_thickness=30,
+        elongation=2,
+        triangularity=0.55,
         number_of_tf_coils=16,
-        rotation_angle=360,
+        pf_coil_radial_thicknesses=[50, 50, 50, 50],
+        pf_coil_vertical_thicknesses=[50, 50, 50, 50],
+        pf_coil_to_rear_blanket_radial_gap=50,
+        pf_coil_to_tf_coil_radial_gap=50,
+        outboard_tf_coil_radial_thickness=100,
+        outboard_tf_coil_poloidal_thickness=50,
+        rotation_angle=180,
     )
 
     # creates a parametric plasma source, more details
