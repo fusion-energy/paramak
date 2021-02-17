d260a1069d66c3d3e21c55d6bacecdf5d79abdaa
Jonathan Shimwell <jonathan.shimwell@ukaea.uk>
2021-02-17 19:36:29 +0000

    added simple creation tests

---
+++ tests/test_parametric_reactors/test_eu_demo_2015_reactor.py
@@ -0,0 +1,57 @@
+
+import os
+import unittest
+from pathlib import Path
+
+import paramak
+
+
+class TestSparc2020Reactor(unittest.TestCase):
+
+    def test_make_sparc_2020_reactor(self):
+        output_filenames = [
+            'blanket.stp',
+            'divertor.stp',
+            'Graveyard.stp',
+            'outboard_pf_coils.stp',
+            'pf_coils_1.stp',
+            'pf_coils_2.stp',
+            'pf_coils_3.stp',
+            'pf_coils_4.stp',
+            'pf_coils_5.stp',
+            'tf_coil_casing.stp',
+            'tf_coil.stp',
+            'vacvessel.stp',
+        ]
+        os.system("rm *.stp")
+        my_reactor = paramak.EuDemoFrom2015PaperDiagram()
+        my_reactor.export_stp()
+        for output_filename in output_filenames:
+            assert Path(output_filename).exists() is True
+        os.system("rm *.stp")
+
+    def test_make_parametric_sparc_2020_rector(self):
+        """Runs the example to check the output files are produced"""
+        output_filenames = [
+            'blanket.stp',
+            'divertor.stp',
+            'Graveyard.stp',
+            'outboard_pf_coils.stp',
+            'pf_coils_1.stp',
+            'pf_coils_2.stp',
+            'pf_coils_3.stp',
+            'pf_coils_4.stp',
+            'pf_coils_5.stp',
+            'tf_coil_casing.stp',
+            'tf_coil.stp',
+            'vacvessel.stp',
+        ]
+        os.system("rm *.stp")
+        my_reactor = paramak.EuDemoFrom2015PaperDiagram(rotation_angle=90)
+        my_reactor.export_stp()
+        for output_filename in output_filenames:
+            assert Path(output_filename).exists() is True
+        os.system("rm *.stp")
+
+
+
