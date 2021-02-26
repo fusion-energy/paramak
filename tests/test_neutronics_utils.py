bbfaca80d07528cec6c0ec0eef3fbb6dbf3f3c85
autopep8 <autopep8@users.noreply.github.com>
2021-02-26 22:06:29 +0000

    Automated autopep8 fixes

--- tests/test_neutronics_utils.py
+++ tests/test_neutronics_utils.py
@@ -95,7 +95,7 @@ class TestNeutronicsUtilityFunctions(unittest.TestCase):
             source = openmc.Source()
             source.space = openmc.stats.Point((0, 0, 0))
             source.energy = openmc.stats.Discrete([14e6], [1])
-            
+
             paramak.neutronics_utils.create_inital_particles(source, 10)
 
             paramak.neutronics_utils.extract_points_from_initial_source(
