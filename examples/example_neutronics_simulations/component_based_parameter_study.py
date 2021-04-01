8e59e68c2b663c0ee626502ed6187f7b0635c2fe
autopep8 <autopep8@users.noreply.github.com>
2021-04-01 08:10:33 +0000

    Automated autopep8 fixes

--- examples/example_neutronics_simulations/component_based_parameter_study.py
+++ examples/example_neutronics_simulations/component_based_parameter_study.py
@@ -33,7 +33,9 @@ def main():
         my_model = paramak.NeutronicsModel(
             geometry=my_shape,
             source=source,
-            materials={'center_column_shield_mat': 'WC'},  # WC is tungsten carbide
+            materials={
+                'center_column_shield_mat': 'WC'},
+            # WC is tungsten carbide
             cell_tallies=['heating'],
             simulation_batches=10,  # should be increased for more accurate result
             simulation_particles_per_batch=10,  # settings are low to reduce time required
