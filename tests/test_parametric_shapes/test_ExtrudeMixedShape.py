683899a5503805a261909dd2eea9b75e82bbf8fc
Jonathan Shimwell <jonathan.shimwell@ukaea.uk>
2021-04-14 12:01:43 +0100

    changed tolerance to relative

--- tests/test_parametric_shapes/test_ExtrudeMixedShape.py
+++ tests/test_parametric_shapes/test_ExtrudeMixedShape.py
@@ -170,7 +170,7 @@ class TestExtrudeMixedShape(unittest.TestCase):
         assert Path("test_solid2.stp").exists() is True
         assert Path("test_wire.stp").exists() is True
 
-        assert pytest.approx(Path("test_solid.stp", abs=10).stat().st_size) == \
+        assert pytest.approx(Path("test_solid.stp", rel=1).stat().st_size) == \
             Path("test_solid2.stp").stat().st_size
         assert Path("test_wire.stp").stat().st_size < \
             Path("test_solid2.stp").stat().st_size
