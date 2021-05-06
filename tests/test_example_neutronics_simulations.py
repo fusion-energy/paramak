280617032af5978b0439ab3b66409bebb11c18cc
Jonathan Shimwell <jonathan.shimwell@ukaea.uk>
2021-05-06 19:17:37 +0100

    removed py file imports

--- tests/test_example_neutronics_simulations.py
+++ tests/test_example_neutronics_simulations.py
@@ -4,12 +4,6 @@ import sys
 import unittest
 from pathlib import Path
 
-from examples.example_neutronics_simulations import (
-    shape_with_gas_production,
-    component_based_mesh_simulation,
-    component_based_parameter_study,
-    shape_with_spectra_cell_tally)
-
 from .notebook_testing import notebook_run
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'examples'))
