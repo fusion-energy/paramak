652542c77c457045d76ef5e3227537512f81d9ba
autopep8 <autopep8@users.noreply.github.com>
2021-07-15 12:51:27 +0000

    Automated autopep8 fixes

--- tests/test_parametric_components/test_VacuumVesselInnerLeg.py
+++ tests/test_parametric_components/test_VacuumVesselInnerLeg.py
@@ -7,7 +7,15 @@ import paramak
 class TestVacuumVessel(unittest.TestCase):
 
     def setUp(self):
-        self.cutter = paramak.PortCutterRectangular(width = 100,height = 100, distance=1100, azimuth_placement_angle=[0,90,180,270])
+        self.cutter = paramak.PortCutterRectangular(
+            width=100,
+            height=100,
+            distance=1100,
+            azimuth_placement_angle=[
+                0,
+                90,
+                180,
+                270])
         self.test_shape = paramak.VacuumVesselInnerLeg(
             inner_height=1100,
             inner_radius=700,
@@ -19,7 +27,7 @@ class TestVacuumVessel(unittest.TestCase):
             inner_radius=700,
             inner_leg_radius=100,
             thickness=100,
-            cut = self.cutter
+            cut=self.cutter
         )
 
     def test_creation(self):
@@ -30,5 +38,3 @@ class TestVacuumVessel(unittest.TestCase):
 
     def test_cut(self):
         assert self.test_shape.volume != self.test_shape_cut.volume
-        
-
