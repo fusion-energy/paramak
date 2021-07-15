c19dc80777fbead399d3398fefc50898c359590d
Mate <mic24@kent.ac.uk>
2021-07-15 10:58:51 +0100

    added more tests

--- tests/test_parametric_components/test_ToroidalFieldCoilRectangleRoundCorner.py
+++ tests/test_parametric_components/test_ToroidalFieldCoilRectangleRoundCorner.py
@@ -215,3 +215,73 @@ def test_input_param_num_coil():
             distance=10,
             number_of_coils=1.5,
         )
+
+@pytest.mark.length
+def test_input_tuple():
+    with pytest.raises(ValueError):
+        o = ToroidalFieldCoilRectangleRoundCorners(
+            lower_inner_coordinates= (50,0,5),
+            mid_point_coordinates= (100,100),
+            thickness= 20,
+            distance= 10,
+            number_of_coils= 1,
+            )
+
+@pytest.mark.length
+def test_input_tuple2():
+    with pytest.raises(ValueError):
+        o = ToroidalFieldCoilRectangleRoundCorners(
+            lower_inner_coordinates= (50,0),
+            mid_point_coordinates= (100,100,100),
+            thickness= 20,
+            distance= 10,
+            number_of_coils= 1,
+            )
+
+@pytest.mark.value
+def test_input_num_coils():
+    with pytest.raises(TypeError):
+        o = ToroidalFieldCoilRectangleRoundCorners(
+            lower_inner_coordinates= (50,0),
+            mid_point_coordinates= (100,100),
+            thickness= 20,
+            distance= 10,
+            number_of_coils= 1.5,
+            )
+
+@pytest.mark.value
+def test_input_x_coordinates():
+    with pytest.raises(ValueError):
+        o = ToroidalFieldCoilRectangleRoundCorners(
+            lower_inner_coordinates= (50,0),
+            mid_point_coordinates= (0,100),
+            thickness= 20,
+            distance= 10,
+            number_of_coils= 1,
+            )
+
+@pytest.mark.value
+def test_input_testboolean():
+    with pytest.raises(TypeError):
+        o = ToroidalFieldCoilRectangleRoundCorners(
+            lower_inner_coordinates= (50,0),
+            mid_point_coordinates= (100,100),
+            thickness= 20,
+            distance= 10,
+            number_of_coils= 1,
+            test="not a boolean"
+            )
+
+
+@pytest.mark.value
+def test_input_analyseboolean():
+    with pytest.raises(TypeError):
+        o = ToroidalFieldCoilRectangleRoundCorners(
+            lower_inner_coordinates= (50,0),
+            mid_point_coordinates= (100,100),
+            thickness= 20,
+            distance= 10,
+            number_of_coils= 1,
+            analyse="not a boolean"
+            )
+
