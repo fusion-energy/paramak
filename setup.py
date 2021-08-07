7483a3c68ad07b03949665292e2581928d90cbe1
Jonathan <mail@jshimwell.com>
2021-08-07 01:45:01 +0100

    still working

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="paramak",
-    version="0.3.2",
+    version="develop",
     author="The Paramak Development Team",
     author_email="mail@jshimwell.com",
     description="Create 3D fusion reactor CAD models based on input parameters",
