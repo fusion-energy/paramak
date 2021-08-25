b0b212ac7890439fa9e10dcf2677d095c386430d
Rémi Delaporte-Mathurin <40028739+RemiTheWarrior@users.noreply.github.com>
2021-08-25 11:26:34 +0200

    changed the structure + added assert False

--- tests/test_example_components.py
+++ tests/test_example_components.py
@@ -1,7 +1,6 @@
 
 import os
 import sys
-import unittest
 from pathlib import Path
 
 from notebook_testing import notebook_run
@@ -9,15 +8,13 @@ from notebook_testing import notebook_run
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'examples'))
 
 
-class TestExampleComponents(unittest.TestCase):
-
-    def test_jupyter_notebooks_example_parametric_components(self):
-        for notebook in Path().rglob("examples/example_parametric_components/*.ipynb"):
-            print(notebook)
-            errors = notebook_run(notebook)
-            assert errors == []
-            # assert False
+def main():
+    for notebook in Path().rglob("examples/example_parametric_components/*.ipynb"):
+        print(notebook)
+        errors = notebook_run(notebook)
+        assert errors == []
+        assert False  # to show @Shimwell it can detect errors
 
 
 if __name__ == "__main__":
-    unittest.main()
+    main()
