6f98213593a118346b9accdd8a1072b3627c5f73
Rémi Delaporte-Mathurin <40028739+RemiTheWarrior@users.noreply.github.com>
2021-08-25 11:45:15 +0200

    fixed command for examples

--- run_tests.sh
+++ run_tests.sh
@@ -6,6 +6,6 @@ pytest tests/test_Reactor.py -v --cov=paramak --cov-append --cov-report term --c
 pytest tests/test_parametric_shapes/ -v --cov=paramak --cov-append --cov-report term --cov-report xml
 pytest tests/test_parametric_components/ -v --cov=paramak --cov-append --cov-report term --cov-report xml
 pytest tests/test_parametric_reactors/ -v --cov=paramak --cov-append --cov-report term --cov-report xml
-pytest tests/test_example_shapes.py -v --cov=paramak --cov-append --cov-report term --cov-report xml
-pytest tests/test_example_components.py -v --cov=paramak --cov-append --cov-report term --cov-report xml
-pytest tests/test_example_reactors.py -v --cov=paramak --cov-append --cov-report term --cov-report xml
+python tests/test_example_shapes.py
+python tests/test_example_components.py
+python tests/test_example_reactors.py
