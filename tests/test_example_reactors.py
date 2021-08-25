7210d7d2c74e3207a6004256256d09c540509e80
Rémi Delaporte-Mathurin <40028739+RemiTheWarrior@users.noreply.github.com>
2021-08-25 11:29:46 +0200

    fixed other example scripts

--- tests/test_example_reactors.py
+++ tests/test_example_reactors.py
@@ -1,34 +1,31 @@
 
 import os
 import sys
-import unittest
 from pathlib import Path
 import time
-from .notebook_testing import notebook_run
+from notebook_testing import notebook_run
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'examples'))
 
 
-class TestExampleReactors(unittest.TestCase):
+def main():
+    timings = []
+    for notebook in Path().rglob("examples/example_parametric_reactors/*.ipynb"):
+        start = time.time()
 
-    def test_jupyter_notebooks_example_parametric_reactors(self):
-        timings = []
-        for notebook in Path().rglob("examples/example_parametric_reactors/*.ipynb"):
-            start = time.time()
+        print(notebook)
+        errors = notebook_run(notebook)
+        assert errors == []
 
-            print(notebook)
-            errors = notebook_run(notebook)
-            assert errors == []
+        stop = time.time()
+        duration = stop - start
 
-            stop = time.time()
-            duration = stop - start
+        print((notebook, duration))
+        timings.append((notebook, duration))
 
-            print((notebook, duration))
-            timings.append((notebook, duration))
-
-        # to see timings run with pytest --capture=tee-sys
-        print(timings)
+    # to see timings run with pytest --capture=tee-sys
+    print(timings)
 
 
 if __name__ == "__main__":
-    unittest.main()
+    main()
