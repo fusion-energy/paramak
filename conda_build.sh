4df2f4436debd2c7f3b396050dc8748796d23c2a
Jonathan <mail@jshimwell.com>
2021-08-26 22:57:32 +0100

    no longer building versions unsupported by cq

--- conda_build.sh
+++ conda_build.sh
@@ -21,19 +21,20 @@ sed -i "s@$PLACEHOLDER@version=\"${VERSION}\"@g" "$VERSION_FILE"
 
 conda-build conda/ -c cadquery -c conda-forge --croot /tmp/conda-build 
 
-# converting using all includes quite a few oxs and linux versions (arm arch etc)
+# converting using all includes quite a few oxs and linux versions.
+# Several of which (arm arch etc) which are not all available for cadquery
 # conda convert /tmp/conda-build/linux-64/*.tar.bz2 --platform all  -o /tmp/conda-build
 
 # option for converting package to specified platforms
-# platforms=( osx-64 linux-32 linux-64 win-32 win-64 )
-# find /tmp/conda-build/linux-64/ -name *.tar.bz2 | while read file
-# do
-#     echo $file
-#     for platform in "${platforms[@]}"
-#     do
-#        conda convert --platform $platform $file  -o /tmp/conda-build/
-#     done
-# done
+platforms=( osx-64 linux-64 win-64 )
+find /tmp/conda-build/linux-64/ -name *.tar.bz2 | while read file
+do
+    echo $file
+    for platform in "${platforms[@]}"
+    do
+       conda convert --platform $platform $file  -o /tmp/conda-build/
+    done
+done
 
 anaconda upload -f /tmp/conda-build/*/*.tar.bz2
 
