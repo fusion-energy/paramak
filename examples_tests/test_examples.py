08fa60ad637ec01a76e5cbc0806929d24a5f846c
Liam Pattinson <liampattinson@gmail.com>
2022-01-21 14:35:40 +0000

    Further streamlined notebook testing to a single file
    Can call a single notebook test via:
    
    pytest examples_tests -k 'test_example[notebook_name]'
    
    For example, the notebook found at:
    
    examples/example_parametric_reactors/ball_reactor.ipynb
    
    Can be tested using
    
    pytest examples_tests -k 'test_example[ball_reactor]'

--- examples_tests/test_examples.py
+++ examples_tests/test_examples.py
@@ -1,19 +1,16 @@
-import os
-import sys
-
+import pytest
 import nbformat
+import platform
 from nbconvert.preprocessors import ExecutePreprocessor
 from nbconvert.preprocessors.execute import CellExecutionError
-
+from pathlib import Path
 
 def notebook_run(path):
     """
     Execute a notebook via nbconvert and collect output.
     :returns (parsed nb object, execution errors)
     """
-    kernel_name = "python%d" % sys.version_info[0]
-    this_file_directory = os.path.dirname(__file__)
-    errors = []
+    kernel_name = "python%s" % platform.python_version_tuple()[0]
 
     with open(path) as file:
         note_book = nbformat.read(file, as_version=4)
@@ -21,7 +18,7 @@ def notebook_run(path):
         ep = ExecutePreprocessor(kernel_name=kernel_name, timeout=800)
 
         try:
-            ep.preprocess(note_book, {"metadata": {"path": this_file_directory}})
+            ep.preprocess(note_book)
 
         except CellExecutionError as e:
             if "SKIP" in e.traceback:
@@ -29,4 +26,10 @@ def notebook_run(path):
             else:
                 raise e
 
-    return errors
+@pytest.mark.parametrize(
+    "notebook",
+    Path().rglob("examples/*/*.ipynb"),
+    ids = lambda x : x.stem,
+)
+def test_example(notebook):
+    notebook_run(notebook)
