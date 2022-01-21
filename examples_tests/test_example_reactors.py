c577d1085755f1bc15746a2a548b56c7a17daeaa
LiamPattinson <LiamPattinson@users.noreply.github.com>
2022-01-21 09:50:23 +0000

    [skip ci] Apply formatting changes

--- examples_tests/test_example_reactors.py
+++ examples_tests/test_example_reactors.py
@@ -7,10 +7,11 @@ from notebook_testing import notebook_run
 
 sys.path.append(os.path.join(os.path.dirname(__file__), "..", "examples"))
 
+
 @pytest.mark.parametrize(
     "notebook",
     Path().rglob("examples/example_parametric_reactors/*.ipynb"),
-    ids = os.path.basename,
+    ids=os.path.basename,
 )
 def test_example_reactors(notebook):
     errors = notebook_run(notebook)
