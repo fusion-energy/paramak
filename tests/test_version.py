0f40d5f2d06fb89f521ccd295ae7a91ea15ee1e0
Liam Pattinson <liampattinson@gmail.com>
2022-01-31 08:40:10 +0000

    Added better error reporting for test_version

--- tests/test_version.py
+++ tests/test_version.py
@@ -12,8 +12,8 @@ def test_version():
     # develop is used as the default version name in the dockerfile
     if version != "develop":
         version_bits = version.split(".")
-        assert len(version_bits) >= 3
+        assert len(version_bits) >= 3, "Version number is %s" % version
         # Ensure each of the first three parts is convertable to int
         # (The fourth part, if it exists, may be a development version)
         for bit in version_bits[:3]:
-            assert bit.isdigit()
+            assert bit.isdigit(), "Version number is %s" % version
