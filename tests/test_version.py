0c8649d3176dcff175de54a883e6a5799675ff48
Liam Pattinson <liampattinson@gmail.com>
2022-01-31 11:44:28 +0000

    Updated to use fstrings in test_version

--- tests/test_version.py
+++ tests/test_version.py
@@ -14,8 +14,8 @@ def test_version():
     # develop is used as the default version name in the dockerfile
     if version != "develop":
         version_bits = version.split(".")
-        assert len(version_bits) >= 2, "Version number is %s" % version
+        assert len(version_bits) >= 2, f"Version number is {version}"
         # Ensure each of the first three parts is convertable to int
         # (The fourth part, if it exists, may be a development version)
         for bit in version_bits[:2]:
-            assert bit.isdigit(), "Version number is %s" % version
+            assert bit.isdigit(), f"Version number is {version}"
