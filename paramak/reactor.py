0db6bd321c63322ee9ddb9eecc2bbc13301420a2
shimwell <mail@jshimwell.com>
2022-03-04 18:54:21 +0000

    [skip ci] changing jupyter cadquery version for ci

--- paramak/reactor.py
+++ paramak/reactor.py
@@ -203,9 +203,9 @@ class Reactor:
             from jupyter_cadquery import Part, PartGroup, show
         except ImportError:
             msg = (
-                "To use Reactor.show() you must install jupyter_cadquery. To"
-                'install jupyter_cadquery type "pip install jupyter_cadquery"'
-                " in the terminal"
+                'To use Reactor.show() you must install jupyter_cadquery. To '
+                'install jupyter_cadquery type "pip install jupyter_cadquery" '
+                'in the terminal'
             )
             raise ImportError(msg)
 
