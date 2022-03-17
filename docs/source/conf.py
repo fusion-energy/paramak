99586d2d4eddf15e0fdda361c47fca899c6dc74d
shimwell <shimwell@users.noreply.github.com>
2022-03-17 00:00:59 +0000

    [skip ci] Apply formatting changes

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -95,15 +95,15 @@ html_theme = "default"
 # documentation.
 #
 html_theme_options = {
-    'display_version': True,
-    'prev_next_buttons_location': 'bottom',
-    'style_external_links': False,
+    "display_version": True,
+    "prev_next_buttons_location": "bottom",
+    "style_external_links": False,
     # toc options
-    'collapse_navigation': True,
-    'sticky_navigation': True,
-    'navigation_depth': 3,
-    'includehidden': True,
-    'titles_only': False,
+    "collapse_navigation": True,
+    "sticky_navigation": True,
+    "navigation_depth": 3,
+    "includehidden": True,
+    "titles_only": False,
 }
 
 # Add any paths that contain custom static files (such as style sheets) here,
