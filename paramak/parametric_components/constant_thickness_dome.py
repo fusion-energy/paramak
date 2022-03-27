21729dd19ebfdc6921b689fbc210342c98bd57dd
shimwell <mail@jshimwell.com>
2022-03-27 23:36:54 +0100

    [skip ci] tidy up dome shape

--- paramak/parametric_components/constant_thickness_dome.py
+++ paramak/parametric_components/constant_thickness_dome.py
@@ -24,7 +24,7 @@ class ConstantThicknessDome(RotateMixedShape):
         thickness: float=10,
         chord_center: Tuple[float, float]=(0,50),
         chord_width: float= 100,
-        chord_height: float=40,
+        chord_height: float=20,
         upper_or_lower: str='lower',
         name='constant_thickness_dome',
         **kwargs,
@@ -98,44 +98,39 @@ class ConstantThicknessDome(RotateMixedShape):
         #          7  -       -
         #          |       -
         #          6   -
-        # 
-
+        #       far side
 
         if self.chord_height * 2 >= self.chord_width:
-            raise ValueError('ConstantThicknessDome requires that the self.chord_width is at least 2 times as large as the chord height')
+            msg='ConstantThicknessDome requires that the self.chord_width is at least 2 times as large as the chord height'
+            raise ValueError(msg)
 
         radius_of_sphere = ((math.pow(self.chord_width, 2)) + (4.0 * math.pow(self.chord_height, 2))) / (8 * self.chord_height)
 
+        point_1 = (self.chord_center[0] + (self.chord_width / 2), self.chord_center[1], "straight")
+
         if self.upper_or_lower == 'upper':
             center_point = (self.chord_center[0], self.chord_center[1] + self.chord_height - radius_of_sphere)
+            inner_tri_angle = math.atan((center_point[1] - self.chord_center[1]) / (self.chord_width / 2))
+            outer_tri_adj = math.cos(inner_tri_angle) * self.thickness
+            point_2 = (point_1[0] + outer_tri_adj, point_1[1], "straight")
+            outer_tri_opp = math.sqrt(math.pow(self.thickness, 2) - math.pow(outer_tri_adj, 2))
             point_7 = (self.chord_center[0], self.chord_center[1] + radius_of_sphere, "straight")
             point_6 = (self.chord_center[0], self.chord_center[1] + radius_of_sphere + self.thickness, "straight")
             self.far_side = (center_point[0], center_point[1] - (radius_of_sphere+self.thickness))
+            point_3 = (point_2[0], point_2[1] + outer_tri_opp, "straight")
         elif self.upper_or_lower == 'lower':
             center_point = (self.chord_center[0], self.chord_center[1] - self.chord_height + radius_of_sphere)
+            inner_tri_angle = math.atan((center_point[1] - self.chord_center[1]) / (self.chord_width / 2))
+            outer_tri_adj = math.cos(inner_tri_angle) * self.thickness
+            point_2 = (point_1[0] + outer_tri_adj, point_1[1], "straight")
+            outer_tri_opp = math.sqrt(math.pow(self.thickness, 2) - math.pow(outer_tri_adj, 2))
             point_7 = (self.chord_center[0], self.chord_center[1] - radius_of_sphere, "straight")
             point_6 = (self.chord_center[0], self.chord_center[1] - (radius_of_sphere + self.thickness), "straight")
             self.far_side = (center_point[0], center_point[1] + radius_of_sphere+self.thickness)
-        elif self.upper_or_lower not in ['upper', 'lower']:
-            raise ValueError('self.upper_or_lower')
-
-
-        point_1 = (self.chord_center[0] + (self.chord_width / 2), self.chord_center[1], "straight")
-
-        inner_tri_angle = math.atan((center_point[1] - self.chord_center[1]) / (self.chord_width / 2))
-
-        outer_tri_adj = math.cos(inner_tri_angle) * self.thickness
-
-        point_2 = (point_1[0] + outer_tri_adj, point_1[1], "straight")
-
-        outer_tri_opp = math.sqrt(math.pow(self.thickness, 2) - math.pow(outer_tri_adj, 2))
-
-        if self.upper_or_lower == 'upper':
-            point_3 = (point_2[0], point_2[1] + outer_tri_opp, "straight")
-        elif self.upper_or_lower == 'lower':
             point_3 = (point_2[0], point_2[1] - outer_tri_opp, "straight")
         else:
-            raise ValueError('self.upper_or_lower')
+            msg=f'upper_or_lower should be either "upper"  or "lower". Not {self.upper_or_lower}'
+            raise ValueError(msg)
 
         self.points = [point_1, point_2, point_3, point_6, point_7]
 
@@ -173,10 +168,9 @@ class ConstantThicknessDome(RotateMixedShape):
         cap = Shape()
         cap.solid = big_sphere.cut(small_sphere)
 
-        height = 2*(radius_of_sphere + center_point[1])
-        radius =  radius_of_sphere + center_point[0]
-        cutter =CuttingWedge(height=1000,radius=1000, rotation_angle=self.rotation_angle)
-        # cutter.show()
+        height = 2*(radius_of_sphere + abs(center_point[1])+self.thickness)
+        radius = 2*(radius_of_sphere + abs(center_point[0])+self.thickness)
+        cutter =CuttingWedge(height=height,radius=radius, rotation_angle=self.rotation_angle)
 
         cap.solid =cap.solid.intersect(cutter.solid)
         cap.solid =cap.solid.cut(outer_cylinder_cutter.solid)
