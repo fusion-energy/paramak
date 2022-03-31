c23c72e91a329e61f2c9d2ab2b461636fdcfbb5b
Jonathan Shimwell <mail@jshimwell.com>
2022-03-31 16:58:01 +0100

    needs height and width finding for wedge cutting

--- paramak/parametric_components/domed_extrusion.py
+++ paramak/parametric_components/domed_extrusion.py
@@ -1,5 +1,7 @@
 import math
-from paramak import Shape, ExtrudeCircleShape
+from paramak import Shape, CuttingWedge
+
+# from paramak.utils import calculate_wedge_cut
 import cadquery as cq
 
 
@@ -14,12 +16,15 @@ class DomedExtrusion(Shape):
 
     def __init__(
         self,
-        extrusion_distance: float=100,
-        dome_height: float=10,
-        radius: float=20,
-        extrusion_start_offset: float = 0.0,
+        extrusion_distance: float = 100,
+        dome_height: float = 5,
+        radius: float = 20,
+        extrusion_start_offset: float = 50.0,
         name: str = "domed_extrusion",
-        upper_or_lower: str = 'upper',
+        upper_or_lower: str = "upper",
+        workplane="XY",
+        # rotation_axis="Z",
+        rotation_angle=360,
         **kwargs,
     ):
 
@@ -31,46 +36,33 @@ class DomedExtrusion(Shape):
         self.extrusion_start_offset = extrusion_start_offset
         self.name = name
         self.upper_or_lower = upper_or_lower
+        self.workplane = workplane
+        self.rotation_angle = rotation_angle
+        # self.rotation_axis = rotation_axis
+
     # def find_points(self):
     #     """
     #     Finds the XZ points joined by straight and circle connections that
     #     describe the 2D profile of the vessel shape.
     #     """
 
-        # Note these points are not used in the normal way when constructing
-        # the solid
-        #
-        #          
-        #          
-        #          p3  -
-        #                -
-        #                  -
-        #                    -
-        #         cc          p2 
-        #     chord center    |
-        #                     |
-        #         sc          |
-        #     sphere center   |
-        #                     |
-        #        p0 ----------p1
-        #
-
-
-
-        # TODO set to 0 for now, add ability to shift the center of the chord left and right
-        # chord_center = (0, self.chord_center_height)
-
-        # point_1 = (chord_center[0] + (self.chord_width / 2), chord_center[1], "straight")
-
-        # point_0 = [(self.ring_radius, 0)]
-
-        # self.points = [point_1, point_2, point_3, point_6, point_7]
-
-    # def find_points(self):
-    
-    #     points = [(0, 0)]
-
-    #     self.points = points
+    # Note these points are not used in the normal way when constructing
+    # the solid
+    #
+    #
+    #
+    #          p3  -
+    #                -
+    #                  -
+    #                    -
+    #         cc          p2
+    #     chord center    |
+    #                     |
+    #         sc          |
+    #     sphere center   |
+    #                     |
+    #        p0 ----------p1
+    #
 
     def create_solid(self):
         """Creates a rotated 3d solid using points with circular edges.
@@ -79,36 +71,54 @@ class DomedExtrusion(Shape):
            A CadQuery solid: A 3D solid volume
         """
 
-        radius_of_sphere = ((math.pow(self.radius, 2)) + (4.0 * math.pow(self.dome_height, 2))) / (
+        # so a positive offset moves extrusion further from axis of azimuthal
+        # placement rotation
+
+        self.points = [(0, self.extrusion_start_offset)]
+
+        radius_of_sphere = ((math.pow(self.radius * 2, 2)) + (4.0 * math.pow(self.dome_height, 2))) / (
             8 * self.dome_height
         )
 
-        chord_center = (0, self.dome_height)
-        
+        cutting_cylinder_wire = (
+            cq.Workplane(self.workplane).workplane(offset=-self.extrusion_start_offset).circle(radius_of_sphere)
+        )
+        cutting_cylinder = cutting_cylinder_wire.extrude(until=-self.extrusion_distance, both=False)
+
         if self.upper_or_lower == "upper":
-            center_point = (chord_center[0], chord_center[1] + self.dome_height - radius_of_sphere)
+            center_of_sphere_offset = -(
+                self.extrusion_distance - self.extrusion_start_offset + radius_of_sphere - self.dome_height
+            )
         elif self.upper_or_lower == "lower":
-            center_point = (chord_center[0], chord_center[1] - self.dome_height + radius_of_sphere)
+            center_of_sphere_offset = (
+                -self.extrusion_distance - self.extrusion_start_offset + radius_of_sphere - self.dome_height
+            )
         else:
             raise ValueError("self.upper_or_lower")
-        
-        big_sphere = (
+
+        sphere = (
             cq.Workplane(self.workplane)
-            .moveTo(center_point[0], center_point[1])
+            .workplane(offset=center_of_sphere_offset)
             .sphere(radius_of_sphere)
+            .cut(cutting_cylinder)
         )
 
-        solid = ExtrudeCircleShape(
-            distance=self.extrusion_distance,
-            radius=self.radius,
-            extrusion_start_offset=self.extrusion_start_offset,
-            extrude_both = False,
-            color=self.color,
-            name="domed_extrusion",
-            # translate=self.translate
-        )
-        
-        solid.points = [(0, 0)]
+        wire = cq.Workplane(self.workplane).workplane(offset=-self.extrusion_start_offset).circle(self.radius)
 
+        solid = wire.extrude(until=-self.extrusion_distance, both=False)
 
-        self.solid = solid.solid.union(big_sphere)
+        solid = self.rotate_solid(solid)
+
+        print(solid.val().BoundingBox())
+        # cutting_wedge = calculate_wedge_cut(self)
+        cutting_wedge = CuttingWedge(height=1000, radius=1000)
+        solid = self.perform_boolean_operations(solid, wedge_cut=cutting_wedge)
+
+        # if self.translate:
+        #     solid = solid.translate(self.translate)
+
+        self.solid = solid.union(sphere)
+        # self.solid = solid.cut(sphereorg)
+        # self.solid = solid
+
+        return solid
