21c847a3615a14ac3645970e379d564b1e8ac793
shimwell <mail@jshimwell.com>
2022-08-13 15:52:29 +0100

    avoiding same name

--- paramak/reactor.py
+++ paramak/reactor.py
@@ -186,14 +186,10 @@ class Reactor:
 
         assembly = cq.Assembly(name="reactor")
         for entry in self.shapes_and_components:
-            if entry.name is None:
-                name = "Shape.name not set"
-            else:
-                name = entry.name
             if entry.color is None:
-                assembly.add(entry.solid, name=name)
+                assembly.add(entry.solid, name=entry.name)
             else:
-                assembly.add(entry.solid, name=name, color=cq.Color(*entry.color))
+                assembly.add(entry.solid, name=entry.name, color=cq.Color(*entry.color))
 
         return show(assembly, **kwargs)
 
