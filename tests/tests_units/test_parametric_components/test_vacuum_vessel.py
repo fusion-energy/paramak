7c91b5b72e4d20b52a0281560c7c4ba57c9621fc
shimwell <shimwell@users.noreply.github.com>
2023-01-24 21:33:10 +0000

    [skip ci] Apply formatting changes

--- tests/tests_units/test_parametric_components/test_vacuum_vessel.py
+++ tests/tests_units/test_parametric_components/test_vacuum_vessel.py
@@ -1,4 +1,3 @@
-
 import paramak
 
 
@@ -10,14 +9,13 @@ def test_creation():
 
     assert test_shape.solid is not None
 
+
 def test_ports():
     """Creates a vacuum vessel with ports holes in it and checks that a
     caquery solid is created."""
 
     test_shape = paramak.VacuumVessel(height=2, inner_radius=1, thickness=0.2)
-    cutter1 = paramak.PortCutterRectangular(
-        distance=3, center_point=(0, 0), height=0.2, width=0.4, fillet_radius=0.01
-    )
+    cutter1 = paramak.PortCutterRectangular(distance=3, center_point=(0, 0), height=0.2, width=0.4, fillet_radius=0.01)
     cutter2 = paramak.PortCutterRectangular(
         distance=3, center_point=(0.5, 0), height=0.2, width=0.4, fillet_radius=0.00
     )
