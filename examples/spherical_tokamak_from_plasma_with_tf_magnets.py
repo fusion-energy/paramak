e02cde02ae02e9466b16cdb89677f96d6ed4f39a
Jon Shimwell <jon@proximafusion.com>
2024-09-26 23:55:04 +0100

    not importing uninstalled package

--- examples/spherical_tokamak_from_plasma_with_tf_magnets.py
+++ examples/spherical_tokamak_from_plasma_with_tf_magnets.py
@@ -34,13 +34,16 @@ result = paramak.spherical_tokamak_from_plasma(
 
 result.save(f"spherical_tokamak_minimal.step")
 
-
+# visualization options
+#
+# pip install jupyter_cadquery
 # needed to downgrade pip with ... python -m pip  install pip==24.0
-from jupyter_cadquery import show
-
-view = show(result)
-view.export_html("3d.html")
-
+# from jupyter_cadquery import show
+# view = show(result)
+# view.export_html("3d.html")
+#
+# from cadquery.vis import show
+# show(result)
 
 my_model = CadToDagmc()
 material_tags = ["mat1"] * 7
