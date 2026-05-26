5137d932c0088b75b0ff8a4e35f545452c71f94a
Hridoy Kabiraj <rudrokabiraj@gmail.com>
2026-05-27 04:50:33 +0600

    Remove split_solids(), _copy_metadata() method from Assembly class
    Removed the _copy_metadata method and split_solids method from the Assembly class. 
    
    I have found a better solution and will add it in next PR.

--- src/paramak/assemblies/assembly.py
+++ src/paramak/assemblies/assembly.py
@@ -13,13 +13,6 @@ class Assembly(cq.Assembly):
     major_radius = None
     minor_radius = None
 
-    def _copy_metadata(self, target):
-        """Copies tokamak geometry metadata to another Assembly instance."""
-        target.elongation = self.elongation
-        target.triangularity = self.triangularity
-        target.major_radius = self.major_radius
-        target.minor_radius = self.minor_radius
-
     def remove(self, name: str):
         new_assembly = Assembly()
         part_found = False
@@ -38,42 +31,3 @@ class Assembly(cq.Assembly):
         for part in self:
             names.append(part[1].split('/')[-1])
         return names
-
-    def split_solids(self):
-        """
-        Explodes any part containing multiple solids into individually named parts.
-
-        Naming convention:
-          - Single-solid parts: name unchanged
-          - Multi-solid parts:  <original_name>_1, <original_name>_2, ... <original_name>_N
-
-        Example:
-          'add_extra_cut_shape_1' with 16 solids becomes:
-          'add_extra_cut_shape_1_1' through 'add_extra_cut_shape_1_16'
-        """
-        new_assembly = Assembly()
-
-        for part in self:
-            obj, full_path, loc, color = part
-            leaf_name = full_path.split('/')[-1]
-
-            if isinstance(obj, cq.Workplane):
-                solids = obj.solids().vals()
-                plane = obj.plane
-            elif isinstance(obj, cq.Shape):
-                solids = obj.Solids()
-                plane = None
-            else:
-                new_assembly.add(obj, name=leaf_name, color=color, loc=loc)
-                continue
-
-            if len(solids) <= 1:
-                new_assembly.add(obj, name=leaf_name, color=color, loc=loc)
-            else:
-                for idx, solid in enumerate(solids, start=1):
-                    split_name = f"{leaf_name}_{idx}"
-                    new_obj = cq.Workplane(plane).add(solid) if plane is not None else solid
-                    new_assembly.add(new_obj, name=split_name, color=color, loc=loc)
-
-        self._copy_metadata(new_assembly)
-        return new_assembly
